574d90c9a9b1e0e96a64af729f28f3ac131188a8
Bshara Zahran <bshara23@gmail.com>
2018-07-02 18:44:43 +1000

    Add work from C9.

--- inv_trader/data.py
+++ inv_trader/data.py
@@ -13,6 +13,8 @@ from redis import Redis
 from redis import ConnectionError
 from typing import List
 
+# Private IP 10.135.55.111
+
 
 class LiveDataClient:
     """
@@ -20,8 +22,8 @@ class LiveDataClient:
     """
 
     def __init__(self,
-                 host: str='localhost',
-                 port: int=6379):
+                 host: str = 'localhost',
+                 port: int = 6379):
         """
         Initializes a new instance of the LiveDataClient class.
 
@@ -93,6 +95,12 @@ class LiveDataClient:
         disconnect_message.append(f"Disconnected from live database at {self._host}:{self._port}.")
         return disconnect_message
 
+    def hacked_tick_message_printer(self, message):
+        print(f"{message['channel']}: {message['data']}", end='\r')
+
+    def hacked_bar_message_printer(self, message):
+        print(f"{message['channel']}: {message['data']}", end='\r')
+
     def subscribe_tick_data(
             self,
             symbol: str,
@@ -109,12 +117,14 @@ class LiveDataClient:
         if not self.is_connected:
             return "No connection is established with the live database."
 
-        security_symbol = f'{symbol}.{venue}'
+        tick_channel = f'{symbol}.{venue}'
 
-        self._pubsub.subscribe(security_symbol)
+        self._pubsub.subscribe(**{tick_channel: self.hacked_tick_message_printer})
+        thread1 = self._pubsub.run_in_thread(sleep_time=0.001)
 
-        if not any(security_symbol for s in self._subscriptions_tick):
-            self._subscriptions_tick.append(security_symbol).sort()
+        if not any(tick_channel for s in self._subscriptions_tick):
+            self._subscriptions_tick.append(tick_channel)
+            self._subscriptions_tick.sort()
 
     def unsubscribe_tick_data(
             self,
@@ -132,13 +142,67 @@ class LiveDataClient:
         if not self.is_connected:
             return "No connection is established with the live database."
 
+        tick_channel = f'{symbol}.{venue}'
+
+        self._pubsub.unsubscribe(tick_channel)
+
+        if any(tick_channel for s in self._subscriptions_tick):
+            self._subscriptions_tick.remove(tick_channel)
+            self._subscriptions_tick.sort()
+
+    def subscribe_bar_data(
+            self,
+            symbol: str,
+            venue: str,
+            period: str,
+            resolution: str,
+            quote_type: str) -> str:
+        """
+        Subscribe to live bar data for the given symbol and venue.
+        """
+        if symbol is None:
+            raise ValueError("The symbol cannot be null.")
+        if venue is None:
+            raise ValueError("The venue cannot be null.")
+        if self._client is None:
+            return "No connection has been established to the live database (please connect first)."
+        if not self.is_connected:
+            return "No connection is established with the live database."
+
         security_symbol = f'{symbol}.{venue}'
+        bar_channel = security_symbol + '-1-' + resolution + '[' + quote_type + ']'
 
-        self._pubsub.unsubscribe(security_symbol)
+        self._pubsub.subscribe(**{bar_channel: self.hacked_bar_message_printer})
+        thread2 = self._pubsub.run_in_thread(sleep_time=0.001)
 
-        if any(security_symbol for s in self._subscriptions_tick):
-            self._subscriptions_tick.remove(security_symbol).sort()
+        if not any(bar_channel for s in self._subscriptions_bars):
+            self._subscriptions_bars.append(bar_channel)
+            self._subscriptions_bars.sort()
 
+    def unsubscribe_bar_data(
+            self,
+            symbol: str,
+            venue: str,
+            period: str,
+            resolution: str,
+            quote_type: str) -> str:
+        """
+        Un-subscribes from live bar data for the given symbol and venue.
+        """
+        if symbol is None:
+            raise ValueError("The symbol cannot be null.")
+        if venue is None:
+            raise ValueError("The venue cannot be null.")
+        if self._client is None:
+            return "No connection has been established to the live database (please connect first)."
+        if not self.is_connected:
+            return "No connection is established with the live database."
 
+        security_symbol = f'{symbol}.{venue}'
+        bar_channel = security_symbol + '-1-' + resolution + '[' + quote_type + ']'
 
+        self._pubsub.unsubscribe(bar_channel)
 
+        if any(bar_channel for s in self._subscriptions_bars):
+            self._subscriptions_bars.remove(bar_channel)
+            self._subscriptions_bars.sort()
