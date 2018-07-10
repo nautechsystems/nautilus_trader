ce579f630a82500ecf0fe3c67f35e9ff025ec430
cjdsellers <cjdsellers@protonmail.com>
2018-07-11 07:45:54 +1000

    Add order types. Add unit tests.

--- tests/test_order.py
+++ tests/test_order.py
@@ -7,3 +7,51 @@
 # </copyright>
 # -------------------------------------------------------------------------------------------------
 
+import unittest
+import redis
+import datetime
+import pytz
+import time
+
+from decimal import Decimal
+
+from inv_trader.data import LiveDataClient
+from inv_trader.objects import Symbol, Tick, BarType, Bar
+from inv_trader.enums import Venue, OrderSide, OrderType, OrderStatus
+from inv_trader.factories import OrderFactory
+
+AUDUSD_FXCM = Symbol('audusd', Venue.FXCM)
+GBPUSD_FXCM = Symbol('gbpusd', Venue.FXCM)
+
+
+class OrderTests(unittest.TestCase):
+
+    def test_can_initialize_market_order(self):
+        # Arrange
+        # Act
+        order = OrderFactory.market(AUDUSD_FXCM,
+                                          'AUDUSD|123456|1',
+                                          'SCALPER-01',
+                                    OrderSide.BUY,
+                                    100000)
+
+        # Assert
+        self.assertEqual(OrderType.MARKET, order.type)
+        self.assertEqual(OrderStatus.INITIALIZED, order.status)
+        self.assertFalse(order.is_complete)
+
+
+    def test_can_initialize_stop_market_order(self):
+        # Arrange
+        # Act
+        order = OrderFactory.stop_market(AUDUSD_FXCM,
+                                          'AUDUSD|123456|1',
+                                          'SCALPER-01',
+                                         OrderSide.BUY,
+                                         100000)
+
+        # Assert
+        self.assertEqual(OrderType.MARKET, order.type)
+        self.assertEqual(OrderStatus.INITIALIZED, order.status)
+        self.assertFalse(order.is_complete)
+
