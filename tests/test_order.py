fdc4d4b7d8d30fb5af4319e97d1aabe0481305ca
cjdsellers <cjdsellers@protonmail.com>
2018-07-11 09:06:50 +1000

    Add unit tests.

--- tests/test_order.py
+++ tests/test_order.py
@@ -14,7 +14,9 @@ from decimal import Decimal
 
 from inv_trader.model.enums import Venue, OrderSide, OrderType, OrderStatus
 from inv_trader.model.objects import Symbol
-from inv_trader.model.events import OrderSubmitted
+from inv_trader.model.events import OrderSubmitted, OrderAccepted, OrderRejected, OrderWorking
+from inv_trader.model.events import OrderExpired, OrderModified, OrderCancelled, OrderCancelReject
+from inv_trader.model.events import OrderPartiallyFilled, OrderFilled
 from inv_trader.factories import OrderFactory
 from test_kit.constants import TestConstants
 
@@ -143,11 +145,151 @@ class OrderTests(unittest.TestCase):
             OrderSide.BUY,
             100000)
 
-        event = OrderSubmitted(order.symbol, order.id, UNIX_EPOCH, uuid.uuid4(), UNIX_EPOCH)
+        event = OrderSubmitted(
+            order.symbol,
+            order.id,
+            UNIX_EPOCH,
+            uuid.uuid4(),
+            UNIX_EPOCH)
 
         # Act
         order.apply(event)
 
         # Assert
         self.assertEqual(OrderStatus.SUBMITTED, order.status)
+        self.assertEqual(1, order.event_count)
+        self.assertEqual(event, order.events[0])
+        self.assertFalse(order.is_complete)
+
+    def test_can_apply_order_accepted_event_to_order(self):
+        # Arrange
+        order = OrderFactory.market(
+            AUDUSD_FXCM,
+            'AUDUSD|123456|1',
+            'SCALPER-01',
+            OrderSide.BUY,
+            100000)
+
+        event = OrderAccepted(
+            order.symbol,
+            order.id,
+            UNIX_EPOCH,
+            uuid.uuid4(),
+            UNIX_EPOCH)
+
+        # Act
+        order.apply(event)
+
+        # Assert
+        self.assertEqual(OrderStatus.ACCEPTED, order.status)
+        self.assertFalse(order.is_complete)
+
+    def test_can_apply_order_rejected_event_to_order(self):
+        # Arrange
+        order = OrderFactory.market(
+            AUDUSD_FXCM,
+            'AUDUSD|123456|1',
+            'SCALPER-01',
+            OrderSide.BUY,
+            100000)
+
+        event = OrderRejected(
+            order.symbol,
+            order.id,
+            UNIX_EPOCH,
+            'ORDER ID INVALID',
+            uuid.uuid4(),
+            UNIX_EPOCH)
+
+        # Act
+        order.apply(event)
+
+        # Assert
+        self.assertEqual(OrderStatus.REJECTED, order.status)
+        self.assertTrue(order.is_complete)
+
+    def test_can_apply_order_working_event_to_order(self):
+        # Arrange
+        order = OrderFactory.market(
+            AUDUSD_FXCM,
+            'AUDUSD|123456|1',
+            'SCALPER-01',
+            OrderSide.BUY,
+            100000)
+
+        event = OrderWorking(
+            order.symbol,
+            order.id,
+            'SOME_BROKER_ID',
+            UNIX_EPOCH,
+            uuid.uuid4(),
+            UNIX_EPOCH)
+
+        # Act
+        order.apply(event)
+
+        # Assert
+        self.assertEqual(OrderStatus.WORKING, order.status)
+        self.assertEqual('SOME_BROKER_ID', order.broker_id)
+        self.assertFalse(order.is_complete)
+
+    def test_can_apply_order_expired_event_to_order(self):
+        # Arrange
+        order = OrderFactory.market(
+            AUDUSD_FXCM,
+            'AUDUSD|123456|1',
+            'SCALPER-01',
+            OrderSide.BUY,
+            100000)
+
+        event = OrderExpired(
+            order.symbol,
+            order.id,
+            UNIX_EPOCH,
+            uuid.uuid4(),
+            UNIX_EPOCH)
+
+        # Act
+        order.apply(event)
+
+        # Assert
+        self.assertEqual(OrderStatus.EXPIRED, order.status)
+        self.assertTrue(order.is_complete)
+
+    def test_can_apply_order_modified_event_to_order(self):
+        # Arrange
+        order = OrderFactory.market(
+            AUDUSD_FXCM,
+            'AUDUSD|123456|1',
+            'SCALPER-01',
+            OrderSide.BUY,
+            100000)
+
+        order_working = OrderWorking(
+            order.symbol,
+            order.id,
+            'SOME_BROKER_ID_1',
+            UNIX_EPOCH,
+            uuid.uuid4(),
+            UNIX_EPOCH)
+
+        order_modified = OrderModified(
+            order.symbol,
+            order.id,
+            'SOME_BROKER_ID_2',
+            Decimal('1.00001'),
+            UNIX_EPOCH,
+            uuid.uuid4(),
+            UNIX_EPOCH)
+
+        order.apply(order_working)
+
+        # Act
+        order.apply(order_modified)
+
+        # Assert
+        self.assertEqual(OrderStatus.WORKING, order.status)
+        self.assertEqual('SOME_BROKER_ID_2', order.broker_id)
+        self.assertEqual(Decimal('1.00001'), order.price)
+        self.assertFalse(order.is_complete)
 
