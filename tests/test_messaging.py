2e06b0cba9c581d1fec26cc423cd4b87891c52df
cjdsellers <cjdsellers@protonmail.com>
2018-07-17 06:48:55 +1000

    Add msgpack tests.

--- tests/test_messaging.py
+++ tests/test_messaging.py
@@ -10,6 +10,8 @@
 import unittest
 import msgpack
 
+UTF8 = 'utf8'
+
 
 class MessagingTests(unittest.TestCase):
 
@@ -21,10 +23,25 @@ class MessagingTests(unittest.TestCase):
             'order_id': 'O123456',
             'timestamp': '1970-01-01T00:00:00.000Z'}
 
-
         # Act
         packed = msgpack.packb(message)
+        unpacked = msgpack.unpackb(packed)
+
+        print('\n')
+        print(packed.hex())
+        self.assertEqual(4, len(unpacked))
+        self.assertEqual(unpacked['header'.encode(UTF8)].decode(UTF8), 'order_cancelled')
+
+    def test_can_deserialize_from_c_sharp_msg_pack(self):
+        # Arrange
+        # From C# MsgPack.Cli
+        # {b'header': b'order_cancelled'}
+        hex_string = '81a6686561646572af6f726465725f63616e63656c6c6564'
+        data = bytes.fromhex(hex_string)
+
+        # Act
+        unpacked = msgpack.unpackb(data)
 
         # Assert
-        print(message)
-        print(packed)
+        self.assertEqual(1, len(unpacked))
+        self.assertEqual(unpacked['header'.encode(UTF8)].decode(UTF8), 'order_cancelled')
