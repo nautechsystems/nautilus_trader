ddd678d70a045e3ec7a932dac01a45921d81ec6c
Bshara Zahran <bshara23@gmail.com>
2018-08-18 12:03:36 +1000

    Cleanup unit test.

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -9,27 +9,7 @@
 
 import unittest
 
-from decimal import Decimal
-
-from inv_trader.model.objects import Price
-
 
 class ObjectTests(unittest.TestCase):
     pass
-    # def test_decimal_can_represent_a_price(self):
-    #     # Arrange
-    #     # Act
-    #     price = Decimal(1.00000)
-    #
-    #     self.assertEqual(1.00000, price)
-    #     self.assertEqual('1.00000', repr(price))
-    #
-    # def test_can_create_expected_prices(self):
-    #     # Arrange
-    #     price = Price(1.00000)
-    #
-    #     # Act
-    #     result = str(price)
-    #
-    #     self.assertEqual('1.00000', result)
-
+    # TODO
