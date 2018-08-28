eafeab22f6e25d8202b9f53a5b408322a737e533
cjdsellers <cjdsellers@protonmail.com>
2018-08-29 06:04:40 +1000

    Create MQWorker base class.

--- inv_trader/messaging.py
+++ inv_trader/messaging.py
@@ -7,6 +7,7 @@
 # </copyright>
 # -------------------------------------------------------------------------------------------------
 
+import abc
 import zmq
 
 from typing import Callable
@@ -18,18 +19,26 @@ from inv_trader.core.checks import typechecking
 UTF8 = 'utf-8'
 
 
-class RequestWorker(Thread):
+class MQWorker(Thread):
+    """
+    The abstract base class for all MQ workers.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
     @typechecking
     def __init__(
             self,
             name: str,
             context: Context,
+            socket_type: int,
             host: str,
             port: int,
             handler: Callable):
         """
-        Initializes a new instance of the RequestWorker class.
+        Initializes a new instance of the MQWorker class.
 
+        :param name: The name of the worker.
         :param context: The ZeroMQ context.
         :param host: The service host address.
         :param port: The service port.
@@ -41,7 +50,7 @@ class RequestWorker(Thread):
         self._context = context
         self._service_address = f'tcp://{host}:{port}'
         self._handler = handler
-        self._socket = self._context.socket(zmq.REQ)
+        self._socket = self._context.socket(socket_type)
         self._cycles = 0
 
     def run(self):
@@ -69,6 +78,58 @@ class RequestWorker(Thread):
         Close the connection and stop the worker.
         """
         self._close_connection()
+        self._log(f"Stopped")
+
+    @abc.abstractmethod
+    def _open_connection(self):
+        """
+        Open a new connection to the service.
+        """
+        # Raise exception if not overridden in implementation.
+        raise NotImplementedError("Method must be implemented in the subclass.")
+
+    @abc.abstractmethod
+    def _close_connection(self):
+        """
+        Close the connection with the service.
+        """
+        # Raise exception if not overridden in implementation.
+        raise NotImplementedError("Method must be implemented in the subclass.")
+
+    @typechecking
+    def _log(self, message: str):
+        """
+        Log the given message (if no logger then prints).
+
+        :param message: The message to log.
+        """
+        print(f"{self._name}: {message}")
+
+
+class RequestWorker(MQWorker):
+    @typechecking
+    def __init__(
+            self,
+            name: str,
+            context: Context,
+            host: str,
+            port: int,
+            handler: Callable):
+        """
+        Initializes a new instance of the RequestWorker class.
+
+        :param context: The ZeroMQ context.
+        :param host: The service host address.
+        :param port: The service port.
+        :param handler: The response handler.
+        """
+        super().__init__(
+            name,
+            context,
+            zmq.REQ,
+            host,
+            port,
+            handler)
 
     def _open_connection(self):
         """
@@ -84,17 +145,8 @@ class RequestWorker(Thread):
         self._log(f"Disconnecting from {self._service_address}...")
         self._socket.disconnect(self._service_address)
 
-    @typechecking
-    def _log(self, message: str):
-        """
-        Log the given message (if no logger then prints).
 
-        :param message: The message to log.
-        """
-        print(f"{self._name}: {message}")
-
-
-class SubscriberWorker(Thread):
+class SubscriberWorker(MQWorker):
     @typechecking
     def __init__(
             self,
@@ -113,14 +165,14 @@ class SubscriberWorker(Thread):
         :param topic: The topic to subscribe to.
         :param handler: The response handler.
         """
-        super().__init__()
-        self._name = name
-        self._context = context
-        self._service_address = f'tcp://{host}:{port}'
-        self._handler = handler
-        self._socket = self._context.socket(zmq.SUB)
+        super().__init__(
+            name,
+            context,
+            zmq.SUB,
+            host,
+            port,
+            handler)
         self._topic = topic
-        self._cycles = 0
 
     def run(self):
         """
@@ -128,24 +180,6 @@ class SubscriberWorker(Thread):
         Starts the worker and opens a connection.
         """
         self._open_connection()
-
-    def send(self, message: bytes):
-        """
-        Send the message to the service socket.
-
-        :param message: The message bytes to send.
-        """
-        self._socket.send(message)
-        self._log(f"Sending {message}")
-
-        response = self._socket.recv()
-        self._log(f"Received {response.decode(UTF8)}[{self._cycles}] response.")
-
-    def stop(self):
-        """
-        Close the connection and stop the worker.
-        """
-        self._close_connection()
 
     def _open_connection(self):
         """
@@ -173,12 +207,3 @@ class SubscriberWorker(Thread):
         """
         self._log(f"Disconnecting from {self._service_address}...")
         self._socket.disconnect(self._service_address)
-
-    @typechecking
-    def _log(self, message: str):
-        """
-        Log the given message (if no logger then prints).
-
-        :param message: The message to log.
-        """
-        print(f"{self._name}: {message}")
