001be58bff04a505375a0996fa8309fa02757a2d
cjdsellers <cjdsellers@protonmail.com>
2018-08-29 06:38:48 +1000

    Add unit tests.

--- test_kit/mocks.py
+++ test_kit/mocks.py
@@ -56,14 +56,14 @@ class MockServer(Thread):
 
     def run(self):
         """
-        Overrides the threads run method.
-        Starts the mock server and opens a connection (use the start method).
+        Overrides the threads run method (call .start() to run in a separate thread).
+        Starts the worker and opens a connection.
         """
         self._open_connection()
 
     def send(self, message: bytes):
         """
-        Send the message to the service socket.
+        Send the given message to the connected requesters.
 
         :param message: The message bytes to send.
         """
@@ -76,13 +76,13 @@ class MockServer(Thread):
 
     def stop(self):
         """
-        Close the connection and stop the worker.
+        Close the connection and stop the mock server.
         """
         self._close_connection()
 
     def _open_connection(self):
         """
-        Open a new connection to the service socket..
+        Open a new connection to the service..
         """
         self._log(f"Connecting to {self._service_address}...")
         self._socket.bind(self._service_address)
@@ -104,6 +104,79 @@ class MockServer(Thread):
     def _close_connection(self):
         """
         Close the connection with the service socket.
+        """
+        self._log(f"Disconnecting from {self._service_address}...")
+        self._socket.disconnect(self._service_address)
+
+    @typechecking
+    def _log(self, message: str):
+        """
+        Log the given message (if no logger then prints).
+
+        :param message: The message to log.
+        """
+        print(f"MockServer: {message}")
+
+
+class MockPublisher(Thread):
+    @typechecking
+    def __init__(
+            self,
+            context: Context,
+            port: int,
+            handler: Callable):
+        """
+        Initializes a new instance of the MockServer class.
+
+        :param context: The ZeroMQ context.
+        :param port: The service port.
+        :param handler: The response handler.
+        """
+        super().__init__()
+        self.daemon = True
+        self._context = context
+        self._service_address = f'tcp://127.0.0.1:{port}'
+        self._handler = handler
+        self._socket = self._context.socket(zmq.PUB)
+        self._cycles = 0
+
+    def run(self):
+        """
+        Overrides the threads run method.
+        Starts the mock server and opens a connection (use the start method).
+        """
+        self._open_connection()
+
+    def publish(
+            self,
+            topic: str,
+            message: bytes):
+        """
+        Publish the message to the subscribers.
+
+        :param topic: The topic of the message being published.
+        :param message: The message bytes to send.
+        """
+        self._socket.send(topic.encode(UTF8) + b' ' + message)
+        self._cycles += 1
+        self._log(f"Publishing message[{self._cycles}] {message} for topic {topic}")
+
+    def stop(self):
+        """
+        Close the connection and stop the publisher.
+        """
+        self._close_connection()
+
+    def _open_connection(self):
+        """
+        Open a new connection to the service.
+        """
+        self._log(f"Connecting to {self._service_address}...")
+        self._socket.bind(self._service_address)
+
+    def _close_connection(self):
+        """
+        Close the connection with the service.
         """
         self._log(f"Disconnecting from {self._service_address}...")
         self._socket.disconnect(self._service_address)
