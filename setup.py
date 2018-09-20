1a52c8158d5ce0400f4a23e4942eb7b4bd298759
cjdsellers <cjdsellers@protonmail.com>
2018-09-20 10:59:12 +1000

    Change version. Fix data client registration.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ extensions = [make_extension(name) for name in scan_dir(INV_TRADER)]
 
 setup(
     name=INV_TRADER,
-    version='0.56.2',
+    version='0.56.3',
     author='Invariance',
     description='The python trading client for Invariance.',
     packages=setuptools.find_packages(),
