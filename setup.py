f3af02e11a8896a1907c747255c57022be15088b
cjdsellers <cjdsellers@protonmail.com>
2018-09-23 12:13:47 +1000

    Fix data subscription.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ extensions = [make_extension(name) for name in scan_dir(INV_TRADER)]
 
 setup(
     name=INV_TRADER,
-    version='0.59.0',
+    version='0.59.1',
     author='Invariance',
     description='The python trading client for Invariance.',
     packages=setuptools.find_packages(),
