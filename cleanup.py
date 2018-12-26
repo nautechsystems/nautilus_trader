5fbff5b5d25c6025c883a0152a65886525d6b93a
Bshara Zahran <bshara23@gmail.com>
2018-12-26 17:35:07 +1100

    Add cleanup program.

---
+++ cleanup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -------------------------------------------------------------------------------------------------
+# <copyright file="cleanup.py" company="Invariance Pte">
+#  Copyright (C) 2018 Invariance Pte. All rights reserved.
+#  The use of this source code is governed by the license as found in the LICENSE.md file.
+#  http://www.invariance.com
+# </copyright>
+# -------------------------------------------------------------------------------------------------
+
+import os
+
+cython_extension = (".c", ".so", ".o", ".pyd")
+
+if __name__ == "__main__":
+    for file in os.listdir('inv_trader'):
+        path = os.path.join('inv_trader', file)
+        if os.path.isfile(path) and path.endswith(cython_extension):
+            os.remove(path)
+
+    for file in os.listdir('inv_trader/core'):
+        path = os.path.join('inv_trader/core', file)
+        if os.path.isfile(path) and path.endswith(cython_extension):
+            os.remove(path)
+
+    for file in os.listdir('inv_trader/model'):
+        path = os.path.join('inv_trader/model', file)
+        if os.path.isfile(path) and path.endswith(cython_extension):
+            os.remove(path)
