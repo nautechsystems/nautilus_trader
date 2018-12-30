37694cb2261d72cc221a21ceac1044542e03ad26
Christopher Sellers <cjdsellers@protonmail.com>
2018-12-30 15:05:56 +1100

    Improve setup.

--- setup.py
+++ setup.py
@@ -25,14 +25,22 @@ INV_TRADER = 'inv_trader'
 Options.embed_pos_in_docstring = True
 
 
-# Recursively scan root directory for all files to cythonize
-def scan_dir(root_dir, files=[]) -> List[str]:
-    for file in os.listdir(root_dir):
-        path = os.path.join(root_dir, file)
+# Recursively scan given directories
+def scan_directories(directories) -> List[str]:
+    file_names = []
+    for directory in directories:
+        file_names + scan_files(directory)
+    return file_names
+
+
+# Recursively scan for all files to cythonize
+def scan_files(directory, files=[]) -> List[str]:
+    for file in os.listdir(directory):
+        path = os.path.join(directory, file)
         if os.path.isfile(path) and path.endswith(".pyx"):
             files.append(path.replace(os.path.sep, ".")[:-4])
         elif os.path.isdir(path):
-            scan_dir(path, files)
+            scan_files(path, files)
     return files
 
 
@@ -46,11 +54,11 @@ def make_extension(ext_name) -> Extension:
 
 
 # Generate list of extensions
-extensions = [make_extension(name) for name in scan_dir(INV_TRADER)]
+extensions = [make_extension(name) for name in scan_directories([INV_TRADER, 'test_kit'])]
 
 setup(
     name=INV_TRADER,
-    version='0.70.0',
+    version='0.70.1',
     author='Invariance',
     description='The python trading client for Invariance.',
     packages=setuptools.find_packages(),
