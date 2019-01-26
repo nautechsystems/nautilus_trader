7b6b14380b3eae5904923f3d281486232ef64560
Bshara Zahran <bshara23@gmail.com>
2019-01-27 10:29:06 +1100

    Fix Price arithmetic.

--- tests/unit_tests/test_objects.py
+++ tests/unit_tests/test_objects.py
@@ -9,39 +9,13 @@
 
 import unittest
 
+from decimal import Decimal
+
 from inv_trader.model.enums import Venue
 from inv_trader.model.objects import Symbol, Price
 
 
 class ObjectTests(unittest.TestCase):
-
-    # def test_create_price_with_zero_price_raises_exception(self):
-    #     # Arrange
-    #     # Act
-    #     # Assert
-    #     self.assertRaises(AssertionError, Price, -1, 0)
-    #
-    # def test_create_price_with_negative_decimals_raises_exception(self):
-    #     # Arrange
-    #     # Act
-    #     # Assert
-    #     self.assertRaises(AssertionError, Price, 1.00000, -1)
-
-    def test_create_price_with_valid_inputs_returns_expected_decimal_object(self):
-        # Arrange
-        # Act
-        result1 = Price(1.00000, 5)
-        result2 = Price(1.0, 0)
-        result3 = Price(1.001, 2)
-        result4 = Price(1.1, 0)
-        result5 = Price(1.000001, 5)
-
-        # Assert
-        self.assertEqual(Price('1.00000'), result1)
-        self.assertEqual(Price('1'), result2)
-        self.assertEqual(Price('1.00'), result3)
-        self.assertEqual(Price('1.0'), result4)
-        self.assertEqual(Price('1.0'), result5)
 
     def test_symbol_equality(self):
         # Arrange
@@ -63,3 +37,111 @@ class ObjectTests(unittest.TestCase):
         # Assert
         self.assertEqual("AUDUSD.FXCM", str(symbol))
         self.assertTrue(repr(symbol).startswith("<AUDUSD.FXCM object at"))
+
+    def test_price_initialized_with_negative_value_raises_exception(self):
+        # Arrange
+        # Act
+        # Assert
+        self.assertRaises(ValueError, Price, -1.0, 0)
+
+    def test_price_initialized_with_negative_precision_raises_exception(self):
+        # Arrange
+        # Act
+        # Assert
+        self.assertRaises(ValueError, Price, 1.00000, -1)
+
+    def test_price_initialized_with_valid_inputs(self):
+        # Arrange
+        # Act
+        result1 = Price(1.00000, 5)
+        result2 = Price(1.0, 0)
+        result3 = Price(1.001, 2)
+        result4 = Price(1.1, 0)
+        result5 = Price(1.000001, 5)
+        result6 = Price(Decimal('1.000'))
+
+        # Assert
+        self.assertEqual(Price('1.00000'), result1)
+        self.assertEqual(Price('1'), result2)
+        self.assertEqual(Price('1.00'), result3)
+        self.assertEqual(Price('1.0'), result4)
+        self.assertEqual(Price('1.0'), result5)
+        self.assertEqual(1.0, result5.as_float())
+        self.assertEqual(Price('1.000'), result6)
+        self.assertEqual(1.000, result6.as_float())
+
+    def test_price_equality(self):
+        # Arrange
+        # Act
+        price1 = Price('1.00000')
+        price2 = Price('1.00000')
+        price3 = Price('2.00000')
+        price4 = Price('1.01')
+
+        result1 = price1 != price4
+
+        # Assert
+        self.assertEqual(price1, price2)
+        self.assertNotEqual(price1, price3)
+        self.assertNotEqual(price1, price4)
+        self.assertTrue(result1)
+
+    def test_price_str(self):
+        # Arrange
+        price = Price(1.00000, 5)
+
+        # Act
+        result = str(price)
+
+        # Assert
+        self.assertEqual('1.00000', result)
+
+    def test_price_repr(self):
+        # Arrange
+        price = Price(1.00000, 5)
+
+        # Act
+        result = repr(price)
+
+        # Assert
+        self.assertTrue(result.startswith('<Price(1.00000) object at'))
+
+    def test_price_operators(self):
+        # Arrange
+        price1 = Price('0.500')
+        price2 = Price('1.000')
+        price3 = Price('1.500')
+
+        # Act
+        # Assert
+        self.assertTrue(price1 < price2)
+        self.assertTrue(price1 <= price2)
+        self.assertTrue(price2 <= price2)
+        self.assertTrue(price3 > price2)
+        self.assertTrue(price3 >= price3)
+
+    def test_price_arithmetic(self):
+        # Arrange
+        # Act
+        result1 = Price('1.0000') + 1.0000
+        result2 = Price('1.0000') + Decimal('1.0000')
+        result3 = Price('1.0000') + Price('1.0000')
+
+        result4 = Price('3.0000') - 1.0000
+        result5 = Price('3.0000') - Decimal('1.0000')
+        result6 = Price('3.0000') - Price('1.0000')
+
+        # Assert
+        self.assertEqual(Decimal, type(result1))
+        self.assertEqual(Decimal('2.0000'), result1)
+        self.assertEqual(Decimal, type(result2))
+        self.assertEqual(Decimal('2.0000'), result2)
+        self.assertEqual(Decimal, type(result3))
+        self.assertEqual(Decimal('2.0000'), result3)
+
+        self.assertEqual(Decimal, type(result4))
+        self.assertEqual(Decimal('2.0000'), result4)
+        self.assertEqual(Decimal, type(result5))
+        self.assertEqual(Decimal('2.0000'), result5)
+        self.assertEqual(Decimal, type(result6))
+        self.assertEqual(Decimal('2.0000'), result6)
