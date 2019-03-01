5b254f4a50a685ad461db6cfb7ae82e6774a59c8
cjdsellers <cjdsellers@protonmail.com>
2019-03-02 10:18:15 +1100

    Improve cleanup.py.

--- tests/performance_tests/backtest_perf.py
+++ tests/performance_tests/backtest_perf.py
@@ -119,5 +119,7 @@ class BacktestEnginePerformanceTests(unittest.TestCase):
         # 15/02/19 45602587 function calls (45602559 primitive calls) in 32.350 seconds (introduced position events)
         # 24/02/19 40874448 function calls (40874420 primitive calls) in 31.250 seconds (separate threading between test and live)
         # 02/03/19 30212024 function calls (30212011 primitive calls) in 17.260 seconds (remove redundant Lock in id generation)
+        # 02/03/19 30638417 function calls (30638404 primitive calls) in 18.212 seconds (add max trade size limiter)
+        # 02/03/19  4521539 function calls  (4521526 primitive calls) in 10.337 seconds (no profile hooks!)
 
         self.assertTrue(True)
