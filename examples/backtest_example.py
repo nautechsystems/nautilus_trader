677a78ee725a54f42f6ab3f1e4fed41117c9c812
cjdsellers <cjdsellers@protonmail.com>
2019-03-10 12:41:28 +1100

    Add Python EMACross example strategy.

--- examples/backtest_example.py
+++ examples/backtest_example.py
@@ -14,6 +14,7 @@ from datetime import datetime, timezone
 
 from inv_trader.model.enums import Resolution
 from inv_trader.backtest.engine import BacktestConfig, BacktestEngine
+from examples.ema_cross import EMACrossPy
 from test_kit.strategies import EMACross
 from test_kit.data import TestDataProvider
 from test_kit.stubs import TestStubs
@@ -54,7 +55,48 @@ if __name__ == "__main__":
         config=config)
 
     start = datetime(2013, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc)
-    stop = datetime(2013, 3, 1, 0, 0, 0, 0, tzinfo=timezone.utc)
+    stop = datetime(2013, 6, 1, 0, 0, 0, 0, tzinfo=timezone.utc)
+
+    engine.run(start, stop)
+    #engine.create_full_tear_sheet()
+
+    input("Press Enter to continue...")
+
+
+if __name__ == "__main__":
+    usdjpy = TestStubs.instrument_usdjpy()
+    bid_data_1min = TestDataProvider.usdjpy_1min_bid()
+    ask_data_1min = TestDataProvider.usdjpy_1min_ask()
+
+    instruments = [TestStubs.instrument_usdjpy()]
+    tick_data = {usdjpy.symbol: pd.DataFrame()}
+    bid_data = {usdjpy.symbol: {Resolution.MINUTE: bid_data_1min}}
+    ask_data = {usdjpy.symbol: {Resolution.MINUTE: ask_data_1min}}
+
+    strategies = [EMACrossPy(
+        instrument=usdjpy,
+        bar_type=TestStubs.bartype_usdjpy_1min_bid(),
+        risk_bp=10,
+        fast_ema=10,
+        slow_ema=20,
+        atr_period=20,
+        sl_atr_multiple=2.0)]
+
+    config = BacktestConfig(
+        leverage=50,
+        slippage_ticks=1,
+        level_console=logging.INFO,
+        log_to_file=False)
+    engine = BacktestEngine(
+        instruments=instruments,
+        data_ticks=tick_data,
+        data_bars_bid=bid_data,
+        data_bars_ask=ask_data,
+        strategies=strategies,
+        config=config)
+
+    start = datetime(2013, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc)
+    stop = datetime(2013, 6, 1, 0, 0, 0, 0, tzinfo=timezone.utc)
 
     engine.run(start, stop)
     #engine.create_full_tear_sheet()
--- examples/ema_cross.py
+++ examples/ema_cross.py
@@ -1,173 +1,47 @@
 #!/usr/bin/env python3
 # -------------------------------------------------------------------------------------------------
-# <copyright file="strategies.pyx" company="Invariance Pte">
+# <copyright file="ema_cross.py" company="Invariance Pte">
 #  Copyright (C) 2018-2019 Invariance Pte. All rights reserved.
 #  The use of this source code is governed by the license as found in the LICENSE.md file.
 #  http://www.invariance.com
 # </copyright>
 # -------------------------------------------------------------------------------------------------
 
-# cython: language_level=3, boundscheck=False
-
 from decimal import Decimal
 from datetime import timedelta
 
-from inv_trader.common.clock cimport Clock, TestClock
-from inv_trader.common.logger cimport Logger
-from inv_trader.enums.order_side cimport OrderSide
-from inv_trader.enums.time_in_force cimport TimeInForce
-from inv_trader.model.objects cimport Quantity, Symbol, Price, Tick, BarType, Bar, Instrument
-from inv_trader.model.events cimport Event
-from inv_trader.model.identifiers cimport Label, PositionId
-from inv_trader.model.order cimport Order, AtomicOrder
-from inv_trader.model.events cimport OrderFilled, OrderExpired, OrderRejected
-from inv_trader.strategy cimport TradeStrategy
-from inv_trader.portfolio.sizing cimport PositionSizer, FixedRiskSizer
+from inv_trader.common.logger import Logger
+from inv_trader.enums.order_side import OrderSide
+from inv_trader.enums.time_in_force import TimeInForce
+from inv_trader.model.objects import Price, Tick, BarType, Bar, Instrument
+from inv_trader.model.events import Event
+from inv_trader.model.identifiers import Label
+from inv_trader.model.events import OrderFilled, OrderExpired, OrderRejected
+from inv_trader.strategy import TradeStrategy
+from inv_trader.portfolio.sizing import FixedRiskSizer
 from inv_indicators.average.ema import ExponentialMovingAverage
 from inv_indicators.atr import AverageTrueRange
-from test_kit.objects cimport ObjectStorer
 
 
-cdef class EmptyStrategy(TradeStrategy):
-    """
-    A strategy which is empty and does nothing.
-    """
-    cpdef void on_start(self):
-        pass
-
-    cpdef void on_tick(self, Tick tick):
-        pass
-
-    cpdef void on_bar(self, BarType bar_type, Bar bar):
-        pass
-
-    cpdef void on_event(self, Event event):
-        pass
-
-    cpdef void on_stop(self):
-        pass
-
-    cpdef void on_reset(self):
-        pass
-
-    cpdef void on_dispose(self):
-        pass
-
-
-cdef class TestStrategy1(TradeStrategy):
-    """"
-    A simple strategy for unit testing.
-    """
-    cdef readonly ObjectStorer object_storer
-    cdef readonly BarType bar_type
-    cdef readonly object ema1
-    cdef readonly object ema2
-    cdef readonly PositionId position_id
-
-    def __init__(self, bar_type: BarType, clock: Clock=TestClock()):
-        """
-        Initializes a new instance of the TestStrategy1 class.
-        """
-        super().__init__(clock=clock)
-        self.object_storer = ObjectStorer()
-        self.bar_type = bar_type
-
-        self.ema1 = ExponentialMovingAverage(10)
-        self.ema2 = ExponentialMovingAverage(20)
-
-        self.register_indicator(bar_type=self.bar_type,
-                                indicator=self.ema1,
-                                update_method=self.ema1.update)
-        self.register_indicator(bar_type=self.bar_type,
-                                indicator=self.ema2,
-                                update_method=self.ema2.update)
-
-        self.position_id = None
-
-    cpdef void on_start(self):
-        self.object_storer.store('custom start logic')
-
-    cpdef void on_tick(self, Tick tick):
-        self.object_storer.store(tick)
-
-    cpdef void on_bar(self, BarType bar_type, Bar bar):
-
-        self.object_storer.store((bar_type, Bar))
-
-        if bar_type == self.bar_type:
-            if self.ema1.value > self.ema2.value:
-                buy_order = self.order_factory.market(
-                    self.bar_type.symbol,
-                    Label('TestStrategy1_E'),
-                    OrderSide.BUY,
-                    100000)
-
-                self.submit_order(buy_order, PositionId(str(buy_order.id)))
-                self.position_id = buy_order.id
-
-            elif self.ema1.value < self.ema2.value:
-                sell_order = self.order_factory.market(
-                    self.bar_type.symbol,
-                    Label('TestStrategy1_E'),
-                    OrderSide.SELL,
-                    100000)
-
-                self.submit_order(sell_order, PositionId(str(sell_order.id)))
-                self.position_id = sell_order.id
-
-    cpdef void on_event(self, Event event):
-        self.object_storer.store(event)
-
-    cpdef void on_stop(self):
-        self.object_storer.store('custom stop logic')
-
-    cpdef void on_reset(self):
-        self.object_storer.store('custom reset logic')
-
-    cpdef void on_dispose(self):
-        self.object_storer.store('custom dispose logic')
-
-
-cdef class EMACross(TradeStrategy):
+class EMACrossPy(TradeStrategy):
     """"
     A simple moving average cross example strategy. When the fast EMA crosses
     the slow EMA then a STOP_MARKET atomic order is placed for that direction
     with a trailing stop.
     """
-    cdef readonly Instrument instrument
-    cdef readonly Symbol symbol
-    cdef readonly BarType bar_type
-    cdef readonly PositionSizer position_sizer
-    cdef readonly int risk_bp
-    cdef readonly int tick_precision
-    cdef readonly object entry_buffer
-    cdef readonly float SL_atr_multiple
-    cdef readonly object SL_buffer
-    cdef readonly object fast_ema
-    cdef readonly object slow_ema
-    cdef readonly object atr
-    cdef readonly Order entry_order
-    cdef readonly Order stop_loss_order
-    cdef readonly PositionId position_id
 
     def __init__(self,
-                 str label,
-                 str id_tag_trader,
-                 str id_tag_strategy,
-                 Instrument instrument,
-                 BarType bar_type,
-                 int risk_bp=10,
-                 int fast_ema=10,
-                 int slow_ema=20,
-                 int atr_period=20,
-                 float sl_atr_multiple=2,
-                 Logger logger=None):
+                 instrument: Instrument,
+                 bar_type: BarType,
+                 risk_bp: int=10,
+                 fast_ema: int=10,
+                 slow_ema: int=20,
+                 atr_period: int=20,
+                 sl_atr_multiple: float=2,
+                 logger: Logger=None):
         """
         Initializes a new instance of the EMACross class.
 
-        :param label: The optional unique label for the strategy.
-        :param id_tag_trader: The unique order identifier tag for the trader.
-        :param id_tag_strategy: The unique order identifier tag for the strategy.
         :param bar_type: The bar type for the strategy (could also input any number of them)
         :param risk_bp: The risk per trade in basis points.
         :param fast_ema: The fast EMA period.
@@ -176,10 +50,7 @@ cdef class EMACross(TradeStrategy):
         :param sl_atr_multiple: The ATR multiple for stop-loss prices.
         :param logger: The logger for the strategy (can be None, will just print).
         """
-        super().__init__(label,
-                         id_tag_trader=id_tag_trader,
-                         id_tag_strategy=id_tag_strategy,
-                         logger=logger)
+        super().__init__(logger=logger)
 
         self.instrument = instrument
         self.symbol = instrument.symbol
@@ -206,7 +77,7 @@ cdef class EMACross(TradeStrategy):
         self.stop_loss_order = None
         self.position_id = None
 
-    cpdef void on_start(self):
+    def on_start(self):
         """
         This method is called when self.start() is called, and after internal
         start logic.
@@ -215,7 +86,7 @@ cdef class EMACross(TradeStrategy):
         self.subscribe_bars(self.bar_type)
         self.subscribe_ticks(self.symbol)
 
-    cpdef void on_tick(self, Tick tick):
+    def on_tick(self, tick: Tick):
         """
         This method is called whenever a Tick is received by the strategy, after
         the Tick has been processed by the base class (update last received Tick
@@ -226,7 +97,7 @@ cdef class EMACross(TradeStrategy):
         """
         self.log.info(f"Received Tick({tick})")  # For demonstration purposes
 
-    cpdef void on_bar(self, BarType bar_type, Bar bar):
+    def on_bar(self, bar_type: BarType, bar: Bar):
         """
         This method is called whenever the strategy receives a Bar, after the
         Bar has been processed by the base class (update indicators etc).
@@ -238,11 +109,6 @@ cdef class EMACross(TradeStrategy):
         if not self.fast_ema.initialized or not self.slow_ema.initialized:
             # Wait for indicators to warm up...
             return
-
-        cdef Price entry_price
-        cdef Price stop_loss_price
-        cdef Quantity position_size
-        cdef AtomicOrder atomic_order
 
         # TODO: Factor in spread, using bid bars only at the moment
         if self.entry_order is None:
@@ -310,7 +176,6 @@ cdef class EMACross(TradeStrategy):
             self.submit_atomic_order(atomic_order, self.position_id)
 
         # TRAILING STOP LOGIC
-        cdef Price temp_price
         if self._is_stop_loss_active():
             if self.stop_loss_order.side is OrderSide.SELL:
                 temp_price = Price(self.last_bar(self.bar_type).low - (self.atr.value * self.SL_atr_multiple))
@@ -321,7 +186,7 @@ cdef class EMACross(TradeStrategy):
                 if self.stop_loss_order.price > temp_price:
                     self.modify_order(self.stop_loss_order, temp_price)
 
-    cpdef void on_event(self, Event event):
+    def on_event(self, event: Event):
         """
         This method is called whenever the strategy receives an Event object,
         after the event has been processed by the base class (updating any objects it needs to).
@@ -336,12 +201,12 @@ cdef class EMACross(TradeStrategy):
         elif isinstance(event, OrderRejected) or isinstance(event, OrderExpired):
             if event.order_id.equals(self.entry_order.id):
                 self._reset_trade()
-            # If a stop-loss order is rejected then flatten the entered position
-            elif event.order_id.equals(self.stop_loss_order.id):
-                self.flatten_all_positions()
-                self._reset_trade()
+        # If a stop-loss order is rejected then flatten the entered position
+        elif event.order_id.equals(self.stop_loss_order.id):
+            self.flatten_all_positions()
+            self._reset_trade()
 
-    cpdef void on_stop(self):
+    def on_stop(self):
         """
         This method is called when self.stop() is called before internal
         stopping logic.
@@ -351,7 +216,7 @@ cdef class EMACross(TradeStrategy):
 
         self.cancel_all_orders("STOPPING STRATEGY")
 
-    cpdef void on_reset(self):
+    def on_reset(self):
         """
         This method is called when self.reset() is called, and after internal
         reset logic such as clearing the internally held bars, ticks and resetting
@@ -361,15 +226,15 @@ cdef class EMACross(TradeStrategy):
         """
         self._reset_trade()
 
-    cpdef void on_dispose(self):
+    def on_dispose(self):
         """
-        This method is called when self.dispose() is called. Dispose of any resources
-        that had been used by the strategy here.
+        This method is called when self.dispose() is called. Dispose of any
+        resources that had been used by the strategy here.
         """
         self.unsubscribe_bars(self.bar_type)
         self.unsubscribe_ticks(self.symbol)
 
-    cdef void _reset_trade(self):
+    def _reset_trade(self):
         """
         Reset the trade by clearing all order and position values.
         """
@@ -377,10 +242,10 @@ cdef class EMACross(TradeStrategy):
         self.stop_loss_order = None
         self.position_id = None
 
-    cdef bint _is_stop_loss_active(self):
+    def _is_stop_loss_active(self):
         """
         Return a value indicating whether the stop-loss is active.
-        
+
         :return: True if active, else False.
         """
         return (self.stop_loss_order is not None
