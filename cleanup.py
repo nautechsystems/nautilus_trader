22d52727d0c9fea14130d24964ac359e5dd47795
cjdsellers <cjdsellers@protonmail.com>
2019-07-28 08:40:37 +1000

    Change company url.

--- cleanup.py
+++ cleanup.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 # -------------------------------------------------------------------------------------------------
 # <copyright file="cleanup.py" company="Nautech Systems Pty Ltd">
 #  Copyright (C) 2015-2019 Nautech Systems Pty Ltd. All rights reserved.
 #  The use of this source code is governed by the license as found in the LICENSE.md file.
-#  http://www.nautechsystems.io
+#  https://nautechsystems.io
 # </copyright>
 # -------------------------------------------------------------------------------------------------
 
