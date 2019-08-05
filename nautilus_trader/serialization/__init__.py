b09d2d4b8039bed66aef5185b22a95d529a568d0
Christopher Sellers <cjdsellers@protonmail.com>
2019-08-05 16:58:19 +1000

    Remove import.

--- nautilus_trader/serialization/__init__.py
+++ nautilus_trader/serialization/__init__.py
@@ -5,5 +5,3 @@
 #  https://nautechsystems.io
 # </copyright>
 # -------------------------------------------------------------------------------------------------
-
-from nautilus_trader.serialization import common
