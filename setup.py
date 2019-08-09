0310d6e4b5e58c75b8a9c305005911a1e456c835
cjdsellers <cjdsellers@protonmail.com>
2019-08-09 10:44:44 +1000

    Improve setup.

--- setup.py
+++ setup.py
@@ -92,13 +92,6 @@ modules = (get_directories(PACKAGE_NAME))
 package_data = {PACKAGE_NAME: [definition_ext]}
 for module in modules:
     package_data[f'{PACKAGE_NAME}/{module}'] = [definition_ext]
-
-
-definition_ext = '*.pxd'
-modules = (get_directories(PACKAGE_NAME))
-package_data = {PACKAGE_NAME: [definition_ext]}
-for module in modules:
-    package_data[f'{PACKAGE_NAME}/{module}'] = [definition_ext]
 print(f"Including package data; {package_data}")
 
 
