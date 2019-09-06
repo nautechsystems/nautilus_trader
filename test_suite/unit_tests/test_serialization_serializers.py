8258a3f7c105c4ec6b9828cde7ef7bf444c01bf8
cjdsellers <cjdsellers@protonmail.com>
2019-09-06 12:50:59 +1000

    Fix tests.

--- test_suite/unit_tests/test_serialization_serializers.py
+++ test_suite/unit_tests/test_serialization_serializers.py
@@ -12,6 +12,7 @@ from base64 import b64encode, b64decode
 
 from nautilus_trader.common.clock import *
 from nautilus_trader.model.commands import *
+from nautilus_trader.model.enums import *
 from nautilus_trader.model.identifiers import *
 from nautilus_trader.model.objects import *
 from nautilus_trader.model.order import *
