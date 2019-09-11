aa650ad5d5c1148aeb6946cbec914da1f91c2085
cjdsellers <cjdsellers@protonmail.com>
2019-09-11 12:08:40 +1000

    Change example strategy.

--- examples/live/live_example_fxcm.py
+++ examples/live/live_example_fxcm.py
@@ -15,14 +15,14 @@ from nautilus_trader.live.node import TradingNode
 from examples.strategies.ema_cross import EMACrossPy
 
 
-#BAR_SPEC = BarSpecification(1, Resolution.MINUTE, QuoteType.BID)
-BAR_SPEC = BarSpecification(1, Resolution.SECOND, QuoteType.BID)
-
-
 # Requirements to run;
 #   - A Redis instance listening on the default port 6379
 #   - A NautilusData instance listening on the default ports
 #   - A NautilusExecutor instance listening on the default ports
+
+
+# BAR_SPEC = BarSpecification(1, Resolution.MINUTE, QuoteType.BID)
+BAR_SPEC = BarSpecification(1, Resolution.SECOND, QuoteType.BID)
 
 symbols_to_trade = [
     Symbol('AUDUSD', Venue('FXCM')),
