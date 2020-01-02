21fd4b2babfb41facbe5a4ce712f4ce1b25f5d45
Bshara Zahran <bshara23@gmail.com>
2020-01-02 17:05:38 +1100

    Refactor clocks.

--- nautilus_trader/common/logger.pyx
+++ nautilus_trader/common/logger.pyx
@@ -387,7 +387,7 @@ cpdef void nautilus_header(LoggerAdapter logger):
         logger.info("#---------------------------------------------------------------#")
         logger.info(f" Nautilus Trader - Algorithmic Trading Platform")
         logger.info(f" by Nautech Systems Pty Ltd. ")
-        logger.info(f" Copyright (C) 2015-2019. All rights reserved.")
+        logger.info(f" Copyright (C) 2015-2020. All rights reserved.")
         logger.info("#---------------------------------------------------------------#")
         logger.info("                                                                 ")
         logger.info("                            .......                              ")
