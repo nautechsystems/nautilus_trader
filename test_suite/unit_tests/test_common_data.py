7388ddb4a9b3f383e3566418d1e8bdc026aaa939
cjdsellers <cjdsellers@protonmail.com>
2020-01-02 16:59:40 +1100

    Refactor clocks.

--- test_suite/unit_tests/test_common_data.py
+++ test_suite/unit_tests/test_common_data.py
@@ -10,8 +10,9 @@ import unittest
 
 from datetime import datetime, timedelta, timezone
 
+from nautilus_trader.common.clock import LiveClock, TestClock
 from nautilus_trader.common.logger import TestLogger
-from nautilus_trader.common.data import TickBarAggregator, TimeBarTimedAggregator
+from nautilus_trader.common.data import TickBarAggregator, TimeBarAggregator
 from nautilus_trader.model.enums import BarStructure, QuoteType
 from nautilus_trader.model.objects import Price, Tick, BarSpecification, BarType
 
@@ -73,7 +74,7 @@ class TimeBarAggregatorTests(unittest.TestCase):
         symbol = TestStubs.symbol_audusd_fxcm()
         bar_spec = BarSpecification(1, BarStructure.MINUTE, QuoteType.MID)
         bar_type = BarType(symbol, bar_spec)
-        aggregator = TimeBarTimedAggregator(bar_type, handler, TestLogger())
+        aggregator = TimeBarAggregator(bar_type, handler, LiveClock(), TestLogger())
 
         stop_time = UNIX_EPOCH + timedelta(minutes=2)
 
