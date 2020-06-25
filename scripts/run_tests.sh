2bb294db742d1f111c37b587f110b83a93c0a049
Bshara Zahran <bshara23@gmail.com>
2020-06-25 16:26:09 +1000

    Update buildspec

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-pytest tests/unit_tests/
-pytest tests/integration_tests/
-pytest tests/performance_tests/
-pytest tests/performance_tests/
-pytest tests/acceptance_tests/
+pytest ../tests/unit_tests/
+pytest ../tests/integration_tests/
+pytest ../tests/performance_tests/
+pytest ../tests/acceptance_tests/
