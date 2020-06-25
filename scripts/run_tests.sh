e27867cfe53c36a37a8886bb93c23c8c75bac351
cjdsellers <chris@cjdsellers.io>
2020-06-25 17:16:39 +1000

    Update buildspec

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python -m pytest ../tests/unit_tests/
-python -m pytest ../tests/integration_tests/
-python -m pytest ../tests/performance_tests/
-python -m pytest ../tests/acceptance_tests/
+python -m pytest tests/unit_tests/
+python -m pytest tests/integration_tests/
+python -m pytest tests/performance_tests/
+python -m pytest tests/acceptance_tests/
