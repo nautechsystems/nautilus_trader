24b63670ebf99db9fb0c907b2d0f6d9f06ff7439
Bshara Zahran <bshara23@gmail.com>
2020-07-18 18:46:37 +1000

    Add code coverage
    - Bump version as release

--- nautilus_trader/__init__.py
+++ nautilus_trader/__init__.py
@@ -25,8 +25,8 @@ __author__ = 'Nautech Systems'
 # Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = 1
 _MINOR_VERSION = 37
-_PATCH_VERSION = 6
-_PRE_RELEASE = 'b1'
+_PATCH_VERSION = 7
+_PRE_RELEASE = ''
 
 __version__ = '.'.join([
     str(_MAJOR_VERSION),
