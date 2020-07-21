cc3e068d1018c6dfe6423820263e6110a610ec2e
Bshara Zahran <bshara23@gmail.com>
2020-07-21 15:37:00 +1000

    Fix package installation issue
    - Needs pyzmq>=19.0.1 to be able to compile C headers
    - Bump version as release

--- nautilus_trader/__init__.py
+++ nautilus_trader/__init__.py
@@ -24,8 +24,8 @@ __author__ = 'Nautech Systems'
 
 # Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = 1
-_MINOR_VERSION = 37
-_PATCH_VERSION = 14
+_MINOR_VERSION = 38
+_PATCH_VERSION = 1
 _PRE_RELEASE = ''
 
 __version__ = '.'.join([
