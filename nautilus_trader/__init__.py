3173438a9d1d19f8e387c04fd7d95f7d8be423e5
cjdsellers <chris@cjdsellers.io>
2020-08-20 11:03:24 +1000

    Fix call to news event currency
    - Bump version

--- nautilus_trader/__init__.py
+++ nautilus_trader/__init__.py
@@ -25,7 +25,7 @@ __author__ = "Nautech Systems"
 # Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = 1
 _MINOR_VERSION = 46
-_PATCH_VERSION = 1
+_PATCH_VERSION = 2
 _PRE_RELEASE = ''
 
 __version__ = '.'.join([
