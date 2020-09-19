d8f1444c14ce33cd53638e934753f86c5b85ca75
Bshara Zahran <bshara23@gmail.com>
2020-09-20 09:54:23 +1000

    Move back to beta and add documentation

---
+++ docs/development/coding_standards.md
@@ -0,0 +1,10 @@
+# Coding Standards
+
+*** This documentation is currently a work in progress ***
+
+- PEP-8 variations
+- numpydocs
+- flake8 usage and ignores
+- pre-commit hook setup and usage
+- isort usage
+- darglint usage
--- nautilus_trader/__init__.py
+++ nautilus_trader/__init__.py
@@ -24,9 +24,9 @@ __author__ = "Nautech Systems"
 
 # Semantic Versioning (https://semver.org/)
 _MAJOR_VERSION = 1
-_MINOR_VERSION = 59
+_MINOR_VERSION = 60
 _PATCH_VERSION = 0
-_PRE_RELEASE = 'b3'
+_PRE_RELEASE = 'b0'
 
 __version__ = '.'.join([
     str(_MAJOR_VERSION),
