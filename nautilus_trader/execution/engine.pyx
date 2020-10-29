5cc422cc8899710bb91840bf71e47b15a70e6333
cjdsellers <chris@cjdsellers.io>
2020-10-29 20:45:27 +1100

    Remove redundant ValidString
    - Consolidate identifier base class
    - Remove redundant curve key generation
    - Update docs

--- nautilus_trader/execution/engine.pyx
+++ nautilus_trader/execution/engine.pyx
@@ -17,11 +17,15 @@
 The `ExecutionEngine` is the central component of the entire execution stack for the platform.
 
 Its primary responsibility is to orchestrate interactions between the individual
-`DataClient` instances, and the rest of the platform. This is could include
+`DataClient` instances, and the rest of the platform. This includes
 ongoing subscriptions to specific data types, for particular endpoints.
 
 Beneath it sits the `DataCache` layer which presents a read-only facade
 to its clients to consume cached data through.
+
+The engine employs a simple fan-in fan-out messaging pattern to receive events
+from the `ExecutionClient` instances, and sending those to the registered
+handlers, namely `TradingStrategy` instances.
 """
 
 from nautilus_trader.common.c_enums.component_trigger cimport ComponentTrigger
