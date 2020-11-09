a8b0aed1ad90ba15acdb6e7688d2aac213876df8
Bshara Zahran <bshara23@gmail.com>
2020-11-10 08:23:25 +1100

    Add unit tests

--- tests/unit_tests/execution/test_client.py
+++ tests/unit_tests/execution/test_client.py
@@ -18,14 +18,20 @@ import unittest
 from nautilus_trader.backtest.loaders import InstrumentLoader
 from nautilus_trader.backtest.logging import TestLogger
 from nautilus_trader.common.clock import TestClock
+from nautilus_trader.common.factories import OrderFactory
 from nautilus_trader.common.uuid import UUIDFactory
 from nautilus_trader.data.cache import DataCache
 from nautilus_trader.execution.client import ExecutionClient
 from nautilus_trader.execution.database import BypassExecutionDatabase
 from nautilus_trader.execution.engine import ExecutionEngine
+from nautilus_trader.model.enums import OrderSide
+from nautilus_trader.model.identifiers import PositionId
+from nautilus_trader.model.identifiers import StrategyId
 from nautilus_trader.model.identifiers import Symbol
 from nautilus_trader.model.identifiers import TraderId
 from nautilus_trader.model.identifiers import Venue
+from nautilus_trader.model.objects import Price
+from nautilus_trader.model.objects import Quantity
 from nautilus_trader.trading.portfolio import Portfolio
 from tests.test_kit.stubs import TestStubs
 
@@ -54,7 +60,7 @@ class ExecutionClientTests(unittest.TestCase):
         portfolio.register_cache(DataCache(self.logger))
 
         database = BypassExecutionDatabase(trader_id=self.trader_id, logger=self.logger)
-        exec_engine = ExecutionEngine(
+        self.exec_engine = ExecutionEngine(
             database=database,
             portfolio=portfolio,
             clock=self.clock,
@@ -62,13 +68,55 @@ class ExecutionClientTests(unittest.TestCase):
             logger=self.logger,
         )
 
-        self.venue = Venue("TDA-NYSE")
+        self.venue = Venue("FXCM")
 
         self.client = ExecutionClient(
             venue=self.venue,
             account_id=self.account_id,
-            exec_engine=exec_engine,
+            engine=self.exec_engine,
             clock=self.clock,
             uuid_factory=self.uuid_factory,
             logger=self.logger,
         )
+
+        self.order_factory = OrderFactory(
+            trader_id=TraderId("TESTER", "000"),
+            strategy_id=StrategyId("S", "001"),
+            clock=TestClock(),
+        )
+
+    def test_connect_when_not_implemented_raises_exception(self):
+        self.assertRaises(NotImplementedError, self.client.connect)
+
+    def test_disconnect_when_not_implemented_raises_exception(self):
+        self.assertRaises(NotImplementedError, self.client.disconnect)
+
+    def test_reset_when_not_implemented_raises_exception(self):
+        self.assertRaises(NotImplementedError, self.client.reset)
+
+    def test_dispose_when_not_implemented_raises_exception(self):
+        self.assertRaises(NotImplementedError, self.client.dispose)
+
+    def test_is_connected_when_not_implemented_raises_exception(self):
+        self.assertRaises(NotImplementedError, self.client.is_connected)
+
+    def test_handle_event_sends_to_execution_engine(self):
+        # Arrange
+        order = self.order_factory.market(
+            AUDUSD_FXCM.symbol,
+            OrderSide.BUY,
+            Quantity(100000),
+        )
+
+        fill = TestStubs.event_order_filled(
+            order,
+            PositionId("P-123456"),
+            StrategyId("S", "001"),
+            Price("1.00001"),
+        )
+
+        # Act
+        self.client._handle_event(fill)  # Accessing protected method
+
+        # Assert
+        self.assertEqual(1, self.exec_engine.event_count)
