f50639bd7bce4aec4a9cdf09533949e3adf22937
cjdsellers <chris@cjdsellers.io>
2020-12-08 06:13:52 +1100

    Fix position flip bug
    - Add unit tests

---
+++ tests/unit_tests/indicators/test_ma_factory.py
@@ -0,0 +1,63 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2020 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
+import unittest
+
+from nautilus_trader.backtest.loaders import InstrumentLoader
+from nautilus_trader.indicators.average.ema import ExponentialMovingAverage
+from nautilus_trader.indicators.average.hma import HullMovingAverage
+from nautilus_trader.indicators.average.ma_factory import MovingAverageFactory
+from nautilus_trader.indicators.average.moving_average import MovingAverageType
+from nautilus_trader.indicators.average.sma import SimpleMovingAverage
+from nautilus_trader.indicators.average.wma import WeightedMovingAverage
+from tests.test_kit.stubs import TestStubs
+
+
+AUDUSD_FXCM = InstrumentLoader.default_fx_ccy(TestStubs.symbol_audusd_fxcm())
+
+
+class MovingAverageConvergenceDivergenceTests(unittest.TestCase):
+
+    def test_simple_returns_expected_indicator(self):
+        # Arrange
+        # Act
+        indicator = MovingAverageFactory.create(10, MovingAverageType.SIMPLE)
+
+        # Assert
+        self.assertTrue(isinstance(indicator, SimpleMovingAverage))
+
+    def test_exponential_returns_expected_indicator(self):
+        # Arrange
+        # Act
+        indicator = MovingAverageFactory.create(10, MovingAverageType.EXPONENTIAL)
+
+        # Assert
+        self.assertTrue(isinstance(indicator, ExponentialMovingAverage))
+
+    def test_hull_returns_expected_indicator(self):
+        # Arrange
+        # Act
+        indicator = MovingAverageFactory.create(10, MovingAverageType.HULL)
+
+        # Assert
+        self.assertTrue(isinstance(indicator, HullMovingAverage))
+
+    def test_weighted_returns_expected_indicator(self):
+        # Arrange
+        # Act
+        indicator = MovingAverageFactory.create(10, MovingAverageType.WEIGHTED)
+
+        # Assert
+        self.assertTrue(isinstance(indicator, WeightedMovingAverage))
