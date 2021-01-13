f7eb329edb01f743085119560b62af81c4ff4e57
Stanislav <jp.arbitrage@gmail.com>
2021-01-13 18:01:55 +0100

    Update + CachedProducer

--- tests/acceptance_tests/test_backtest_acceptance.py
+++ tests/acceptance_tests/test_backtest_acceptance.py
@@ -13,7 +13,6 @@
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
 
-from decimal import Decimal
 import os
 import sys
 import unittest
