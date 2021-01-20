3f93b13468d19f2544d1f2e2925249c311b5745d
Bshara Zahran <bshara23@gmail.com>
2021-01-21 08:15:08 +1100

    Refactor

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -41,9 +41,9 @@ DIRS_TO_REMOVE = {
 }
 
 DIRS_TO_CLEAN = {
-    "/nautilus_trader/"
-    "/examples/"
-    "/tests/"
+    "/nautilus_trader/",
+    "/examples/",
+    "/tests/",
 }
 
 EXTENSIONS_TO_CLEAN = (
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     # Walk directories to clean and remove files by extension
     removed_count = 0
     for directory in DIRS_TO_CLEAN:
-        for root, _dirs, files in os.walk(root_dir + directory):
+        for root, _dirs, files in os.walk(root_dir + "/nautilus_trader/"):
             for name in files:
                 path = os.path.join(root, name)
                 if os.path.isfile(path) and path.endswith(EXTENSIONS_TO_CLEAN):
