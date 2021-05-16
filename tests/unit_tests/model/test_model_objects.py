5d30cb620a24574245e955e8a56d96939a2ff0d7
cjdsellers <chris@cjdsellers.io>
2021-05-16 19:06:41 +1000

    Fix codacy issues

--- tests/unit_tests/model/test_model_objects.py
+++ tests/unit_tests/model/test_model_objects.py
@@ -322,7 +322,7 @@ class TestBaseDecimal:
         result = value1 + value2
 
         # Assert
-        assert type(result) == expected_type
+        assert isinstance(result, expected_type)
         assert result == expected_value
 
     @pytest.mark.parametrize(
