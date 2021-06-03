e020375f33075da527679fabebde405a618c81a0
cjdsellers <chris@cjdsellers.io>
2021-06-03 18:45:58 +1000

    Rename `Position.relative_qty` to `Position.net_qty`

--- nautilus_trader/trading/portfolio.pyx
+++ nautilus_trader/trading/portfolio.pyx
@@ -661,8 +661,8 @@ cdef class Portfolio(PortfolioFacade):
 
     cpdef object net_position(self, InstrumentId instrument_id):
         """
-        Return the net relative position for the given instrument identifier. If no positions
-        for instrument_id then will return `Decimal('0')`.
+        Return the total net position for the given instrument identifier.
+        If no positions for instrument_id then will return `Decimal('0')`.
 
         Parameters
         ----------
@@ -760,7 +760,7 @@ cdef class Portfolio(PortfolioFacade):
     cdef void _update_net_position(self, InstrumentId instrument_id, list positions_open) except *:
         net_position = Decimal()
         for position in positions_open:
-            net_position += position.relative_qty
+            net_position += position.net_qty
 
         self._net_positions[instrument_id] = net_position
         self._log.info(f"{instrument_id} net_position={net_position}")
