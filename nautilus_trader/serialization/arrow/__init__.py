6119400fbf3513be7f7a28b21a37d5975ecf3e82
cjdsellers <chris@cjdsellers.io>
2021-06-16 05:55:23 +1000

    Reorganize
    - Rename to `serializers.py`.
    - Add subpackage inits.
    - Add headers.

--- nautilus_trader/serialization/arrow/__init__.py
+++ nautilus_trader/serialization/arrow/__init__.py
@@ -12,20 +12,3 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
-
-
-def list_dicts_to_dict_lists(dicts):
-    result = {}
-    for d in dicts:
-        for k, v in d.items():
-            if k not in result:
-                result[k] = [v]
-            else:
-                result[k].append(v)
-    return result
-
-
-def maybe_list(dict_or_list):
-    if isinstance(dict_or_list, dict):
-        return [dict_or_list]
-    return dict_or_list
