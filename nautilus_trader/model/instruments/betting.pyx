f9fd1b301b530f6d170a9a5c0ce5d8f8f1f6a161
Bshara Zahran <bshara23@gmail.com>
2021-06-20 18:48:23 +1000

    Add blank line

--- nautilus_trader/model/instruments/betting.pyx
+++ nautilus_trader/model/instruments/betting.pyx
@@ -12,6 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
+
 import pandas as pd
 from libc.stdint cimport int64_t
 
