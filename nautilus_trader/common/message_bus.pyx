2b08ef88c6089cecf76d8c8c56ef72f21d438e81
Bshara Zahran <bshara23@gmail.com>
2021-07-11 08:23:02 +1000

    Refactor `MessageBus`

--- nautilus_trader/common/message_bus.pyx
+++ nautilus_trader/common/message_bus.pyx
@@ -15,31 +15,32 @@
 
 from typing import Callable
 
+import cython
+import numpy as np
+
 from nautilus_trader.common.clock cimport Clock
 from nautilus_trader.common.logging cimport Logger
-from nautilus_trader.common.uuid cimport UUIDFactory
 from nautilus_trader.core.correctness cimport Condition
 from nautilus_trader.core.message cimport Message
-from nautilus_trader.core.type cimport MessageType
 
 
 cdef class Subscription:
     """
-    Represents a subscription to a particular message type.
+    Represents a subscription to a particular topic.
 
     This is an internal class intended to be used by the message bus to
     organize channels and their subscribers.
 
     Notes
     -----
-    The subscription equality is determined by the msg_type and handler,
+    The subscription equality is determined by the topic and handler,
     priority is not considered (and could change).
 
     """
 
     def __init__(
         self,
-        MessageType msg_type,
+        str topic,
         handler not None: Callable[[Message], None],
         int priority=0,
     ):
@@ -48,7 +49,7 @@ cdef class Subscription:
 
         Parameters
         ----------
-        msg_type : MessageType, optional
+        topic : str, optional
             The message type for the subscription.
             If None then represents a subscription for ALL messages.
         handler : Callable[[Message], None]
@@ -57,12 +58,13 @@ cdef class Subscription:
             The priority for the subscription.
 
         """
-        self.msg_type = msg_type
+        self._topic_str = topic
+        self.topic = topic.replace("*", "")
         self.handler = handler
         self.priority = priority
 
     def __eq__(self, Subscription other) -> bool:
-        return self.msg_type == other.msg_type and self.handler == other.handler
+        return self.topic == other.topic and self.handler == other.handler
 
     def __lt__(self, Subscription other) -> bool:
         return self.priority < other.priority
@@ -77,11 +79,11 @@ cdef class Subscription:
         return self.priority >= other.priority
 
     def __hash__(self) -> int:
-        return hash((self.msg_type, self.handler))
+        return hash((self.topic, self.handler))
 
     def __repr__(self) -> str:
         return (f"{type(self).__name__}("
-                f"msg_type={str(self.msg_type) if self.msg_type else '*'}, "
+                f"topic={self._topic_str}, "
                 f"handler={self.handler}, "
                 f"priority={self.priority})")
 
@@ -116,11 +118,10 @@ cdef class MessageBus:
         Condition.not_none(name, "name")
 
         self._clock = clock
-        self._uuid_factory = UUIDFactory()
         self._log = LoggerAdapter(component=name, logger=logger)
 
-        self._channels = {}     # type: dict[type, list[Subscription]]
-        self._channel_all = []  # type: list[Subscription]
+        self._channels = {}    # type: dict[str, Subscription[:]]
+        self._patterns = None  # type: Subscription[:]
 
         # Counters
         self.processed_count = 0
@@ -136,19 +137,20 @@ cdef class MessageBus:
         list[type]
 
         """
-        channels = list(self._channels.keys())
-        if self._channel_all:
-            channels.append(None)
+        cdef list channels = []
+        channels.extend(list(self._channels.keys()))
+        if self._patterns is not None:
+            channels.extend([s.topic + "*" for s in list(self._patterns)])
         return channels
 
-    cpdef list subscriptions(self, MessageType msg_type=None):
+    cpdef list subscriptions(self, str topic):
         """
         Return all subscriptions for the given message type.
 
         Parameters
         ----------
-        msg_type : MessageType
-            The message type filter.
+        topic : str
+            The topic filter.
             If None then will return subscriptions for ALL messages.
 
         Returns
@@ -156,21 +158,25 @@ cdef class MessageBus:
         list[Subscription]
 
         """
-        if msg_type is None:
-            return self._channel_all.copy()
+        Condition.valid_string(topic, "topic")
 
-        cdef list subscriptions = self._channels.get(msg_type.type, [])
+        topic = topic.replace("*", "")
+
         cdef list output = []
-        for sub in subscriptions:
-            if sub.msg_type.key.issubset(msg_type.key):
-                output.append(sub)
+        for sub in self._channels.get(topic, []):
+            output.append(sub)
+
+        if self._patterns is not None:
+            for sub in list(self._patterns):
+                if sub.topic.startswith(topic):
+                    output.append(sub)
 
         return output
 
     cpdef void subscribe(
         self,
-        MessageType msg_type,
-        handler: Callable[[Message], None],
+        str topic,
+        handler: Callable,
         int priority=0,
     ) except *:
         """
@@ -178,10 +184,9 @@ cdef class MessageBus:
 
         Parameters
         ----------
-        msg_type : MessageType, optional
-            The message type to subscribe to.
-            If None then subscribes too ALL messages.
-        handler : Callable[[Message], None]
+        topic : MessageType
+            The message type to subscribe to. If "*" then subscribes to ALL messages.
+        handler : Callable
             The handler for the subscription.
         priority : int
             The priority for the subscription. Determines the ordering of
@@ -198,105 +203,141 @@ cdef class MessageBus:
         for logically sound behaviour.
 
         """
+        Condition.valid_string(topic, "topic")
         Condition.not_none(handler, "handler")
 
         # Create subscription
         cdef Subscription sub = Subscription(
-            msg_type=msg_type,
+            topic=topic,
             handler=handler,
             priority=priority,
         )
 
-        # Get channel subscriptions
-        cdef list subscriptions
-        if msg_type is None:  # Subscribe ALL
-            subscriptions = self._channel_all
+        # Get current subscriptions for topic
+        if "*" in topic:
+            self._subscribe_pattern(sub)
         else:
-            subscriptions = self._channels.get(msg_type.type, [])
+            self._subscribe_channel(sub)
+
+    cdef void _subscribe_pattern(self, Subscription sub) except *:
+        if self._patterns is None:
+            subscriptions = []
+        else:
+            subscriptions = list(self._patterns)
 
         # Check if already exists
         if sub in subscriptions:
             self._log.warning(f"{sub} already exists.")
             return
 
-        # Add to subscriptions in priority order (highest first)
         subscriptions.append(sub)
         subscriptions = sorted(subscriptions, reverse=True)
+        self._patterns = np.ascontiguousarray(subscriptions)
+        self._log.info(f"Added {sub}.")
 
-        # Update channel
-        if msg_type is None:  # Subscribe ALL
-            self._channel_all = subscriptions
-        else:
-            self._channels[msg_type.type] = subscriptions
+    cdef void _subscribe_channel(self, Subscription sub) except *:
+        cdef list subscriptions = list(self._channels.get(sub.topic, []))
+        # Check if already exists
+        if sub in subscriptions:
+            self._log.warning(f"{sub} already exists.")
+            return
 
-        self._log.info(f"Added {repr(sub)}.")
+        subscriptions.append(sub)
+        subscriptions = sorted(subscriptions, reverse=True)
+        self._channels[sub.topic] = np.ascontiguousarray(subscriptions)
+        self._log.info(f"Added {sub}.")
 
-    cpdef void unsubscribe(self, MessageType msg_type, handler: Callable) except *:
+    cpdef void unsubscribe(self, str topic, handler: Callable) except *:
         """
         Unsubscribe the handler from the given message type.
 
         Parameters
         ----------
-        msg_type : MessageType
-            The message type to unsubscribe from.
-            If None then unsubscribes from ALL messages.
+        topic : str, optional
+            The topic to unsubscribe from. If "*" then unsubscribes from ALL messages.
         handler : Callable
             The handler for the subscription.
 
         """
+        Condition.valid_string(topic, "topic")
         Condition.not_none(handler, "handler")
 
-        cdef Subscription sub = Subscription(msg_type=msg_type, handler=handler)
+        cdef Subscription sub = Subscription(topic=topic, handler=handler)
 
-        # Get channel subscriptions
-        cdef list subscriptions
-        if msg_type is None:  # Unsubscribe ALL
-            subscriptions = self._channel_all
+        # Get current subscriptions for topic
+        if "*" in topic:
+            self._unsubscribe_pattern(sub)
         else:
-            subscriptions = self._channels.get(msg_type.type, [])
+            self._unsubscribe_channel(sub)
+
+    cdef void _unsubscribe_pattern(self, Subscription sub) except *:
+        if self._patterns is None:
+            subscriptions = []
+        else:
+            subscriptions = list(self._patterns)
 
         # Check if exists
         if sub not in subscriptions:
             self._log.warning(f"{sub} not found.")
             return
 
-        # Remove from channel
+        subscriptions.remove(sub)
+        subscriptions = sorted(subscriptions, reverse=True)
+        if not subscriptions:
+            self._patterns = None
+        else:
+            self._patterns = np.ascontiguousarray(subscriptions)
+        self._log.debug(f"Removed {sub}.")
+
+    cdef void _unsubscribe_channel(self, Subscription sub) except *:
+        cdef list subscriptions = list(self._channels.get(sub.topic, []))
+
+        # Check if already exists
+        if sub not in subscriptions:
+            self._log.warning(f"{sub} not found.")
+            return
+
         subscriptions.remove(sub)
         self._log.debug(f"Removed {sub}.")
 
-        # Delete channel if no more handlers
-        if msg_type and not self._channels[msg_type.type]:
-            del self._channels[msg_type.type]
+        if not subscriptions:
+            del self._channels[sub.topic]
+            return
 
-    cpdef void publish(self, MessageType msg_type, message) except *:
+        subscriptions = sorted(subscriptions, reverse=True)
+        self._channels[sub.topic] = np.ascontiguousarray(subscriptions)
+
+    @cython.boundscheck(False)
+    @cython.wraparound(False)
+    cpdef void publish(self, str topic, msg) except *:
         """
         Publish the given message.
 
-        Subscriber handlers will receive the message in priority order (highest
-        first).
+        Subscription handlers will receive the message in priority order
+        (highest first).
 
         Parameters
         ----------
-        msg_type : MessageType
-            The message type to process. Determines the channel.
-        message : object
-            The message to process.
+        topic : str
+            The topic to publish on (determines the channel).
+        msg : object
+            The message to publish.
 
         """
-        Condition.not_none(msg_type, "msg_type")
-        Condition.not_none(message, "message")
+        Condition.not_none(topic, "topic")
+        Condition.not_none(msg, "msg")
 
         cdef Subscription sub
-        cdef list subscriptions = self._channels.get(msg_type.type)
-        if subscriptions:
+        cdef Subscription[:] subscriptions = self._channels.get(topic)
+        if subscriptions is not None:
             # Send to channel subscriptions
-            for sub in subscriptions:
-                if sub.msg_type.key.issubset(msg_type.key):
-                    sub.handler(message)
+            for i in range(len(subscriptions)):
+                sub = subscriptions[i]
+                sub.handler(msg)
 
-        if self._channel_all:
-            # Send to ALL subscriptions
-            for sub in self._channel_all:
-                sub.handler(message)
+        for i in range(len(self._patterns)):
+            sub = self._patterns[i]
+            if topic.__contains__(sub.topic):
+                sub.handler(msg)
 
         self.processed_count += 1
