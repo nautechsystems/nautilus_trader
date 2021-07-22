be716ad6b4ee1783ae8ecd5d9a7ab3a50ec042ba
Bshara Zahran <bshara23@gmail.com>
2021-07-22 16:21:01 +1000

    Add blank line

--- tests/unit_tests/serialization/test_serialization_serializers.py
+++ tests/unit_tests/serialization/test_serialization_serializers.py
@@ -12,6 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
+
 from base64 import b64encode
 import copy
 import sys
