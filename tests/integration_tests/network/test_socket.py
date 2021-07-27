71f3ddef057d6c8b8de223d9bde4a8cb5f8a632c
cjdsellers <chris@cjdsellers.io>
2021-07-27 16:40:44 +1000

    Add headers

--- tests/integration_tests/network/test_socket.py
+++ tests/integration_tests/network/test_socket.py
@@ -12,3 +12,31 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
+
+import pytest
+
+from nautilus_trader.common.logging import LoggerAdapter
+from nautilus_trader.network.socket import SocketClient
+
+
+@pytest.mark.asyncio
+@pytest.mark.skip  # Flaky
+async def test_socket_base(socket_server, logger, event_loop):
+    messages = []
+
+    def handler(raw):
+        messages.append(raw)
+        if len(messages) > 5:
+            client.stop()
+
+    host, port = socket_server.server_address
+    client = SocketClient(
+        host=host,
+        port=port,
+        message_handler=handler,
+        loop=event_loop,
+        logger_adapter=LoggerAdapter("Socket", logger),
+        ssl=False,
+    )
+    await client.start()
+    assert messages == [b"hello"] * 6
