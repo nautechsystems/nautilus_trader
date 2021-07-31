650d6e70b65a0d0203ddb853ac2acc633d90404a
Bshara Zahran <bshara23@gmail.com>
2021-08-01 06:05:06 +1000

    Cleanup docs

--- nautilus_trader/common/factories.pyx
+++ nautilus_trader/common/factories.pyx
@@ -147,7 +147,7 @@ cdef class OrderFactory:
         ValueError
             If quantity is not positive (> 0).
         ValueError
-            If time_in_force is other than GTC, IOC or FOK.
+            If time_in_force is other than GTC, IOC, FOK or OC.
 
         """
         return MarketOrder(
@@ -209,7 +209,7 @@ cdef class OrderFactory:
         ValueError
             If quantity is not positive (> 0).
         ValueError
-            If time_in_force is GTD expire_time is None.
+            If time_in_force is GTD and expire_time is None.
         ValueError
             If post_only and hidden.
         ValueError
@@ -274,7 +274,7 @@ cdef class OrderFactory:
         ValueError
             If quantity is not positive (> 0).
         ValueError
-            If time_in_force is GTD expire_time is None.
+            If time_in_force is GTD and expire_time is None.
 
         """
         return StopMarketOrder(
