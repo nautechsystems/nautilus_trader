1c008f7343031b306f2f96ba6326e224fc269de6
Bshara Zahran <bshara23@gmail.com>
2021-08-08 18:10:19 +1000

    Refactor and cleanup network base classes

--- tests/integration_tests/network/test_ws.py
+++ tests/integration_tests/network/test_ws.py
@@ -1,31 +1,40 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2021 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------import asyncio
+
 import asyncio
 
 import pytest
 
-from nautilus_trader.common.logging import LoggerAdapter
-from nautilus_trader.network.ws_client import WebsocketClient
+from nautilus_trader.network.ws_client import WebSocketClient
 from tests.test_kit.stubs import TestStubs
 
 
-@pytest.fixture()
-def logger_adapter() -> LoggerAdapter:
-    return LoggerAdapter("socket_test", TestStubs.logger())
-
-
+@pytest.mark.skip(reason="WIP")
 @pytest.mark.asyncio
-@pytest.mark.skip
-async def test_client_recv(logger_adapter):
+async def test_client_recv():
     NUM_MESSAGES = 3
     lines = []
 
     def record(*args, **kwargs):
         lines.append((args, kwargs))
 
-    client = WebsocketClient(
+    client = WebSocketClient(
         ws_url="ws://echo.websocket.org",
-        handler=record,
-        logger=logger_adapter,
         loop=asyncio.get_event_loop(),
+        handler=record,
+        logger=TestStubs.logger(),
     )
     await client.connect()
     for _ in range(NUM_MESSAGES):
