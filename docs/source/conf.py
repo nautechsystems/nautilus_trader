cae54c5d3f0f8d7276341c14b982112b72b4d54f
Bshara Zahran <bshara23@gmail.com>
2021-08-17 16:04:09 +1000

    Fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,23 +33,26 @@ release = version
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.doctest",
-    "sphinx.ext.coverage",
     "sphinx.ext.napoleon",
-    "sphinx.ext.intersphinx",
     "numpydoc",
 ]
 
-# The suffix(es) of source filenames.
+# The suffix(es) of source filenames
 # You can specify multiple suffix as a list of string:
 # source_suffix = [".rst", ".md"]
-source_suffix = ".rst"
+source_suffix = [".rst", ".md"]
 
-# The master toctree document.
+# The master toctree document
 master_doc = "index"
 
-# The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+# The name of the Pygments (syntax highlighting) style to use
+pygments_style = "friendly"
+
+# Don't auto-generate summary for class members
+numpydoc_show_class_members = False
+
+# Show the documentation of __init__ and the class docstring
+autoclass_content = "both"
 
 # do not prepend module name to functions
 add_module_names = False
@@ -57,17 +60,18 @@ todo_include_todos = False
 
 autosummary_generate = True
 autodoc_member_order = "bysource"
-numpydoc_show_class_members = False
 
 napoleon_google_docstring = False
+
+# Do not show the return type as separate section
 napoleon_use_rtype = False
 
-# Add any paths that contain templates here, relative to this directory.
+# Add any paths that contain templates here, relative to this directory
 templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+# This pattern also affects html_static_path and html_extra_path
 exclude_patterns: List[str] = []
 
 # -- Options for HTML output -------------------------------------------------
@@ -80,13 +84,3 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_style = "css/nautilus.css"
 html_logo = "_static/img/nautilus-black.png"
-
-
-def skip(app, what, name, obj, would_skip, options):  # noqa
-    if name == "__init__":
-        return False
-    return would_skip
-
-
-def setup(app):  # noqa
-    app.connect("autodoc-skip-member", skip)
