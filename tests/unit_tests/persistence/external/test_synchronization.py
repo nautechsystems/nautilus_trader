42b2f7c753fce9be41b7335d298ff9c0a9d958e8
Bshara Zahran <bshara23@gmail.com>
2021-08-30 13:16:04 +1000

    Fix tests

--- tests/unit_tests/persistence/external/test_synchronization.py
+++ tests/unit_tests/persistence/external/test_synchronization.py
@@ -8,6 +8,7 @@ from dask import compute
 from dask import delayed
 from distributed import Client
 
+from nautilus_trader.persistence.external.synchronization import named_lock
 from tests.test_kit import PACKAGE_ROOT
 
 
@@ -18,13 +19,16 @@ TEST_DATA = PACKAGE_ROOT + "/data"
 
 @delayed
 def _run():
-    with open("test.file", "ab") as f:
-        f.write(b"hello")
-        time.sleep(0.1)
+    with named_lock("test.file"):
+        with open("test.file", "ab") as f:
+            f.write(b"hello")
+            time.sleep(0.1)
 
 
 @pytest.mark.parametrize("scheduler", ("sync", Client()))
 def test_named_lock_sync(scheduler):
+    if os.path.exists("test.file"):
+        os.unlink("test.file")
     tasks = (_run(), _run(), _run())
     with dask.config.set(scheduler=scheduler):
         compute(tasks)
