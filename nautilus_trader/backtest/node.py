b93e28b765b7f0856eb6f31a80cfa473b6dc5455
cjdsellers <chris@cjdsellers.io>
2021-09-10 16:20:51 +1000

    Refactor `BacktestNode`
    - Refactor tests.

--- nautilus_trader/backtest/node.py
+++ nautilus_trader/backtest/node.py
@@ -14,16 +14,17 @@
 # -------------------------------------------------------------------------------------------------
 
 from functools import partial
-from typing import List
+from typing import Any, Dict, List, Tuple
 
+import dask
 import pandas as pd
-from dask import delayed
 from dask.base import normalize_token
 from dask.base import tokenize
 from dask.delayed import Delayed
 
 from nautilus_trader.backtest.config import BacktestDataConfig
 from nautilus_trader.backtest.config import BacktestRunConfig
+from nautilus_trader.backtest.config import BacktestVenueConfig
 from nautilus_trader.backtest.engine import BacktestEngine
 from nautilus_trader.backtest.engine import BacktestEngineConfig
 from nautilus_trader.model.currency import Currency
@@ -39,11 +40,10 @@ from nautilus_trader.model.orderbook.data import OrderBookDelta
 from nautilus_trader.persistence.catalog import DataCatalog
 from nautilus_trader.trading.config import ImportableStrategyConfig
 from nautilus_trader.trading.config import StrategyFactory
+from nautilus_trader.trading.strategy import TradingStrategy
 
 
 # Register tokenization methods with dask
-# TODO(cs): Possible move this somewhere else?
-
 for cls in Instrument.__subclasses__():
     normalize_token.register(cls, func=cls.to_dict)
 
@@ -57,14 +57,71 @@ class BacktestNode:
         """
         Initialize a new instance of the ``BacktestNode`` class.
         """
-        pass
 
-    @delayed(pure=True)
-    def load(self, config: BacktestDataConfig):
+    def build_graph(self, run_configs: List[BacktestRunConfig]) -> Delayed:
+        """
+        Build a `Delayed` graph from `backtest_configs` which can be passed to a dask executor.
+
+        Returns
+        -------
+        Delayed
+
+        """
+        results: List[Tuple[str, Dict[str, Any]]] = []
+        for config in run_configs:
+            config.check(ignore=("name",))  # check all values set
+            input_data = []
+            for data_config in config.data:
+                load_func = partial(
+                    self._load_delayed,
+                    dask_key_name=f"load-{tokenize(data_config.query)}",
+                )
+                input_data.append(load_func(data_config))
+            results.append(
+                self._run_delayed(
+                    name=config.name or f"backtest-{tokenize(config)}",
+                    venue_configs=config.venues,
+                    input_data=input_data,
+                    strategy_configs=config.strategies,
+                )
+            )
+        return self._gather_delayed(results)
+
+    def run_sync(self, run_configs: List[BacktestRunConfig]) -> Dict[str, Any]:
+        """
+        Run a list of backtest configs synchronously.
+
+        Parameters
+        ----------
+        run_configs : Union[BacktestRunConfig, List[BacktestRunConfig]]
+            The backtest run configurations.
+
+        """
+        results: List[Tuple[str, Dict[str, Any]]] = []
+        for config in run_configs:
+            config.check(ignore=("name",))  # check all values set
+            input_data = []
+            for data_config in config.data:
+                input_data.append(self._load(data_config))
+            results.append(
+                self._run(
+                    name=config.name or f"backtest-{tokenize(config)}",
+                    venue_configs=config.venues,
+                    input_data=input_data,
+                    strategy_configs=config.strategies,
+                )
+            )
+        return self._gather(results)
+
+    @dask.delayed(pure=True)
+    def _load_delayed(self, config: BacktestDataConfig):
         return self._load(config=config)
 
     def _load(self, config: BacktestDataConfig):
-        catalog = DataCatalog(path=config.catalog_path, fs_protocol=config.catalog_fs_protocol)
+        catalog = DataCatalog(
+            path=config.catalog_path,
+            fs_protocol=config.catalog_fs_protocol,
+        )
         query = config.query
         return {
             "type": query["cls"],
@@ -75,7 +132,31 @@ class BacktestNode:
             "client_id": config.client_id,
         }
 
-    def create_backtest_engine(self, venues, data):
+    @dask.delayed
+    def _run_delayed(self, name, venue_configs, input_data, strategy_configs):
+        return self._run(
+            name=name,
+            venue_configs=venue_configs,
+            input_data=input_data,
+            strategy_configs=strategy_configs,
+        )
+
+    def _run(self, name, venue_configs, input_data, strategy_configs) -> Tuple[str, Dict[str, Any]]:
+        engine: BacktestEngine = self._create_engine(
+            venue_configs=venue_configs,
+            input_data=input_data,
+        )
+        results: Dict[str, Any] = self._run_engine(
+            engine=engine,
+            strategy_configs=strategy_configs,
+        )
+        return name, results
+
+    def _create_engine(
+        self,
+        venue_configs: List[BacktestVenueConfig],
+        input_data,
+    ):
         # Configure backtest engine
         config = BacktestEngineConfig(
             bypass_logging=True,
@@ -85,7 +166,7 @@ class BacktestNode:
         engine = BacktestEngine(config=config)
 
         # Add data
-        for d in data:
+        for d in input_data:
             instrument = d["instrument"]
             if instrument is not None:
                 engine.add_instrument(instrument)
@@ -96,28 +177,35 @@ class BacktestNode:
                 engine.add_trade_tick_objects(data=d["data"], instrument_id=d["instrument"].id)
             elif d["type"] == OrderBookDelta:
                 engine.add_order_book_data(data=d["data"])
-            # TODO(cs): Unsure if we should allow adding events to the engine directly in this way?
-            # elif isinstance(d["data"][0], Event):
-            #     engine.add_events(client_id=d["client_id"], data=d["data"])
             else:
                 engine.add_generic_data(client_id=d["client_id"], data=d["data"])
 
         # Add venues
-        for venue in venues:
+        for config in venue_configs:
             engine.add_venue(
-                venue=Venue(venue.name),
-                venue_type=VenueType[venue.venue_type],
-                oms_type=OMSType[venue.oms_type],
-                account_type=AccountType[venue.account_type],
-                base_currency=Currency.from_str(venue.base_currency),
-                starting_balances=[Money.from_str(m) for m in venue.starting_balances],
-                # modules=venue.modules,  # TODO(cs): Implement next iteration
+                venue=Venue(config.name),
+                venue_type=VenueType[config.venue_type],
+                oms_type=OMSType[config.oms_type],
+                account_type=AccountType[config.account_type],
+                base_currency=Currency.from_str(config.base_currency),
+                starting_balances=[Money.from_str(m) for m in config.starting_balances],
             )
         return engine
 
-    def run_engine(self, engine, strategies):
-        strategies = [StrategyFactory.create(config) for config in strategies]
+    def _run_engine(
+        self,
+        engine: BacktestEngine,
+        strategy_configs: List[ImportableStrategyConfig],
+    ) -> Dict[str, Any]:
+        # Create strategies
+        strategies: List[TradingStrategy] = [
+            StrategyFactory.create(config) for config in strategy_configs
+        ]
+
+        # Run backtest
         engine.run(strategies=strategies)
+
+        # Collect results
         data = {
             "account": pd.concat(
                 [
@@ -131,80 +219,9 @@ class BacktestNode:
         engine.dispose()
         return data
 
-    def _run_backtest(self, venues, data, strategies, name):
-        engine = self.create_backtest_engine(venues=venues, data=data)
-        results = self.run_engine(engine=engine, strategies=strategies)
-        return name, results
-
-    @delayed
-    def run_backtest(self, venues, data, strategies, name):
-        return self._run_backtest(venues, data, strategies, name)
+    @dask.delayed
+    def _gather_delayed(self, *results):
+        return self._gather(*results)
 
     def _gather(self, *results):
         return {k: v for r in results for k, v in r}
-
-    @delayed
-    def gather(self, *results):
-        return self._gather(*results)
-
-    def _check_configs(self, configs):
-        if isinstance(configs, BacktestRunConfig):
-            configs = [configs]
-
-        for config in configs:
-            if not isinstance(config.strategies, list):
-                config.strategies = [config.strategies]
-            for strategy in config.strategies:
-                err = "strategy argument must be tuple of (TradingStrategy, TradingStrategyConfig)"
-                assert isinstance(strategy, ImportableStrategyConfig), err
-
-        return configs
-
-    def run_sync(self, backtest_configs: List[BacktestRunConfig]):
-        """
-        Run a list of backtest configs synchronously
-        """
-        backtest_configs = self._check_configs(backtest_configs)
-
-        results = []
-        for config in backtest_configs:
-            config.check(ignore=("name",))  # check all values set
-            input_data = []
-            for data_config in config.data:
-                input_data.append(self._load(data_config))
-            results.append(
-                self._run_backtest(
-                    venues=config.venues,
-                    data=input_data,
-                    strategies=config.strategies,
-                    name=config.name or f"backtest-{tokenize(config)}",
-                )
-            )
-        return self._gather(results)
-
-    def build_graph(self, backtest_configs: List[BacktestRunConfig]) -> Delayed:
-        """
-        Build a `Delayed` graph from `backtest_configs` that can be passed to a dask executor
-        """
-        backtest_configs = self._check_configs(backtest_configs)
-
-        results = []
-        for config in backtest_configs:
-            config.check(ignore=("name",))  # check all values set
-            input_data = []
-            for data_config in config.data:
-                load_func = partial(self.load, dask_key_name=f"load-{tokenize(data_config.query)}")
-                input_data.append(
-                    load_func(  # type: ignore
-                        data_config,
-                    )
-                )
-            results.append(
-                self.run_backtest(
-                    venues=config.venues,
-                    data=input_data,
-                    strategies=config.strategies,
-                    name=config.name or f"backtest-{tokenize(config)}",
-                )
-            )
-        return self.gather(results)
