5be6649c9a4ff900cf4ed66721975e6f17b81dfd
Bshara Zahran <bshara23@gmail.com>
2021-10-06 15:34:23 +1100

    De-cythonize IB adapter

--- nautilus_trader/adapters/ib/providers.py
+++ nautilus_trader/adapters/ib/providers.py
@@ -13,35 +13,38 @@
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
 
+import time
+from typing import Dict, List
+
 import ib_insync
+from ib_insync import ContractDetails
 
-from nautilus_trader.common.providers cimport InstrumentProvider
-from nautilus_trader.core.correctness cimport Condition
-from nautilus_trader.core.time cimport unix_timestamp_ns
-from nautilus_trader.model.c_enums.asset_class cimport AssetClass
-from nautilus_trader.model.c_enums.asset_class cimport AssetClassParser
-from nautilus_trader.model.c_enums.asset_type cimport AssetType
-from nautilus_trader.model.c_enums.asset_type cimport AssetTypeParser
-from nautilus_trader.model.currency cimport Currency
-from nautilus_trader.model.identifiers cimport InstrumentId
-from nautilus_trader.model.instruments.base cimport Instrument
-from nautilus_trader.model.instruments.equity cimport Equity
-from nautilus_trader.model.instruments.future cimport Future
-from nautilus_trader.model.objects cimport Price
-from nautilus_trader.model.objects cimport Quantity
+from nautilus_trader.common.providers import InstrumentProvider
+from nautilus_trader.core.correctness import PyCondition
+from nautilus_trader.model.c_enums.asset_class import AssetClass
+from nautilus_trader.model.c_enums.asset_class import AssetClassParser
+from nautilus_trader.model.c_enums.asset_type import AssetType
+from nautilus_trader.model.c_enums.asset_type import AssetTypeParser
+from nautilus_trader.model.currency import Currency
+from nautilus_trader.model.identifiers import InstrumentId
+from nautilus_trader.model.instruments.base import Instrument
+from nautilus_trader.model.instruments.equity import Equity
+from nautilus_trader.model.instruments.future import Future
+from nautilus_trader.model.objects import Price
+from nautilus_trader.model.objects import Quantity
 
 
-cdef class IBInstrumentProvider(InstrumentProvider):
+class IBInstrumentProvider(InstrumentProvider):
     """
     Provides a means of loading `Instrument` objects through Interactive Brokers.
     """
 
     def __init__(
         self,
-        client not None: ib_insync.IB,
-        str host="127.0.0.1",
-        str port="7497",
-        int client_id=1,
+        client: ib_insync.IB,
+        host: str = "127.0.0.1",
+        port: int = 7497,
+        client_id: int = 1,
     ):
         """
         Initialize a new instance of the ``IBInstrumentProvider`` class.
@@ -65,14 +68,14 @@ cdef class IBInstrumentProvider(InstrumentProvider):
         self._port = port
         self._client_id = client_id
 
-    cpdef void connect(self):
+    def connect(self):
         self._client.connect(
             host=self._host,
             port=self._port,
             clientId=self._client_id,
         )
 
-    cpdef void load(self, InstrumentId instrument_id, dict details) except *:
+    def load(self, instrument_id: InstrumentId, details: Dict):
         """
         Load the instrument for the given ID and details.
 
@@ -84,9 +87,9 @@ cdef class IBInstrumentProvider(InstrumentProvider):
             The instrument details.
 
         """
-        Condition.not_none(instrument_id, "instrument_id")
-        Condition.not_none(details, "details")
-        Condition.is_in("asset_type", details, "asset_type", "details")
+        PyCondition.not_none(instrument_id, "instrument_id")
+        PyCondition.not_none(details, "details")
+        PyCondition.is_in("asset_type", details, "asset_type", "details")
 
         if not self._client.client.CONNECTED:
             self.connect()
@@ -98,13 +101,17 @@ cdef class IBInstrumentProvider(InstrumentProvider):
             currency=details.get("currency"),
         )
 
-        cdef list contract_details = self._client.reqContractDetails(contract=contract)
+        contract_details: List[ContractDetails] = self._client.reqContractDetails(contract=contract)
         if not contract_details:
-            raise ValueError(f"No contract details found for the given instrument ID {instrument_id}")
+            raise ValueError(
+                f"No contract details found for the given instrument ID {instrument_id}"
+            )
         elif len(contract_details) > 1:
-            raise ValueError(f"Multiple contract details found for the given instrument ID {instrument_id}")
+            raise ValueError(
+                f"Multiple contract details found for the given instrument ID {instrument_id}"
+            )
 
-        cdef Instrument instrument = self._parse_instrument(
+        instrument: Instrument = self._parse_instrument(
             asset_type=AssetTypeParser.from_str(details.get("asset_type")),
             instrument_id=instrument_id,
             details=details,
@@ -113,42 +120,40 @@ cdef class IBInstrumentProvider(InstrumentProvider):
 
         self._instruments[instrument_id] = instrument
 
-    cdef Instrument _parse_instrument(
+    def _parse_instrument(
         self,
-        AssetType asset_type,
-        InstrumentId instrument_id,
-        dict details,
-        object contract_details
-    ):
+        asset_type: AssetType,
+        instrument_id: InstrumentId,
+        details: Dict,
+        contract_details: ContractDetails,
+    ) -> Instrument:
         if asset_type == AssetType.FUTURE:
-            Condition.is_in("asset_class", details, "asset_class", "details")
+            PyCondition.is_in("asset_class", details, "asset_class", "details")
             return self._parse_futures_contract(
                 instrument_id=instrument_id,
-                asset_class=AssetClassParser.from_str(details['asset_class']),
-                details=contract_details
+                asset_class=AssetClassParser.from_str(details["asset_class"]),
+                details=contract_details,
             )
         elif asset_type == AssetType.SPOT:
             return self._parse_equity_contract(
-                instrument_id=instrument_id,
-                details=contract_details
+                instrument_id=instrument_id, details=contract_details
             )
         else:
             raise TypeError(f"No parser for asset_type {asset_type}")
 
-    cdef int _tick_size_to_precision(self, double tick_size) except *:
-        cdef tick_size_str = f"{tick_size:f}"
-        return len(tick_size_str.partition('.')[2].rstrip('0'))
+    def _tick_size_to_precision(self, tick_size: float) -> int:
+        tick_size_str = f"{tick_size:f}"
+        return len(tick_size_str.partition(".")[2].rstrip("0"))
 
-    cdef Future _parse_futures_contract(
+    def _parse_futures_contract(
         self,
-        InstrumentId instrument_id,
-        AssetClass asset_class,
-        object details,
-    ):
-        cdef int price_precision = self._tick_size_to_precision(details.minTick)
-
-        timestamp = unix_timestamp_ns()
-        cdef Future future = Future(
+        instrument_id: InstrumentId,
+        asset_class: AssetClass,
+        details: ContractDetails,
+    ) -> Future:
+        price_precision: int = self._tick_size_to_precision(details.minTick)
+        timestamp = time.time_ns()
+        future = Future(
             instrument_id=instrument_id,
             asset_class=asset_class,
             currency=Currency.from_str_c(details.contract.currency),
@@ -173,19 +178,21 @@ cdef class IBInstrumentProvider(InstrumentProvider):
 
         return future
 
-    cpdef Equity _parse_equity_contract(
+    def _parse_equity_contract(
         self,
-        InstrumentId instrument_id,
-        object details,
-    ):
-        cdef int price_precision = self._tick_size_to_precision(details.minTick)
-        timestamp = unix_timestamp_ns()
+        instrument_id: InstrumentId,
+        details: ContractDetails,
+    ) -> Equity:
+        price_precision: int = self._tick_size_to_precision(details.minTick)
+        timestamp = time.time_ns()
         equity = Equity(
             instrument_id=instrument_id,
             currency=Currency.from_str_c(details.contract.currency),
             price_precision=price_precision,
             price_increment=Price(details.minTick, price_precision),
-            multiplier=Quantity.from_int_c(int(details.contract.multiplier or details.mdSizeMultiplier)),  # is this right?
+            multiplier=Quantity.from_int_c(
+                int(details.contract.multiplier or details.mdSizeMultiplier)
+            ),  # is this right?
             lot_size=Quantity.from_int_c(1),
             contract_id=details.contract.conId,
             local_symbol=details.contract.localSymbol,
