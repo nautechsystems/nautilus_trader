36e3b31b3fabefe92b12d44296ade856d0b5b72a
Bshara Zahran <bshara23@gmail.com>
2021-10-07 16:33:03 +1100

    Skip 2 flakey tests on Windows

--- tests/integration_tests/network/test_http.py
+++ tests/integration_tests/network/test_http.py
@@ -14,6 +14,7 @@
 # -------------------------------------------------------------------------------------------------
 
 import asyncio
+import sys
 
 import pytest
 
@@ -31,12 +32,14 @@ async def client():
     return client
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="failing on windows")
 @pytest.mark.asyncio
 async def test_client_get(client):
     resp = await client.get("https://httpbin.org/get")
     assert len(resp.data) > 100
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="failing on windows")
 @pytest.mark.asyncio
 async def test_client_post(client):
     resp = await client.post("https://httpbin.org/post")
