4f5ab6459ff77887d51df7685bfa61f4d5bfd1b5
Bshara Zahran <bshara23@gmail.com>
2021-10-13 17:37:58 +1100

    Clarify client connect and disconnect
    - Consolidate live configs.

--- examples/live/betfair_example.py
+++ examples/live/betfair_example.py
@@ -1,6 +1,28 @@
+#!/usr/bin/env python3
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2021 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
 import asyncio
 import os
+import sys
 import traceback
+
+
+sys.path.insert(
+    0, str(os.path.abspath(__file__ + "/../../../"))
+)  # Allows relative imports from examples
 
 from examples.strategies.orderbook_imbalance import OrderBookImbalance
 from examples.strategies.orderbook_imbalance import OrderBookImbalanceConfig
@@ -103,4 +125,4 @@ async def main(market_id: str):
 
 
 if __name__ == "__main__":
-    asyncio.run(main(market_id="1.188737202"))
+    asyncio.run(main(market_id="1.189009581"))
