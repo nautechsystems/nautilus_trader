abe5c79a6372d3e0e7830a72d75f4fbbd0a4e8ea
limx0 <bradley.mcelroy@live.com>
2021-10-29 10:36:27 +1100

    Add orjson default serializer

---
+++ nautilus_trader/serialization/json/default.py
@@ -0,0 +1,26 @@
+import decimal
+from typing import Callable, Dict
+
+
+class Default:
+    """
+    Serialization extensions for orjson.dumps
+    """
+
+    registry: Dict = {}
+
+    @classmethod
+    def register_serializer(cls, type_: type, serializer: Callable):
+        """Register a new type `type_` for serialization in orjson"""
+        assert type_ not in cls.registry
+        cls.registry[type_] = serializer
+
+    @classmethod
+    def serialize(cls, obj):
+        """Serialize for types orjson.dumps can't understand"""
+        if type(obj) in cls.registry:
+            return cls.registry[type(obj)](obj)
+        raise TypeError
+
+
+Default.register_serializer(type_=decimal.Decimal, serializer=str)
