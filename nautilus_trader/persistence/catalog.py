3a6cb9760d274d00661c7baa0ec8d7ac47c4c410
Bshara Zahran <bshara23@gmail.com>
2021-11-17 08:11:24 +1100

    Fix precommit

--- nautilus_trader/persistence/catalog.py
+++ nautilus_trader/persistence/catalog.py
@@ -69,9 +69,12 @@ class DataCatalog(metaclass=Singleton):
             The fs storage options.
 
         """
+        self.path = pathlib.Path(path)
         self.fs_protocol = fs_protocol
         self.fs_storage_options = fs_storage_options or {}
-        self.fs: fsspec.AbstractFileSystem = fsspec.filesystem(self.fs_protocol, **self.fs_storage_options)
+        self.fs: fsspec.AbstractFileSystem = fsspec.filesystem(
+            self.fs_protocol, **self.fs_storage_options
+        )
 
     @classmethod
     def from_env(cls):
