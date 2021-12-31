daa273ec8024ae929d3aa69e194e2e6ad360f25d
Bshara Zahran <bshara23@gmail.com>
2022-01-01 07:58:00 +1100

    Update header

--- examples/backtest/fx_ema_cross_audusd_bars_from_ticks.py
+++ examples/backtest/fx_ema_cross_audusd_bars_from_ticks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -------------------------------------------------------------------------------------------------
-#  Copyright (C) 2015-2021 Nautech Systems Pty Ltd. All rights reserved.
+#  Copyright (C) 2015-2022 Nautech Systems Pty Ltd. All rights reserved.
 #  https://nautechsystems.io
 #
 #  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
