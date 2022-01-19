1bc337b16720acb2e597414de9a3035f4bc63b78
Bshara Zahran <bshara23@gmail.com>
2022-01-19 20:16:21 +1100

    Fix `expire_time_ns` handling

--- nautilus_trader/model/orders/limit.pyx
+++ nautilus_trader/model/orders/limit.pyx
@@ -19,7 +19,7 @@ from libc.stdint cimport int64_t
 from nautilus_trader.core.correctness cimport Condition
 from nautilus_trader.core.datetime cimport dt_to_unix_nanos
 from nautilus_trader.core.datetime cimport format_iso8601
-from nautilus_trader.core.datetime cimport unix_nanos_to_dt
+from nautilus_trader.core.datetime cimport maybe_unix_nanos_to_dt
 from nautilus_trader.core.uuid cimport UUID4
 from nautilus_trader.model.c_enums.contingency_type cimport ContingencyType
 from nautilus_trader.model.c_enums.contingency_type cimport ContingencyTypeParser
@@ -139,11 +139,11 @@ cdef class LimitOrder(Order):
         )
 
         # Set options
-        cdef int64_t expire_time_ns = dt_to_unix_nanos(expire_time) if expire_time is not None else 0
+        cdef int64_t expire_time_ns = dt_to_unix_nanos(expire_time) if expire_time is not None else -1
         cdef dict options = {
             "price": str(price),
             "display_qty": str(display_qty) if display_qty is not None else None,
-            "expire_time_ns": expire_time_ns,
+            "expire_time_ns": expire_time_ns if expire_time_ns >= 0 else None,
         }
 
         # Create initialization event
@@ -256,7 +256,6 @@ cdef class LimitOrder(Order):
         Condition.not_none(init, "init")
         Condition.equal(init.type, OrderType.LIMIT, "init.type", "OrderType")
 
-        cdef int64_t expire_time_ns = init.options["expire_time_ns"]
         cdef str display_qty_str = init.options["display_qty"]
 
         return LimitOrder(
@@ -268,7 +267,7 @@ cdef class LimitOrder(Order):
             quantity=init.quantity,
             price=Price.from_str_c(init.options["price"]),
             time_in_force=init.time_in_force,
-            expire_time=unix_nanos_to_dt(expire_time_ns) if expire_time_ns > 0 else None,
+            expire_time=maybe_unix_nanos_to_dt(init.options["expire_time_ns"]),
             init_id=init.id,
             ts_init=init.ts_init,
             post_only=init.post_only,
