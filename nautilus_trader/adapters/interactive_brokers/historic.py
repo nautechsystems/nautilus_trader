d23069f18397b0544a8baf68655fb8a7a7d45574
limx0 <bradley.mcelroy@live.com>
2022-02-07 18:31:01 +1100

    Fix licence

--- nautilus_trader/adapters/interactive_brokers/historic.py
+++ nautilus_trader/adapters/interactive_brokers/historic.py
@@ -1,3 +1,18 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2022 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
 import datetime
 import logging
 from typing import List
@@ -10,11 +25,11 @@ from ib_insync import Contract
 logger = logging.getLogger(__name__)
 
 
-def back_fill_catalog(ib: IB, contract: Contract, start: datetime.datetime, end: datetime.datetime):
-    """
-    Back fill the data catalog with market data for `contract` between `start` and `end`
-    """
-    pass
+# def back_fill_catalog(ib: IB, contract: Contract, start: datetime.datetime, end: datetime.datetime):
+#     """
+#     Back fill the data catalog with market data for `contract` between `start` and `end`
+#     """
+#     pass
 
 
 def _request_historical_ticks(ib: IB, contract: Contract, start_time: str, what="BID_ASK"):
