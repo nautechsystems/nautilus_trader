854c6ade0782b3ab0afa3396d4790e4ab1586547
cjdsellers <chris@cjdsellers.io>
2022-02-10 13:57:18 +1100

    Docs cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@
 import os
 import sys
 
+
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.append(os.path.abspath("./_pygments"))
 
@@ -150,6 +151,5 @@ napoleon_google_docstring = False
 
 # Do not show the return type as separate section
 napoleon_use_rtype = False
-
 
 pygments_style = "monokai.MonokaiStyle"
