a2f18aa78bb5ee581174dbbf294348f02f687050
Bshara Zahran <bshara23@gmail.com>
2022-03-10 09:54:57 +1100

    Interactive Brokers Adapter (#569)

---
+++ nautilus_trader/adapters/interactive_brokers/historic.py
@@ -0,0 +1,200 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2022 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
+import datetime
+import logging
+from typing import List
+
+import pandas as pd
+from ib_insync import IB
+from ib_insync import Contract
+from ib_insync import HistoricalTickBidAsk
+from ib_insync import HistoricalTickLast
+
+from nautilus_trader.adapters.interactive_brokers.parsing.data import generate_trade_id
+from nautilus_trader.adapters.interactive_brokers.parsing.instruments import parse_instrument
+from nautilus_trader.core.datetime import dt_to_unix_nanos
+from nautilus_trader.model.data.tick import QuoteTick
+from nautilus_trader.model.data.tick import TradeTick
+from nautilus_trader.model.enums import AggressorSide
+from nautilus_trader.model.identifiers import InstrumentId
+from nautilus_trader.model.objects import Price
+from nautilus_trader.model.objects import Quantity
+from nautilus_trader.persistence.catalog import DataCatalog
+from nautilus_trader.persistence.external.core import write_objects
+
+
+logger = logging.getLogger(__name__)
+
+
+def back_fill_catalog(
+    ib: IB,
+    catalog: DataCatalog,
+    contracts: List[Contract],
+    start_date: datetime.date,
+    end_date: datetime.date,
+    tz_name="Asia/Hong_Kong",
+    kinds=("BID_ASK", "TRADES"),
+):
+    """
+    Back fill the data catalog with market data from Interactive Brokers.
+
+    Parameters
+    ----------
+    ib : IB
+        The ib_insync client.
+    catalog : DataCatalog
+        DataCatalog to write the data to
+    contracts : List[Contract]
+        The list of IB Contracts to collect data for
+    start_date : datetime.date
+        The start_date for the back fill.
+    end_date : datetime.date
+        The end_date for the back fill.
+    tz_name : str
+        The timezone of the contracts
+    kinds : tuple[str] (default: ('BID_ASK', 'TRADES')
+        The kinds to query data for
+    """
+    for date in pd.bdate_range(start_date, end_date):
+        for kind in kinds:
+            for contract in contracts:
+                [details] = ib.reqContractDetails(contract=contract)
+                instrument = parse_instrument(contract_details=details)
+                raw = fetch_market_data(
+                    contract=contract, date=date.to_pydatetime(), kind=kind, tz_name=tz_name, ib=ib
+                )
+                if kind == "TRADES":
+                    ticks = parse_historic_trade_ticks(
+                        historic_ticks=raw, instrument_id=instrument.id
+                    )
+                elif kind == "BID_ASK":
+                    ticks = parse_historic_quote_ticks(
+                        historic_ticks=raw, instrument_id=instrument.id
+                    )
+                else:
+                    raise RuntimeError()
+                write_objects(catalog=catalog, chunk=ticks)
+
+
+def fetch_market_data(
+    contract: Contract, date: datetime.date, kind: str, tz_name: str, ib=None
+) -> List:
+    if isinstance(date, datetime.datetime):
+        date = date.date()
+    assert kind in ("TRADES", "BID_ASK")
+    data: List = []
+
+    while True:
+        start_time = _determine_next_timestamp(
+            date=date, timestamps=[d.time for d in data], tz_name=tz_name
+        )
+        logger.info(f"Using start_time: {start_time}")
+
+        ticks = _request_historical_ticks(
+            ib=ib,
+            contract=contract,
+            start_time=start_time.strftime("%Y%m%d %H:%M:%S %Z"),
+            what=kind,
+        )
+
+        ticks = [t for t in ticks if t not in data]
+
+        if not ticks or ticks[0].time < start_time:
+            break
+
+        logger.debug(f"Received {len(ticks)} ticks between {ticks[0].time} and {ticks[-1].time}")
+
+        last_timestamp = pd.Timestamp(ticks[-1].time)
+        last_date = last_timestamp.astimezone(tz_name).date()
+
+        if last_date != date:
+            # May contain data from next date, filter this out
+            data.extend([tick for tick in ticks if pd.to_datetime(tick)])
+            break
+        else:
+            data.extend(ticks)
+    return data
+
+
+def _request_historical_ticks(ib: IB, contract: Contract, start_time: str, what="BID_ASK"):
+    return ib.reqHistoricalTicks(
+        contract=contract,
+        startDateTime=start_time,
+        endDateTime="",
+        numberOfTicks=1000,
+        whatToShow=what,
+        useRth=False,
+    )
+
+
+def _determine_next_timestamp(timestamps: List[pd.Timestamp], date: datetime.date, tz_name: str):
+    """
+    While looping over available data, it is possible for very liquid products that a 1s period may contain 1000 ticks,
+    at which point we need to step the time forward to avoid getting stuck when iterating.
+    """
+    if not timestamps:
+        return pd.Timestamp(date, tz=tz_name).tz_convert("UTC")
+    unique_values = set(timestamps)
+    if len(unique_values) == 1:
+        timestamp = timestamps[-1]
+        return timestamp + pd.Timedelta(seconds=1)
+    else:
+        return timestamps[-1]
+
+
+def parse_historic_quote_ticks(
+    historic_ticks: List[HistoricalTickBidAsk], instrument_id: InstrumentId
+) -> List[QuoteTick]:
+    trades = []
+    for tick in historic_ticks:
+        ts_init = dt_to_unix_nanos(tick.time)
+        quote_tick = QuoteTick(
+            instrument_id=instrument_id,
+            bid=Price.from_str(str(tick.priceBid)),
+            bid_size=Quantity.from_str(str(tick.sizeBid)),
+            ask=Price.from_str(str(tick.priceAsk)),
+            ask_size=Quantity.from_str(str(tick.sizeAsk)),
+            ts_init=ts_init,
+            ts_event=ts_init,
+        )
+        trades.append(quote_tick)
+
+    return trades
+
+
+def parse_historic_trade_ticks(
+    historic_ticks: List[HistoricalTickLast], instrument_id: InstrumentId
+) -> List[TradeTick]:
+    trades = []
+    for tick in historic_ticks:
+        ts_init = dt_to_unix_nanos(tick.time)
+        trade_tick = TradeTick(
+            instrument_id=instrument_id,
+            price=Price.from_str(str(tick.price)),
+            size=Quantity.from_str(str(tick.size)),
+            aggressor_side=AggressorSide.UNKNOWN,
+            trade_id=generate_trade_id(
+                symbol=instrument_id.symbol.value,
+                ts_event=ts_init,
+                price=tick.price,
+                size=tick.size,
+            ),
+            ts_init=ts_init,
+            ts_event=ts_init,
+        )
+        trades.append(trade_tick)
+
+    return trades
---
+++ tests/integration_tests/adapters/interactive_brokers/test_providers.py
@@ -0,0 +1,216 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2021 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+import asyncio
+import datetime
+from unittest.mock import MagicMock
+
+import pytest
+from ib_insync import Contract
+from ib_insync import Forex
+
+from nautilus_trader.adapters.interactive_brokers.providers import (
+    InteractiveBrokersInstrumentProvider,
+)
+from nautilus_trader.common.clock import LiveClock
+from nautilus_trader.common.config import InstrumentProviderConfig
+from nautilus_trader.common.logging import LiveLogger
+from nautilus_trader.common.logging import LogLevel
+from nautilus_trader.model.enums import AssetClass
+from nautilus_trader.model.enums import AssetType
+from nautilus_trader.model.enums import OptionKind
+from nautilus_trader.model.identifiers import InstrumentId
+from nautilus_trader.model.identifiers import Symbol
+from nautilus_trader.model.identifiers import Venue
+from nautilus_trader.model.objects import Price
+from tests.integration_tests.adapters.interactive_brokers.test_kit import IBTestStubs
+
+
+class TestIBInstrumentProvider:
+    def setup(self):
+        self.ib = MagicMock()
+        self.loop = asyncio.get_event_loop()
+        self.clock = LiveClock()
+        self.logger = LiveLogger(
+            loop=self.loop,
+            clock=self.clock,
+            level_stdout=LogLevel.DEBUG,
+        )
+        self.provider = InteractiveBrokersInstrumentProvider(
+            client=self.ib, logger=self.logger, config=InstrumentProviderConfig()
+        )
+
+    @staticmethod
+    def async_return_value(value: object) -> asyncio.Future:
+        future: asyncio.Future = asyncio.Future()
+        future.set_result(value)
+        return future
+
+    @pytest.mark.parametrize(
+        "filters, expected",
+        [
+            ({"secType": "CASH", "pair": "EURUSD", "exchange": "IDEALPRO"}, Forex("EURUSD")),
+            (
+                {"secType": "STK", "symbol": "AAPL", "exchange": "SMART", "currency": "USD"},
+                Contract("STK", symbol="AAPL", exchange="SMART", currency="USD"),
+            ),
+        ],
+    )
+    def test_parse_contract(self, filters, expected):
+        result = self.provider._parse_contract(**filters)
+        assert result == expected
+
+    @pytest.mark.asyncio
+    async def test_load_equity_contract_instrument(self, mocker):
+        # Arrange
+        instrument_id = InstrumentId.from_str("AAPL.NASDAQ")
+        contract = IBTestStubs.contract(symbol="AAPL")
+        contract_details = IBTestStubs.contract_details("AAPL")
+        mocker.patch.object(
+            self.provider._client,
+            "reqContractDetailsAsync",
+            return_value=self.async_return_value([contract_details]),
+        )
+        mocker.patch.object(
+            self.provider._client,
+            "qualifyContractsAsync",
+            return_value=self.async_return_value([contract]),
+        )
+
+        # Act
+        await self.provider.load(secType="STK", symbol="AAPL", exchange="NASDAQ")
+        equity = self.provider.find(instrument_id)
+
+        # Assert
+        assert InstrumentId(symbol=Symbol("AAPL"), venue=Venue("NASDAQ")) == equity.id
+        assert equity.asset_class == AssetClass.EQUITY
+        assert equity.asset_type == AssetType.SPOT
+        assert 100 == equity.multiplier
+        assert Price.from_str("0.01") == equity.price_increment
+        assert 2, equity.price_precision
+
+    @pytest.mark.asyncio
+    async def test_load_futures_contract_instrument(self, mocker):
+        # Arrange
+        instrument_id = InstrumentId.from_str("CLZ2.NYMEX")
+        contract = IBTestStubs.contract(symbol="CLZ2", exchange="NYMEX")
+        contract_details = IBTestStubs.contract_details("CLZ2")
+        mocker.patch.object(
+            self.provider._client,
+            "reqContractDetailsAsync",
+            return_value=self.async_return_value([contract_details]),
+        )
+        mocker.patch.object(
+            self.provider._client,
+            "qualifyContractsAsync",
+            return_value=self.async_return_value([contract]),
+        )
+
+        # Act
+        await self.provider.load(symbol="CLZ2", exchange="NYMEX")
+        future = self.provider.find(instrument_id)
+
+        # Assert
+        assert future.id == instrument_id
+        assert future.asset_class == AssetClass.INDEX
+        assert future.multiplier == 1000
+        assert future.price_increment == Price.from_str("0.01")
+        assert future.price_precision == 2
+
+    @pytest.mark.asyncio
+    async def test_load_options_contract_instrument(self, mocker):
+        # Arrange
+        instrument_id = InstrumentId.from_str("AAPL211217C00160000.SMART")
+        contract = IBTestStubs.contract(
+            secType="OPT", symbol="AAPL211217C00160000", exchange="NASDAQ"
+        )
+        contract_details = IBTestStubs.contract_details("AAPL211217C00160000")
+        mocker.patch.object(
+            self.provider._client,
+            "reqContractDetailsAsync",
+            return_value=self.async_return_value([contract_details]),
+        )
+        mocker.patch.object(
+            self.provider._client,
+            "qualifyContractsAsync",
+            return_value=self.async_return_value([contract]),
+        )
+
+        # Act
+        await self.provider.load(secType="OPT", symbol="AAPL211217C00160000", exchange="SMART")
+        option = self.provider.find(instrument_id)
+
+        # Assert
+        assert option.id == instrument_id
+        assert option.asset_class == AssetClass.EQUITY
+        assert option.multiplier == 100
+        assert option.expiry_date == datetime.date(2021, 12, 17)
+        assert option.strike_price == Price.from_str("160.0")
+        assert option.kind == OptionKind.CALL
+        assert option.price_increment == Price.from_str("0.01")
+        assert option.price_precision == 2
+
+    @pytest.mark.asyncio
+    async def test_load_forex_contract_instrument(self, mocker):
+        # Arrange
+        instrument_id = InstrumentId.from_str("EUR/USD.IDEALPRO")
+        contract = IBTestStubs.contract(secType="CASH", symbol="EURUSD", exchange="IDEALPRO")
+        contract_details = IBTestStubs.contract_details("EURUSD")
+        mocker.patch.object(
+            self.provider._client,
+            "reqContractDetailsAsync",
+            return_value=self.async_return_value([contract_details]),
+        )
+        mocker.patch.object(
+            self.provider._client,
+            "qualifyContractsAsync",
+            return_value=self.async_return_value([contract]),
+        )
+
+        # Act
+        await self.provider.load(secType="CASH", symbol="EURUSD", exchange="IDEALPRO")
+        fx = self.provider.find(instrument_id)
+
+        # Assert
+        assert fx.id == instrument_id
+        assert fx.asset_class == AssetClass.FX
+        assert fx.multiplier == 1
+        assert fx.price_increment == Price.from_str("0.00005")
+        assert fx.price_precision == 5
+
+    @pytest.mark.asyncio
+    async def test_contract_id_to_instrument_id(self, mocker):
+        # Arrange
+        contract = IBTestStubs.contract(symbol="CLZ2", exchange="NYMEX")
+        contract_details = IBTestStubs.contract_details("CLZ2")
+        mocker.patch.object(
+            self.provider._client,
+            "qualifyContractsAsync",
+            return_value=self.async_return_value([contract]),
+        )
+        mocker.patch.object(
+            self.provider._client,
+            "reqContractDetailsAsync",
+            return_value=self.async_return_value([contract_details]),
+        )
+
+        # Act
+        await self.provider.load(symbol="CLZ2", exchange="NYMEX")
+
+        # Assert
+        expected = {138979238: InstrumentId.from_str("CLZ2.NYMEX")}
+        assert self.provider.contract_id_to_instrument_id == expected
+
+    def test_filters(self):
+        pass
