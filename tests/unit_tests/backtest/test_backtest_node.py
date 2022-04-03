4442e42c3f64abc9850082c81e64c35ff76946b9
Bshara Zahran <bshara23@gmail.com>
2022-04-03 18:05:52 +1000

    Fix tests (#600)

--- tests/unit_tests/backtest/test_backtest_node.py
+++ tests/unit_tests/backtest/test_backtest_node.py
@@ -35,7 +35,6 @@ from tests.test_kit.mocks.data import data_catalog_setup
 pytestmark = pytest.mark.skipif(sys.platform == "win32", reason="test path broken on windows")
 
 
-@pytest.mark.skip(reason="bm to fix persistence")
 class TestBacktestNode:
     def setup(self):
         data_catalog_setup()
@@ -56,7 +55,7 @@ class TestBacktestNode:
             start_time=1580398089820000000,
             end_time=1580504394501000000,
         )
-        strategies = [
+        self.strategies = [
             ImportableStrategyConfig(
                 strategy_path="nautilus_trader.examples.strategies.ema_cross:EMACross",
                 config_path="nautilus_trader.examples.strategies.ema_cross:EMACrossConfig",
@@ -72,28 +71,11 @@ class TestBacktestNode:
         ]
         self.backtest_configs = [
             BacktestRunConfig(
-                engine=BacktestEngineConfig(strategies=strategies),
+                engine=BacktestEngineConfig(strategies=self.strategies),
                 venues=[self.venue_config],
                 data=[self.data_config],
             )
         ]
-        # self.strategies = [
-        #     ImportableStrategyConfig(
-        #         strategy_path="nautilus_trader.examples.strategies.ema_cross:EMACross",
-        #         config_path="nautilus_trader.examples.strategies.ema_cross:EMACrossConfig",
-        #         config=dict(
-        #             instrument_id="AUD/USD.SIM",
-        #             bar_type="AUD/USD.SIM-100-TICK-MID-INTERNAL",
-        #             fast_ema_period=10,
-        #             slow_ema_period=20,
-        #             trade_size=Decimal(1_000_000),
-        #             order_id_tag="001",
-        #         ),
-        #     )
-        # ]
-        # self.backtest_configs_strategies = [
-        #     self.backtest_configs[0].replace(strategies=self.strategies)
-        # ]
         aud_usd_data_loader()  # Load sample data
 
     def test_init(self):
@@ -105,7 +87,7 @@ class TestBacktestNode:
         node = BacktestNode()
 
         # Act
-        results = node.run(run_configs=self.backtest_configs_strategies)
+        results = node.run(run_configs=self.backtest_configs)
 
         # Assert
         assert len(results) == 1
@@ -113,8 +95,12 @@ class TestBacktestNode:
     def test_backtest_run_streaming_sync(self):
         # Arrange
         node = BacktestNode()
-        base = self.backtest_configs[0]
-        config = base.replace(strategies=self.strategies, batch_size_bytes=parse_bytes("10kib"))
+        config = BacktestRunConfig(
+            engine=BacktestEngineConfig(strategies=self.strategies),
+            venues=[self.venue_config],
+            data=[self.data_config],
+            batch_size_bytes=parse_bytes("10kib"),
+        )
 
         # Act
         results = node.run([config])
@@ -127,12 +113,12 @@ class TestBacktestNode:
         node = BacktestNode()
 
         # Act
-        results = node.run(self.backtest_configs_strategies)
+        results = node.run(self.backtest_configs)
 
         # Assert
         assert isinstance(results, list)
         assert len(results) == 1
-        # assert (  # TODO(cs): string changed
+        # assert (
         #     str(results[0])
         #     == "BacktestResult(trader_id='BACKTESTER-000', machine_id='CJDS-X99-Ubuntu', run_config_id='e7647ae948f030bbd50e0b6cb58f67ae', instance_id='ecdf513e-9b07-47d5-9742-3b984a27bb52', run_id='d4d7a09c-fac7-4240-b80a-fd7a7d8f217c', run_started=1648796370520892000, run_finished=1648796371603767000, backtest_start=1580398089820000000, backtest_end=1580504394500999936, elapsed_time=106304.680999, iterations=100000, total_events=192, total_orders=96, total_positions=48, stats_pnls={'USD': {'PnL': -3634.12, 'PnL%': Decimal('-0.36341200'), 'Max Winner': 2673.19, 'Avg Winner': 530.0907692307693, 'Min Winner': 123.13, 'Min Loser': -16.86, 'Avg Loser': -263.9497142857143, 'Max Loser': -616.84, 'Expectancy': -48.89708333333337, 'Win Rate': 0.2708333333333333}}, stats_returns={'Annual Volatility (Returns)': 0.01191492048585753, 'Average (Return)': -3.3242292920660964e-05, 'Average Loss (Return)': -0.00036466955522398476, 'Average Win (Return)': 0.0007716524869588397, 'Sharpe Ratio': -0.7030729097982443, 'Sortino Ratio': -1.492072178035927, 'Profit Factor': 0.8713073377919724, 'Risk Return Ratio': -0.04428943030649289})"  # noqa
         # )
