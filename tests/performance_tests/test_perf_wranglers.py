eb70f2b038a11e9c612025f1857d53c3f73f5593
Bshara Zahran <bshara23@gmail.com>
2022-05-01 10:17:30 +1000

    Add from raw wrangling

---
+++ tests/performance_tests/test_perf_wranglers.py
@@ -0,0 +1,59 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2022 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
+from nautilus_trader.backtest.data.providers import TestDataProvider
+from nautilus_trader.backtest.data.providers import TestInstrumentProvider
+from nautilus_trader.backtest.data.wranglers import QuoteTickDataWrangler
+from nautilus_trader.backtest.data.wranglers import TradeTickDataWrangler
+from tests.test_kit.performance import PerformanceBench
+from tests.test_kit.performance import PerformanceHarness
+
+
+class TestDataWranglersPerformance(PerformanceHarness):
+    def test_quote_tick_data_wrangler_process_tick_data(self):
+        usdjpy = TestInstrumentProvider.default_fx_ccy("USD/JPY")
+
+        wrangler = QuoteTickDataWrangler(instrument=usdjpy)
+        provider = TestDataProvider()
+
+        def wrangler_process():
+            # 1000 ticks in data
+            wrangler.process(
+                data=provider.read_csv_ticks("truefx-usdjpy-ticks.csv"),
+                default_volume=1000000,
+            )
+
+        PerformanceBench.profile_function(
+            target=wrangler_process,
+            runs=100,
+            iterations=1,
+        )
+        # ~7.8ms / ~7766.6μs / 7766626ns minimum of 100 runs @ 1 iteration each run.
+
+    def test_trade_tick_data_wrangler_process(self):
+        ethusdt = TestInstrumentProvider.ethusdt_binance()
+        wrangler = TradeTickDataWrangler(instrument=ethusdt)
+        provider = TestDataProvider()
+
+        def wrangler_process():
+            # 69806 ticks in data
+            wrangler.process(data=provider.read_csv_ticks("binance-ethusdt-trades.csv"))
+
+        PerformanceBench.profile_function(
+            target=wrangler_process,
+            runs=10,
+            iterations=1,
+        )
+        # ~500.2ms / ~500210.6μs / 500210608ns minimum of 10 runs @ 1 iteration each run.
