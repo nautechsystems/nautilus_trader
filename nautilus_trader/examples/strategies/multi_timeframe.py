983a8b115ac0a6fa9e7a300ec5738c49f9898ec7
graceyangfan <yf1825617022@outlook.com>
2022-05-04 08:34:43 +0800

    Add simple multi-time frame strategy example

---
+++ nautilus_trader/examples/strategies/multi_timeframe.py
@@ -0,0 +1,362 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2022 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
+from decimal import Decimal
+from typing import Dict, Optional
+
+from nautilus_trader.common.logging import LogColor
+from nautilus_trader.config import StrategyConfig
+from nautilus_trader.core.data import Data
+from nautilus_trader.core.message import Event
+from nautilus_trader.indicators.average.ema import ExponentialMovingAverage
+from nautilus_trader.indicators.rsi import RelativeStrengthIndex
+from nautilus_trader.model.data.bar import Bar
+from nautilus_trader.model.data.bar import BarType
+from nautilus_trader.model.data.tick import QuoteTick
+from nautilus_trader.model.data.tick import TradeTick
+from nautilus_trader.model.data.ticker import Ticker
+from nautilus_trader.model.enums import OrderSide
+from nautilus_trader.model.identifiers import InstrumentId
+from nautilus_trader.model.instruments.base import Instrument
+from nautilus_trader.model.orderbook.book import OrderBook
+from nautilus_trader.model.orderbook.data import OrderBookData
+from nautilus_trader.model.orders.market import MarketOrder
+from nautilus_trader.trading.strategy import Strategy
+
+
+class MultiTimeframeConfig(StrategyConfig):
+    """
+    Configuration for ``MultiTimeframe`` instances.
+
+    instrument_id : InstrumentId
+        The instrument ID for the strategy.
+    hf_bar_type : BarType
+        The high frequency bar type for the strategy.
+    lf_bar_type : BarType
+        The low frequency bar type for the strategy. 
+    fast_ema_period : int
+        The fast EMA period.
+    slow_ema_period : int
+        The slow EMA period.
+    rsi_period : int
+        The rsi period.
+    rsi_threshold : float 
+        The threshold for determining the rsi signal is long or short.
+    trade_size : str
+        The position size per trade (interpreted as Decimal).
+    order_id_tag : str
+        The unique order ID tag for the strategy. Must be unique
+        amongst all running strategies for a particular trader ID.
+    oms_type : OMSType
+        The order management system type for the strategy. This will determine
+        how the `ExecutionEngine` handles position IDs (see docs).
+    """
+
+    instrument_id: str
+    hf_bar_type: str
+    lf_bar_type: str 
+    fast_ema_period: int = 10
+    slow_ema_period: int = 20
+    rsi_period: int = 14 
+    rsi_threshold: float = 20 
+    trade_size: Decimal
+
+
+class MultiTimeframe(Strategy):
+    """
+    A simple multi-time frame example strategy.
+
+    Parameters
+    ----------
+    config : MultiTimeframeConfig
+        The configuration for the instance.
+    """
+
+    def __init__(self, config: MultiTimeframeConfig):
+        super().__init__(config)
+
+        # Configuration
+        self.instrument_id = InstrumentId.from_str(config.instrument_id)
+        self.hf_bar_type = BarType.from_str(config.hf_bar_type)
+        self.lf_bar_type = BarType.from_str(config.lf_bar_type)
+        self.rsi_threshold = config.rsi_threshold 
+        self.trade_size = Decimal(config.trade_size)
+
+        # Create the indicators for the strategy
+        self.rsi = RelativeStrengthIndex(config.rsi_period) 
+        self.fast_ema = ExponentialMovingAverage(config.fast_ema_period)
+        self.slow_ema = ExponentialMovingAverage(config.slow_ema_period)
+
+        self.ma_trend = 0 
+        self.instrument: Optional[Instrument] = None  # Initialized in on_start
+
+    def on_start(self):
+        """Actions to be performed on strategy start."""
+        self.instrument = self.cache.instrument(self.instrument_id)
+        if self.instrument is None:
+            self.log.error(f"Could not find instrument for {self.instrument_id}")
+            self.stop()
+            return
+
+        # Register the indicators for updating 
+        self.register_indicator_for_bars(self.hf_bar_type, self.rsi)
+        self.register_indicator_for_bars(self.lf_bar_type, self.fast_ema)
+        self.register_indicator_for_bars(self.lf_bar_type, self.slow_ema)
+
+        # Get historical data
+        self.request_bars(self.hf_bar_type)
+        self.request_bars(self.lf_bar_type)
+        # self.request_quote_ticks(self.instrument_id)
+        # self.request_trade_ticks(self.instrument_id)
+
+        # Subscribe to live data
+        self.subscribe_bars(self.hf_bar_type)
+        self.subscribe_bars(self.lf_bar_type)
+        self.subscribe_quote_ticks(self.instrument_id)
+        # self.subscribe_trade_ticks(self.instrument_id)
+        # self.subscribe_ticker(self.instrument_id)  # For debugging
+        # self.subscribe_order_book_deltas(self.instrument_id, depth=20)  # For debugging
+        # self.subscribe_order_book_snapshots(self.instrument_id, depth=20)  # For debugging
+
+    def on_instrument(self, instrument: Instrument):
+        """
+        Actions to be performed when the strategy is running and receives an
+        instrument.
+
+        Parameters
+        ----------
+        instrument : Instrument
+            The instrument received.
+
+        """
+        # For debugging (must add a subscription)
+        # self.log.info(repr(instrument), LogColor.CYAN)
+        pass
+
+    def on_order_book_delta(self, data: OrderBookData):
+        """
+        Actions to be performed when the strategy is running and receives order data.
+
+        Parameters
+        ----------
+        data : OrderBookData
+            The order book data received.
+
+        """
+        # For debugging (must add a subscription)
+        # self.log.info(repr(data), LogColor.CYAN)
+        pass
+
+    def on_order_book(self, order_book: OrderBook):
+        """
+        Actions to be performed when the strategy is running and receives an order book.
+
+        Parameters
+        ----------
+        order_book : OrderBook
+            The order book received.
+
+        """
+        # For debugging (must add a subscription)
+        # self.log.info(repr(order_book), LogColor.CYAN)
+        pass
+
+    def on_ticker(self, ticker: Ticker):
+        """
+        Actions to be performed when the strategy is running and receives a ticker.
+
+        Parameters
+        ----------
+        ticker : Ticker
+            The ticker received.
+
+        """
+        # For debugging (must add a subscription)
+        # self.log.info(repr(ticker), LogColor.CYAN)
+        pass
+
+    def on_quote_tick(self, tick: QuoteTick):
+        """
+        Actions to be performed when the strategy is running and receives a quote tick.
+
+        Parameters
+        ----------
+        tick : QuoteTick
+            The quote tick received.
+
+        """
+        # For debugging (must add a subscription)
+        # self.log.info(repr(tick), LogColor.CYAN)
+        pass
+
+    def on_trade_tick(self, tick: TradeTick):
+        """
+        Actions to be performed when the strategy is running and receives a trade tick.
+
+        Parameters
+        ----------
+        tick : TradeTick
+            The tick received.
+
+        """
+        # For debugging (must add a subscription)
+        # self.log.info(repr(tick), LogColor.CYAN)
+        pass
+
+    def on_bar(self, bar: Bar):
+        """
+        Actions to be performed when the strategy is running and receives a bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The bar received.
+
+        """
+        self.log.info(repr(bar), LogColor.CYAN)
+
+        # Check if indicators ready
+        if not self.indicators_initialized():
+            self.log.info(
+                f"Waiting for indicators to warm up " f"[{self.cache.bar_count(self.hf_bar_type)}]...",
+                color=LogColor.BLUE,
+            )
+            return  # Wait for indicators to warm up...
+        if bar.type == self.hf_bar_type:
+            if self.portfolio.is_flat(self.instrument_id):
+                if self.ma_trend > 0 and self.rsi.value > 50 + self.rsi_threshold:
+                    self.buy() 
+                elif self.ma_trend < 0 and self.rsi.value < 50 - self.rsi_threshold:
+                    self.sell() 
+            elif self.portfolio.is_net_long(self.instrument_id):
+                if self.ma_trend < 0 and self.rsi.value < 50:
+                     self.close_all_positions(self.instrument_id) 
+            elif self.portfolio.is_net_short(self.instrument_id):
+                if self.ma_trend > 0 and self.rsi.value > 50:
+                     self.close_all_positions(self.instrument_id) 
+        elif bar.type == self.lf_bar_type:
+            self.ma_trend = self.fast_ema.value - self.slow_ema.value 
+
+    def buy(self):
+        """
+        Users simple buy method (example).
+        """
+        order: MarketOrder = self.order_factory.market(
+            instrument_id=self.instrument_id,
+            order_side=OrderSide.BUY,
+            quantity=self.instrument.make_qty(self.trade_size),
+            # time_in_force=TimeInForce.FOK,
+        )
+
+        self.submit_order(order)
+
+    def sell(self):
+        """
+        Users simple sell method (example).
+        """
+        order: MarketOrder = self.order_factory.market(
+            instrument_id=self.instrument_id,
+            order_side=OrderSide.SELL,
+            quantity=self.instrument.make_qty(self.trade_size),
+            # time_in_force=TimeInForce.FOK,
+        )
+
+        self.submit_order(order)
+
+    def on_data(self, data: Data):
+        """
+        Actions to be performed when the strategy is running and receives generic data.
+
+        Parameters
+        ----------
+        data : Data
+            The data received.
+
+        """
+        pass
+
+    def on_event(self, event: Event):
+        """
+        Actions to be performed when the strategy is running and receives an event.
+
+        Parameters
+        ----------
+        event : Event
+            The event received.
+
+        """
+        pass
+
+    def on_stop(self):
+        """
+        Actions to be performed when the strategy is stopped.
+        """
+        self.cancel_all_orders(self.instrument_id)
+        self.close_all_positions(self.instrument_id)
+
+        # Unsubscribe from data
+        self.unsubscribe_bars(self.hf_bar_type)
+        self.unsubscribe_bars(self.lf_bar_type)
+        self.unsubscribe_quote_ticks(self.instrument_id)
+        # self.unsubscribe_trade_ticks(self.instrument_id)
+        # self.unsubscribe_ticker(self.instrument_id)
+        # self.unsubscribe_order_book_deltas(self.instrument_id)
+        # self.unsubscribe_order_book_snapshots(self.instrument_id)
+
+    def on_reset(self):
+        """
+        Actions to be performed when the strategy is reset.
+        """
+        # Reset indicators here
+        self.fast_ema.reset()
+        self.slow_ema.reset()
+        self.rsi.reset() 
+
+    def on_save(self) -> Dict[str, bytes]:
+        """
+        Actions to be performed when the strategy is saved.
+
+        Create and return a state dictionary of values to be saved.
+
+        Returns
+        -------
+        dict[str, bytes]
+            The strategy state dictionary.
+
+        """
+        return {}
+
+    def on_load(self, state: Dict[str, bytes]):
+        """
+        Actions to be performed when the strategy is loaded.
+
+        Saved state values will be contained in the give state dictionary.
+
+        Parameters
+        ----------
+        state : dict[str, bytes]
+            The strategy state dictionary.
+
+        """
+        pass
+
+    def on_dispose(self):
+        """
+        Actions to be performed when the strategy is disposed.
+
+        Cleanup any resources used by the strategy here.
+
+        """
+        pass
