f754d936a8b1e154cb57a43c7c20f87d0b0cf7ac
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2022-05-15 13:05:35 +0530

    Use python object reference in logger

--- nautilus_core/common/src/lib.rs
+++ nautilus_core/common/src/lib.rs
@@ -1,15 +1,9 @@
-use logging::{LogFormat, LogLevel, Logger};
 use pyo3::prelude::*;
 
 pub mod logging;
 
 #[pymodule]
 fn common(py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    let logging = PyModule::new(py, "logging")?;
-    logging.add_class::<LogFormat>()?;
-    logging.add_class::<LogLevel>()?;
-    logging.add_class::<Logger>()?;
-
-    m.add_submodule(logging)?;
+    logging::register_module(py, m)?;
     Ok(())
 }
--- nautilus_core/common/src/logging.rs
+++ nautilus_core/common/src/logging.rs
@@ -3,7 +3,7 @@ use std::{
     io::{self, BufWriter, Stderr, Stdout, Write},
 };
 
-use pyo3::prelude::*;
+use pyo3::{prelude::*, types::PyString, Python};
 
 #[pyclass]
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
@@ -46,16 +46,16 @@ pub enum LogFormat {
 impl Display for LogFormat {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let display = match self {
-            LogFormat::HEADER => "\033[95m",
-            LogFormat::GREEN => "\033[92m",
-            LogFormat::BLUE => "\033[94m",
-            LogFormat::MAGENTA => "\033[35m",
-            LogFormat::CYAN => "\033[36m",
-            LogFormat::YELLOW => "\033[1;33m",
-            LogFormat::RED => "\033[1;31m",
-            LogFormat::ENDC => "\033[0m",
-            LogFormat::BOLD => "\033[1m",
-            LogFormat::UNDERLINE => "\033[4m",
+            LogFormat::HEADER => "\x1b[95m",
+            LogFormat::GREEN => "\x1b[92m",
+            LogFormat::BLUE => "\x1b[94m",
+            LogFormat::MAGENTA => "\x1b[35m",
+            LogFormat::CYAN => "\x1b[36m",
+            LogFormat::YELLOW => "\x1b[1;33m",
+            LogFormat::RED => "\x1b[1;31m",
+            LogFormat::ENDC => "\x1b[0m",
+            LogFormat::BOLD => "\x1b[1m",
+            LogFormat::UNDERLINE => "\x1b[4m",
         };
         write!(f, "{}", display)
     }
@@ -74,7 +74,7 @@ impl Logger {
     #[new]
     fn new(trader_id: Option<String>, level_stdout: LogLevel) -> Self {
         Logger {
-            trader_id: trader_id.unwrap_or("TRADER-000".to_string()),
+            trader_id: trader_id.unwrap_or_else(|| "TRADER-000".to_string()),
             level_stdout,
             out: BufWriter::new(io::stdout()),
             err: BufWriter::new(io::stderr()),
@@ -87,11 +87,11 @@ impl Logger {
         timestamp_ns: u64,
         level: LogLevel,
         color: LogFormat,
-        component: String,
-        msg: String,
-    ) {
+        component: &PyString,
+        msg: &PyString,
+    ) -> Result<(), io::Error> {
         let fmt_line = format!(
-            "{bold}{dt}{startc} {color}[{level}] {trader_id}{component}: {msg}{endc}",
+            "{bold}{dt}{startc} {color}[{level}] {trader_id}{component}: {msg}{endc}\n",
             bold = LogFormat::BOLD,
             dt = timestamp_ns,
             startc = LogFormat::ENDC,
@@ -103,29 +103,71 @@ impl Logger {
             endc = LogFormat::ENDC,
         );
         if level >= LogLevel::ERR {
-            self.out.write(fmt_line.as_bytes()).unwrap();
+            self.out.write_all(fmt_line.as_bytes())
         } else if level >= self.level_stdout {
-            self.err.write(fmt_line.as_bytes()).unwrap();
+            self.err.write_all(fmt_line.as_bytes())
+        } else {
+            Ok(())
         }
     }
 
-    fn debug(&mut self, timestamp_ns: u64, color: LogFormat, component: String, msg: String) {
+    fn debug(
+        &mut self,
+        timestamp_ns: u64,
+        color: LogFormat,
+        component: &PyString,
+        msg: &PyString,
+    ) -> Result<(), io::Error> {
         self.log(timestamp_ns, LogLevel::DBG, color, component, msg)
     }
-    fn info(&mut self, timestamp_ns: u64, color: LogFormat, component: String, msg: String) {
+    fn info(
+        &mut self,
+        timestamp_ns: u64,
+        color: LogFormat,
+        component: &PyString,
+        msg: &PyString,
+    ) -> Result<(), io::Error> {
         self.log(timestamp_ns, LogLevel::INF, color, component, msg)
     }
-    fn warning(&mut self, timestamp_ns: u64, color: LogFormat, component: String, msg: String) {
+    fn warning(
+        &mut self,
+        timestamp_ns: u64,
+        color: LogFormat,
+        component: &PyString,
+        msg: &PyString,
+    ) -> Result<(), io::Error> {
         self.log(timestamp_ns, LogLevel::WRN, color, component, msg)
     }
-    fn error(&mut self, timestamp_ns: u64, color: LogFormat, component: String, msg: String) {
+    fn error(
+        &mut self,
+        timestamp_ns: u64,
+        color: LogFormat,
+        component: &PyString,
+        msg: &PyString,
+    ) -> Result<(), io::Error> {
         self.log(timestamp_ns, LogLevel::ERR, color, component, msg)
     }
-    fn critical(&mut self, timestamp_ns: u64, color: LogFormat, component: String, msg: String) {
+    fn critical(
+        &mut self,
+        timestamp_ns: u64,
+        color: LogFormat,
+        component: &PyString,
+        msg: &PyString,
+    ) -> Result<(), io::Error> {
         self.log(timestamp_ns, LogLevel::CRT, color, component, msg)
     }
-    fn flush(&mut self) {
-        self.out.flush().unwrap();
-        self.err.flush().unwrap();
+    fn flush(&mut self) -> Result<(), io::Error> {
+        self.out.flush()?;
+        self.err.flush()
     }
+}
+
+pub fn register_module(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    let logging = PyModule::new(py, "logging")?;
+    logging.add_class::<LogFormat>()?;
+    logging.add_class::<LogLevel>()?;
+    logging.add_class::<Logger>()?;
+
+    m.add_submodule(logging)?;
+    Ok(())
 }
