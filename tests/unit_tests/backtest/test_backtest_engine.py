5a3f98bc8310189d5c44a36638b44901314ac337
George Hill <georgehill010@gmail.com>
2022-06-08 18:24:55 +0100

    Updated assertion value of test_dump_pickled_data test

--- tests/unit_tests/backtest/test_backtest_engine.py
+++ tests/unit_tests/backtest/test_backtest_engine.py
@@ -533,7 +533,7 @@ class TestBacktestWithAddedBars:
 
     def test_dump_pickled_data(self):
         # Arrange, # Act, # Assert
-        assert len(self.engine.dump_pickled_data()) == 7229570
+        assert len(self.engine.dump_pickled_data()) == 5602702
 
     def test_load_pickled_data(self):
         # Arrange
