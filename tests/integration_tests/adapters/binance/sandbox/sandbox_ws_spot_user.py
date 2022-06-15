cb8ca732554dd977a400fdf3c0c06576cc805d19
Jordan Parker <jordanhparker6@gmail.com>
2022-06-15 08:02:50 +0800

    :recycle: adjusting code to fit _template

--- tests/integration_tests/adapters/binance/sandbox/sandbox_ws_spot_user.py
+++ tests/integration_tests/adapters/binance/sandbox/sandbox_ws_spot_user.py
@@ -19,9 +19,9 @@ import os
 import pytest
 
 from nautilus_trader.adapters.binance.common.enums import BinanceAccountType
+from nautilus_trader.adapters.binance.common.websocket.client import BinanceWebSocketClient
 from nautilus_trader.adapters.binance.factories import get_cached_binance_http_client
 from nautilus_trader.adapters.binance.spot.http.user import BinanceSpotUserDataHttpAPI
-from nautilus_trader.adapters.binance.websocket.client import BinanceWebSocketClient
 from nautilus_trader.common.clock import LiveClock
 from nautilus_trader.common.logging import LiveLogger
 from nautilus_trader.common.logging import Logger
