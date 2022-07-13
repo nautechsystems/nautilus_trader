05e2511f9f279914a6e7179e442874d1f0b3c3d0
George Hill <georgehill010@gmail.com>
2022-07-13 16:06:55 +0100

    Util methods to create data for benchmarking/testing the catalog

--- nautilus_core/catalog/src/lib.rs
+++ nautilus_core/catalog/src/lib.rs
@@ -12,4 +12,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 // -------------------------------------------------------------------------------------------------
+#[allow(unused_imports)]
+#[allow(dead_code)]
+#[allow(unused_variables)]
+#[allow(unused_parens)]
 pub mod c_api;
