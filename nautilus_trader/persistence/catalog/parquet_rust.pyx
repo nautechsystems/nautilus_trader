25104f05c60bb1e0dcaca214aca04631ef34f0c6
George Hill <georgehill010@gmail.com>
2022-07-18 19:46:07 +0100

    ParquetReader Cython wrapper
    A Cython wrapper iterator for the Rust ParquetReader<QuoteTick> class

--- nautilus_trader/persistence/catalog/parquet_rust.pyx
+++ nautilus_trader/persistence/catalog/parquet_rust.pyx
@@ -13,35 +13,30 @@
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
 
-from nautilus_trader.core.rust.model cimport Bar_t
 from nautilus_trader.core.rust.model cimport QuoteTick_t
-from nautilus_trader.core.rust.persistence cimport Vec_Bar
-from nautilus_trader.core.rust.persistence cimport Vec_QuoteTick
-from nautilus_trader.core.rust.persistence cimport index_bar_vector
-from nautilus_trader.core.rust.persistence cimport index_quote_tick_vector
-from nautilus_trader.model.data.bar cimport Bar
-from nautilus_trader.model.data.tick cimport QuoteTick
+from nautilus_trader.core.rust.catalog cimport Vec_QuoteTick
+from nautilus_trader.core.rust.catalog cimport quote_tick_reader_new
+from nautilus_trader.core.rust.catalog cimport quote_tick_reader_next
+from nautilus_trader.core.rust.catalog cimport quote_tick_vector_index
 
 
-cdef list parse_quote_tick_vector(Vec_QuoteTick tick_vec):
-    cdef QuoteTick_t _mem
-    cdef QuoteTick tick
-    cdef list ticks = []
-    for i in range(0, tick_vec.len - 1):
-        tick = QuoteTick.__new__(QuoteTick)
-        tick.ts_event = _mem.ts_event
-        tick.ts_init = _mem.ts_init
-        tick._mem = index_quote_tick_vector(&tick_vec, i)[0]
-        ticks.append(tick)
+cdef class QuoteTickReader(ParquetReader):
+    def __init__(self, file_name, batch_size):
+        self.chunk_iterator = quote_tick_reader_new(file_name, batch_size)
 
-
-cdef list parse_bar_vector(Vec_Bar bar_vec):
-    cdef Bar_t _mem
-    cdef Bar bar
-    cdef list bars = []
-    for i in range(0, bar_vec.len - 1):
-        bar = Bar.__new__(Bar)
-        bar.ts_event = _mem.ts_event
-        bar.ts_init = _mem.ts_init
-        bar._mem = index_bar_vector(&bar_vec, i)[0]
-        bars.append(bar)
+    cdef list next(self):
+        cdef Vec_QuoteTick tick_vec = quote_tick_reader_next(self.chunk_iterator)
+        cdef list ticks = self._initialize(tick_vec) # List[QuoteTick]
+        return ticks
+        
+    cdef list _initialize(Vec_QuoteTick tick_vec):
+        cdef QuoteTick_t _mem
+        cdef QuoteTick tick
+        cdef list ticks = []
+        for i in range(0, tick_vec.len - 1):
+            tick = QuoteTick.__new__(QuoteTick)
+            tick.ts_event = _mem.ts_event
+            tick.ts_init = _mem.ts_init
+            tick._mem = quote_tick_vector_index(&tick_vec, i)[0]
+            ticks.append(tick)
+        return ticks
