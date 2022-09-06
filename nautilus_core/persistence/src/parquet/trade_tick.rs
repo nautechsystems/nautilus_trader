97ded66a175738b289dc3f81a3e5d3433e0ecf83
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2022-09-06 19:49:34 +0800

    Handle trade_id conversion for parquet

--- nautilus_core/persistence/src/parquet/trade_tick.rs
+++ nautilus_core/persistence/src/parquet/trade_tick.rs
@@ -13,157 +13,148 @@
 // //  limitations under the License.
 // // -------------------------------------------------------------------------------------------------
 
-// use std::collections::BTreeMap;
-// use std::fmt::Binary;
+use std::collections::BTreeMap;
 
-// use arrow2::array::{BinaryArray, FixedSizeBinaryArray};
-// use arrow2::buffer::Buffer;
-// use arrow2::{
-//     array::{Array, Int64Array, UInt64Array, UInt8Array},
-//     chunk::Chunk,
-//     datatypes::{DataType, Field, Schema},
-//     io::parquet::write::{transverse, Encoding},
-// };
+use arrow2::array::Utf8Array;
 
-// use super::{DecodeFromChunk, EncodeToChunk};
-// use nautilus_model::data::tick::TradeTick;
-// use nautilus_model::enums::OrderSide;
-// use nautilus_model::identifiers::trade_id::TradeId;
-// use nautilus_model::{
-//     identifiers::instrument_id::InstrumentId,
-//     types::{price::Price, quantity::Quantity},
-// };
+use arrow2::{
+    array::{Array, Int64Array, UInt64Array, UInt8Array},
+    chunk::Chunk,
+    datatypes::{DataType, Field, Schema},
+    io::parquet::write::{transverse, Encoding},
+};
 
-// impl EncodeToChunk for TradeTick {
-//     fn encodings(metadata: BTreeMap<String, String>) -> Vec<Vec<Encoding>> {
-//         TradeTick::encode_schema(metadata)
-//             .fields
-//             .iter()
-//             .map(|f| transverse(&f.data_type, |_| Encoding::Plain))
-//             .collect()
-//     }
+use super::{DecodeFromChunk, EncodeToChunk};
+use nautilus_model::data::tick::TradeTick;
+use nautilus_model::enums::OrderSide;
+use nautilus_model::identifiers::trade_id::TradeId;
+use nautilus_model::{
+    identifiers::instrument_id::InstrumentId,
+    types::{price::Price, quantity::Quantity},
+};
 
-//     fn encode_schema(metadata: BTreeMap<String, String>) -> Schema {
-//         let fields = vec![
-//             Field::new("price", DataType::Int64, false),
-//             Field::new("size", DataType::Int64, false),
-//             Field::new("aggressor_side", DataType::UInt8, false),
-//             Field::new("trade_id", DataType::Binary, false),
-//             Field::new("ts_event", DataType::UInt64, false),
-//             Field::new("ts_init", DataType::UInt64, false),
-//         ];
+impl EncodeToChunk for TradeTick {
+    fn encodings(metadata: BTreeMap<String, String>) -> Vec<Vec<Encoding>> {
+        TradeTick::encode_schema(metadata)
+            .fields
+            .iter()
+            .map(|f| transverse(&f.data_type, |_| Encoding::Plain))
+            .collect()
+    }
 
-//         Schema::from(fields).with_metadata(metadata)
-//     }
+    fn encode_schema(metadata: BTreeMap<String, String>) -> Schema {
+        let fields = vec![
+            Field::new("price", DataType::Int64, false),
+            Field::new("size", DataType::Int64, false),
+            Field::new("aggressor_side", DataType::UInt8, false),
+            Field::new("trade_id", DataType::Binary, false),
+            Field::new("ts_event", DataType::UInt64, false),
+            Field::new("ts_init", DataType::UInt64, false),
+        ];
 
-//     #[allow(clippy::type_complexity)]
-//     fn encode(data: Vec<Self>) -> Chunk<Box<dyn Array>> {
-//         let (
-//             mut price_column,
-//             mut size_column,
-//             mut aggressor_side_column,
-//             mut trade_id_column,
-//             mut ts_event_column,
-//             mut ts_init_column,
-//         ): (
-//             Vec<i64>,
-//             Vec<u64>,
-//             Vec<u8>,
-//             Vec<[u8; 36]>,
-//             Vec<u64>,
-//             Vec<u64>,
-//         ) = (vec![], vec![], vec![], vec![], vec![], vec![]);
+        Schema::from(fields).with_metadata(metadata)
+    }
 
-//         data.iter().fold((), |(), tick| {
-//             price_column.push(tick.price.raw);
-//             size_column.push(tick.size.raw);
-//             aggressor_side_column.push(tick.aggressor_side as u8);
-//             trade_id_column
-//                 .push(<[u8; 36]>::try_from(tick.trade_id.to_string().as_bytes()).unwrap());
-//             ts_event_column.push(tick.ts_event);
-//             ts_init_column.push(tick.ts_init);
-//         });
+    #[allow(clippy::type_complexity)]
+    fn encode(data: Vec<Self>) -> Chunk<Box<dyn Array>> {
+        let (
+            mut price_column,
+            mut size_column,
+            mut aggressor_side_column,
+            mut trade_id_column,
+            mut ts_event_column,
+            mut ts_init_column,
+        ): (Vec<i64>, Vec<u64>, Vec<u8>, Vec<&str>, Vec<u64>, Vec<u64>) =
+            (vec![], vec![], vec![], vec![], vec![], vec![]);
 
-//         let price_array = Int64Array::from_vec(price_column);
-//         let size_array = UInt64Array::from_vec(size_column);
-//         let aggressor_side_array = UInt8Array::from_vec(aggressor_side_column);
-//         let trade_id_array =
-//             BinaryArray::<i32>::from_data(DataType::Binary, Buffer::from(trade_id_column), None);
-//         let ts_event_array = UInt64Array::from_vec(ts_event_column);
-//         let ts_init_array = UInt64Array::from_vec(ts_init_column);
-//         Chunk::new(vec![
-//             price_array.to_boxed(),
-//             size_array.to_boxed(),
-//             aggressor_side_array.to_boxed(),
-//             trade_id_array.to_boxed(),
-//             ts_event_array.to_boxed(),
-//             ts_init_array.to_boxed(),
-//         ])
-//     }
-// }
+        data.iter().fold((), |(), tick| {
+            price_column.push(tick.price.raw);
+            size_column.push(tick.size.raw);
+            aggressor_side_column.push(tick.aggressor_side as u8);
+            trade_id_column.push(&tick.trade_id.value);
+            ts_event_column.push(tick.ts_event);
+            ts_init_column.push(tick.ts_init);
+        });
 
-// impl DecodeFromChunk for TradeTick {
-//     fn decode(schema: &Schema, cols: Chunk<Box<dyn Array>>) -> Vec<Self> {
-//         let instrument_id = InstrumentId::from(schema.metadata.get("instrument_id").unwrap());
-//         let price_precision = schema
-//             .metadata
-//             .get("price_precision")
-//             .unwrap()
-//             .parse::<u8>()
-//             .unwrap();
-//         let size_precision = schema
-//             .metadata
-//             .get("size_precision")
-//             .unwrap()
-//             .parse::<u8>()
-//             .unwrap();
+        let price_array = Int64Array::from_vec(price_column);
+        let size_array = UInt64Array::from_vec(size_column);
+        let aggressor_side_array = UInt8Array::from_vec(aggressor_side_column);
+        let trade_id_array = Utf8Array::<i32>::from_iter_values(trade_id_column.into_iter());
+        let ts_event_array = UInt64Array::from_vec(ts_event_column);
+        let ts_init_array = UInt64Array::from_vec(ts_init_column);
+        Chunk::new(vec![
+            price_array.to_boxed(),
+            size_array.to_boxed(),
+            aggressor_side_array.to_boxed(),
+            trade_id_array.to_boxed(),
+            ts_event_array.to_boxed(),
+            ts_init_array.to_boxed(),
+        ])
+    }
+}
 
-//         // extract field value arrays from chunk separately
-//         let price_values = cols.arrays()[0]
-//             .as_any()
-//             .downcast_ref::<Int64Array>()
-//             .unwrap();
-//         let size_values = cols.arrays()[1]
-//             .as_any()
-//             .downcast_ref::<UInt64Array>()
-//             .unwrap();
-//         let aggressor_side_values = cols.arrays()[2]
-//             .as_any()
-//             .downcast_ref::<UInt8Array>()
-//             .unwrap();
-//         let trade_id_values_values = cols.arrays()[3]
-//             .as_any()
-//             .downcast_ref::<FixedSizeBinaryArray>()
-//             .unwrap();
-//         let ts_event_values = cols.arrays()[4]
-//             .as_any()
-//             .downcast_ref::<UInt64Array>()
-//             .unwrap();
-//         let ts_init_values = cols.arrays()[4]
-//             .as_any()
-//             .downcast_ref::<UInt64Array>()
-//             .unwrap();
+impl DecodeFromChunk for TradeTick {
+    fn decode(schema: &Schema, cols: Chunk<Box<dyn Array>>) -> Vec<Self> {
+        let instrument_id = InstrumentId::from(schema.metadata.get("instrument_id").unwrap());
+        let price_precision = schema
+            .metadata
+            .get("price_precision")
+            .unwrap()
+            .parse::<u8>()
+            .unwrap();
+        let size_precision = schema
+            .metadata
+            .get("size_precision")
+            .unwrap()
+            .parse::<u8>()
+            .unwrap();
 
-//         // construct iterator of values from field value arrays
-//         let values = price_values
-//             .into_iter()
-//             .zip(size_values.into_iter())
-//             .zip(aggressor_side_values.into_iter())
-//             .zip(trade_id_values_values.into_iter())
-//             .zip(ts_event_values.into_iter())
-//             .zip(ts_init_values.into_iter())
-//             .map(
-//                 |(((((price, size), aggressor_side), trade_id), ts_event), ts_init)| TradeTick {
-//                     instrument_id: instrument_id.clone(),
-//                     price: Price::from_raw(*price.unwrap(), price_precision),
-//                     size: Quantity::from_raw(*size.unwrap(), size_precision),
-//                     aggressor_side: OrderSide::from(*aggressor_side.unwrap()),
-//                     trade_id: TradeId::from(*trade_id.unwrap()),
-//                     ts_event: *ts_event.unwrap(),
-//                     ts_init: *ts_init.unwrap(),
-//                 },
-//             );
+        // extract field value arrays from chunk separately
+        let price_values = cols.arrays()[0]
+            .as_any()
+            .downcast_ref::<Int64Array>()
+            .unwrap();
+        let size_values = cols.arrays()[1]
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
+        let aggressor_side_values = cols.arrays()[2]
+            .as_any()
+            .downcast_ref::<UInt8Array>()
+            .unwrap();
+        let trade_id_values_values = cols.arrays()[3]
+            .as_any()
+            .downcast_ref::<Utf8Array<i32>>()
+            .unwrap();
+        let ts_event_values = cols.arrays()[4]
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
+        let ts_init_values = cols.arrays()[4]
+            .as_any()
+            .downcast_ref::<UInt64Array>()
+            .unwrap();
 
-//         values.collect()
-//     }
-// }
+        // construct iterator of values from field value arrays
+        let values = price_values
+            .into_iter()
+            .zip(size_values.into_iter())
+            .zip(aggressor_side_values.into_iter())
+            .zip(trade_id_values_values.into_iter())
+            .zip(ts_event_values.into_iter())
+            .zip(ts_init_values.into_iter())
+            .map(
+                |(((((price, size), aggressor_side), trade_id), ts_event), ts_init)| TradeTick {
+                    instrument_id: instrument_id.clone(),
+                    price: Price::from_raw(*price.unwrap(), price_precision),
+                    size: Quantity::from_raw(*size.unwrap(), size_precision),
+                    aggressor_side: OrderSide::from(*aggressor_side.unwrap()),
+                    trade_id: TradeId::from(trade_id.unwrap()),
+                    ts_event: *ts_event.unwrap(),
+                    ts_init: *ts_init.unwrap(),
+                },
+            );
+
+        values.collect()
+    }
+}
