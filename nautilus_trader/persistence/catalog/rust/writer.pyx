a39c36b42d031707850959425c8168546ebfad99
cjdsellers <chris@cjdsellers.io>
2022-09-09 22:26:31 +1000

    Cleanups

--- nautilus_trader/persistence/catalog/rust/writer.pyx
+++ nautilus_trader/persistence/catalog/rust/writer.pyx
@@ -1,25 +1,30 @@
-from cpython.object cimport PyObject
-from cython.operator cimport dereference
-from libc.stdlib cimport free
-from libc.stdlib cimport malloc
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2022 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
 
 import os
 
-from nautilus_trader.core.rust.core cimport CVec
-from nautilus_trader.core.rust.core cimport cvec_drop
-from nautilus_trader.core.rust.core cimport cvec_new
-from nautilus_trader.core.rust.model cimport QuoteTick_t
-from nautilus_trader.core.rust.model cimport TradeTick_t
-from nautilus_trader.core.rust.persistence cimport parquet_writer_chunk_append
-from nautilus_trader.core.rust.persistence cimport parquet_writer_drop
-from nautilus_trader.core.rust.persistence cimport parquet_writer_new
-from nautilus_trader.core.rust.persistence cimport parquet_writer_write
-
 from nautilus_trader.persistence.catalog.rust.common import py_type_to_parquet_type
 
+from cpython.object cimport PyObject
+
 from nautilus_trader.core.correctness cimport Condition
+from nautilus_trader.core.rust.core cimport CVec
+from nautilus_trader.core.rust.core cimport cvec_new
+from nautilus_trader.core.rust.persistence cimport parquet_writer_chunk_append
+from nautilus_trader.core.rust.persistence cimport parquet_writer_new
 from nautilus_trader.model.data.tick cimport QuoteTick
-from nautilus_trader.model.data.tick cimport TradeTick
 
 
 cdef class ParquetWriter:
@@ -30,7 +35,7 @@ cdef class ParquetWriter:
         self,
         str file_path,
         type parquet_type,
-        dict metadata = {"key":"value"} # TODO
+        dict metadata = {"key": "value"}, # TODO
     ):
         Condition.valid_string(file_path, "file_path")
 
@@ -41,20 +46,20 @@ cdef class ParquetWriter:
 
         self._parquet_type = py_type_to_parquet_type(parquet_type)
         self._writer = parquet_writer_new(
-                            <PyObject *>file_path,
-                            self._parquet_type,
-                            <PyObject *>metadata)
+            <PyObject *>file_path,
+            self._parquet_type,
+            <PyObject *>metadata,
+        )
 
     cpdef void write(self, list items):
         self._write(items)
 
     cdef void _write(self, list items):
+        cdef CVec chunk = cvec_new()
 
         cdef:
-            CVec chunk = cvec_new()
             QuoteTick tick
             void *item
-
         tick = <QuoteTick>items[0]
         item = <void *>&tick._mem
 
