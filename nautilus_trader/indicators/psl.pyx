c63660655166f14b9bba83bef7196ee89ab3b7f1
graceyangfan <yf1825617022@outlook.com>
2022-09-10 11:11:20 +0800

    Add PsychologicalLine indicator

---
+++ nautilus_trader/indicators/psl.pyx
@@ -0,0 +1,107 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2022 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
+from nautilus_trader.indicators.average.ma_factory import MovingAverageFactory
+from nautilus_trader.indicators.average.ma_factory import MovingAverageType
+
+from nautilus_trader.core.correctness cimport Condition
+from nautilus_trader.indicators.base.indicator cimport Indicator
+from nautilus_trader.model.data.bar cimport Bar
+
+
+cdef class PsychologicalLine(Indicator):
+    """
+    The Psychological Line is an oscillator-type indicator that compares the
+    number of the rising periods to the total number of periods. In other
+    words, it is the percentage of bars that close above the previous
+    bar over a given period.
+
+    Parameters
+    ----------
+    period : int
+        The rolling window period for the indicator (> 0).
+    ma_type : MovingAverageType
+        The moving average type for the indicator (cannot be None).
+    """
+
+    def __init__(
+        self,
+        int period,
+        ma_type not None: MovingAverageType=MovingAverageType.SIMPLE,
+    ):
+        Condition.positive_int(period, "period")
+        params = [
+            period,
+            ma_type.name,
+        ]
+        super().__init__(params=params)
+
+        self.period = period
+        self._ma = MovingAverageFactory.create(period, ma_type)
+        self._diff = 0
+        self._previous_close = 0
+        self.value = 0
+
+    cpdef void handle_bar(self, Bar bar) except *:
+        """
+        Update the indicator with the given bar.
+
+        Parameters
+        ----------
+        bar : Bar
+            The update bar.
+
+        """
+        Condition.not_none(bar, "bar")
+
+        self.update_raw(
+            bar.close.as_double(),
+        )
+
+    cpdef void update_raw(
+        self,
+        double close,
+    ) except *:
+        """
+        Update the indicator with the given raw values.
+
+        Parameters
+        ----------
+        close : double
+            The close price.
+
+        """
+        # Update inputs
+        if not self.has_inputs:
+            self._previous_close = close
+
+        self._diff = close - self._previous_close
+        if self._diff <= 0:
+            self._ma.update_raw(0)
+        else:
+            self._ma.update_raw(1)
+        self.value = 100.0 * self._ma.value
+
+        if not self.initialized:
+            self._set_has_inputs(True)
+            if self._ma.initialized:
+                self._set_initialized(True)
+        self._previous_close = close
+
+    cpdef void _reset(self) except *:
+        self._ma.reset()
+        self._diff = 0
+        self._previous_close = 0
+        self.value = 0
