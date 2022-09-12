4a0a374b7d6ca0e8002dcdff036f1e6b6bf2777f
ghill2 <georgehill010@gmail.com>
2022-09-13 00:00:13 +0100

    Parquet writer vec (#773)
    * Add work in progress
    
    * Update work in progress
    
    * Update work in progress
    
    * Add c integrations for parquet reader writer
    
    * Update Cython bindings
    
    * Handle trade_id conversion for parquet
    
    * Add Cython interface
    
    * Implement `read_parquet_quote_ticks`
    
    * ParquetReader Cython wrapper (#763)
    
    * Expose rust CVec to Cython
    
    * Expose persistence C-API to Cython
    
    By adding #[no_mangle] the C-API methods are exposed to Cython.
    
    * ParquetReader index chunk C-API method
    
    * ParquetReader Cython wrapper class
    
    * Pre-commit & Build
    
    * Light refactoring and optimizations
    
    * Add trade tick and expose more functionality
    
    * Remove redundant enum
    
    * Implement ParquetWriter (#765)
    
    * Ran pre-commit
    
    * Writer takes slices instead of owned vecs
    
    * Transfer vector of QuoteTick_t structs to rust
    
    * Cleanup
    
    * Pre-commit
    
    * Pre-commit
    
    * End writer when dropping it
    
    Co-authored-by: Ishan Bhanuka <bhanuka.ishan@gmail.com>

---
+++ nautilus_trader/persistence/catalog/rust/vec.pyx
@@ -0,0 +1,15 @@
+# distutils: language = c++
+from libcpp.vector cimport vector
+
+from nautilus_trader.core.rust.model cimport QuoteTick_t
+from nautilus_trader.model.data.tick cimport QuoteTick
+
+
+cdef void* create_vector(list items):
+    if isinstance(items[0], QuoteTick):
+        return _create_quote_tick_vector(items)
+
+cdef void* _create_quote_tick_vector(list items):
+    cdef vector[QuoteTick_t] vec
+    [vec.push_back(<QuoteTick_t>(<QuoteTick>item)._mem) for item in items]
+    return <void*>vec.data()
--- nautilus_trader/persistence/catalog/rust/writer_test.py
+++ nautilus_trader/persistence/catalog/rust/writer_test.py
@@ -1,13 +1,15 @@
+import itertools
 import os
 
 from nautilus_trader.model.data.tick import QuoteTick
 from nautilus_trader.model.identifiers import InstrumentId
 from nautilus_trader.model.objects import Price
 from nautilus_trader.model.objects import Quantity
+from nautilus_trader.persistence.catalog.rust.reader import ParquetReader
 from nautilus_trader.persistence.catalog.rust.writer import ParquetWriter
 
 
-if __name__ == "__main__":
+def test_parquet_writer_round_trip():
     n = 100
     ticks = [
         QuoteTick(
@@ -20,7 +22,22 @@ if __name__ == "__main__":
             0,
         )
     ] * n
-
     file_path = os.path.expanduser("~/Desktop/test_parquet_writer.parquet")
-    writer = ParquetWriter(file_path, QuoteTick)
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    metadata = {
+        "instrument_id": "EUR/USD.DUKA",
+        "price_precision": "4",
+        "size_precision": "4"
+    }
+    writer = ParquetWriter(file_path, QuoteTick, metadata)
     writer.write(ticks)
+    writer.drop()
+
+    reader = ParquetReader(file_path, QuoteTick)
+    ticks = list(itertools.chain(*list(reader)))
+    print(ticks)
+
+
+if __name__ == "__main__":
+    test_parquet_writer_round_trip()
