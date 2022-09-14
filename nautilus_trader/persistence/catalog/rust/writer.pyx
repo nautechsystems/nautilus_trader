13d0fab6eb8cd422aaf091d9c1d01c511de7b6a5
cjdsellers <chris@cjdsellers.io>
2022-09-15 06:33:21 +1000

    Fix some formatting

--- nautilus_trader/persistence/catalog/rust/writer.pyx
+++ nautilus_trader/persistence/catalog/rust/writer.pyx
@@ -1,3 +1,18 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2022 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
 import os
 
 from cpython.object cimport PyObject
@@ -35,9 +50,10 @@ cdef class ParquetWriter:
 
         self._parquet_type = py_type_to_parquet_type(parquet_type)
         self._writer = parquet_writer_new(
-                            <PyObject *>file_path,
-                            self._parquet_type,
-                            <PyObject *>metadata)
+            <PyObject *>file_path,
+            self._parquet_type,
+            <PyObject *>metadata,
+        )
 
     def write(self, list items):
         parquet_writer_write(
@@ -46,6 +62,6 @@ cdef class ParquetWriter:
             <void *>create_vector(items),
             len(items)
         )
-        
+
     def drop(self):
         parquet_writer_drop(self._writer, self._parquet_type)
--- nautilus_trader/persistence/catalog/rust/writer_test.py
+++ nautilus_trader/persistence/catalog/rust/writer_test.py
@@ -1,3 +1,18 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2022 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
 import itertools
 import os
 
@@ -25,11 +40,7 @@ def test_parquet_writer_round_trip():
     file_path = os.path.expanduser("~/Desktop/test_parquet_writer.parquet")
     if os.path.exists(file_path):
         os.remove(file_path)
-    metadata = {
-        "instrument_id": "EUR/USD.DUKA",
-        "price_precision": "4",
-        "size_precision": "4"
-    }
+    metadata = {"instrument_id": "EUR/USD.DUKA", "price_precision": "4", "size_precision": "4"}
     writer = ParquetWriter(file_path, QuoteTick, metadata)
     writer.write(ticks)
     writer.drop()
