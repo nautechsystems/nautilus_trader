f0fe066917cecc5726b3040cf9b6d2860b44087d
Chris Sellers <chris@cjdsellers.io>
2022-10-20 21:57:00 +1100

    Cleanup build

--- build.py
+++ build.py
@@ -115,7 +115,7 @@ def _build_extensions() -> List[Extension]:
 
     extra_compile_args = []
     if BUILD_MODE == "release" and platform.system() != "Windows":
-        # extra_compile_args.append("-O2")  # Temporary to tweak total build size
+        extra_compile_args.append("-O2")  # Temporary to tweak total build size
         extra_compile_args.append("-pipe")
 
     extra_link_args = RUST_LIBS
