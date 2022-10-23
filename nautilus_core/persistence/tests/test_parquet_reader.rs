ef6bd194c2e0ce5eb739ed3e9bbdcf7faaed42c7
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2022-10-23 12:40:02 +0800

    Fix dropping CVec to prevent memory leak (#824)
    * Fix cvec dropping
    
    * Add working example to test leaks

--- nautilus_core/persistence/tests/test_parquet_reader.rs
+++ nautilus_core/persistence/tests/test_parquet_reader.rs
@@ -13,46 +13,47 @@
 // //  limitations under the License.
 // // -------------------------------------------------------------------------------------------------
 //
-// use pyo3::{AsPyPointer, prelude::*, types::*};
-//
-// use nautilus_core::cvec::CVec;
-// use nautilus_persistence::c_api::quote_tick::{
-//     parquet_reader_drop, parquet_reader_drop_chunk, parquet_reader_new, parquet_reader_next_chunk,
-//     ParquetReaderType,
-// };
-//
-// mod test_util;
-//
-// #[test]
-// #[allow(unused_assignments)]
-// fn test_parquet_reader() {
-//     pyo3::prepare_freethreaded_python();
-//
-//     let file_path = "../../tests/test_kit/data/quote_tick_data.parquet";
-//
-//     // return an opaque reader pointer
-//     let file_path = Python::with_gil(|py| PyString::new(py, file_path)).as_ptr();
-//
-//     let reader = unsafe { parquet_reader_new(file_path, ParquetReaderType::QuoteTick) };
-//
-//     let mut total = 0;
-//     let mut chunk = CVec::default();
-//     unsafe {
-//         loop {
-//             chunk = parquet_reader_next_chunk(reader, ParquetReaderType::QuoteTick);
-//             if chunk.len == 0 {
-//                 parquet_reader_drop_chunk(chunk, ParquetReaderType::QuoteTick);
-//                 break;
-//             } else {
-//                 total += chunk.len;
-//                 parquet_reader_drop_chunk(chunk, ParquetReaderType::QuoteTick);
-//             }
-//         }
-//     }
-//
-//     unsafe {
-//         parquet_reader_drop(reader, ParquetReaderType::QuoteTick);
-//     }
-//
-//     assert_eq!(total, 9500);
-// }
+use pyo3::{prelude::*, types::*, AsPyPointer};
+
+use nautilus_core::cvec::CVec;
+use nautilus_persistence::parquet::{
+    parquet_reader_drop, parquet_reader_drop_chunk, parquet_reader_new, parquet_reader_next_chunk,
+    ParquetType,
+};
+
+mod test_util;
+
+#[test]
+#[allow(unused_assignments)]
+fn test_parquet_reader() {
+    pyo3::prepare_freethreaded_python();
+
+    let file_path = "../../tests/test_kit/data/quote_tick_data.parquet";
+
+    // return an opaque reader pointer
+    let reader = Python::with_gil(|py| {
+        let file_path = PyString::new(py, file_path);
+        unsafe { parquet_reader_new(file_path.as_ptr(), ParquetType::QuoteTick, 100) }
+    });
+
+    let mut total = 0;
+    let mut chunk = CVec::default();
+    unsafe {
+        loop {
+            chunk = parquet_reader_next_chunk(reader, ParquetType::QuoteTick);
+            if chunk.len == 0 {
+                parquet_reader_drop_chunk(chunk, ParquetType::QuoteTick);
+                break;
+            } else {
+                total += chunk.len;
+                parquet_reader_drop_chunk(chunk, ParquetType::QuoteTick);
+            }
+        }
+    }
+
+    unsafe {
+        parquet_reader_drop(reader, ParquetType::QuoteTick);
+    }
+
+    assert_eq!(total, 9500);
+}
