376d17fea50e48ecfe403d26fbb8bc23e2b5ff4f
Bshara Zahran <bshara23@gmail.com>
2022-11-04 22:49:47 +0300

    Fix to return single latest record for Bar and Instrument from Parquet Catalog (#839)
    This is the fix I have made for duplicate records received for dataframe/nautilus objects and part is reported in #833 . This makes sure that the returned records are the latest single record being returned. Also subset keyword is added to allow custom unique filtering for any other classes not added above.

--- nautilus_trader/persistence/catalog/parquet.py
+++ nautilus_trader/persistence/catalog/parquet.py
@@ -123,6 +123,18 @@ class ParquetDataCatalog(BaseDataCatalog):
         table = dataset.to_table(filter=combine_filters(*filters), **(table_kwargs or {}))
         mappings = self.load_inverse_mappings(path=full_path)
 
+        table = table.to_pandas()
+        if cls.__base__ == Instrument:
+            table = table.sort_values('ts_init').drop_duplicates(
+                subset=['id'], keep=kwargs.get('keep', str('last'))
+            )
+        elif cls == Bar:
+            table = table.sort_values('ts_init').drop_duplicates(
+                subset=['bar_type', 'ts_event'], keep=kwargs.get('keep', str('last'))
+            )
+        else:
+            table = table.drop_duplicates()
+            
         # TODO: Un-wired rust parquet reader
         # if isinstance(cls, QuoteTick):
         #     reader = ParquetReader(file_path=full_path, parquet_type=QuoteTick)  # noqa
@@ -134,6 +146,7 @@ class ParquetDataCatalog(BaseDataCatalog):
                 table=table, mappings=mappings, raise_on_empty=raise_on_empty, **kwargs
             )
         else:
+            table = pa.Table.from_pandas(table)
             return self._handle_table_nautilus(table=table, cls=cls, mappings=mappings)
 
     def load_inverse_mappings(self, path):
@@ -144,13 +157,12 @@ class ParquetDataCatalog(BaseDataCatalog):
 
     @staticmethod
     def _handle_table_dataframe(
-        table: pa.Table,
+        table: pd.DataFrame,
         mappings: Optional[dict],
         raise_on_empty: bool = True,
         sort_columns: Optional[list] = None,
         as_type: Optional[dict] = None,
     ):
-        df = table.to_pandas().drop_duplicates()
         for col in mappings:
             df.loc[:, col] = df[col].map(mappings[col])
 
