b48410b2438b9fd6acf4dad82ed4bde33759d3d2
Bshara Zahran <bshara23@gmail.com>
2022-11-23 20:46:40 +1100

    Fix docstring

--- nautilus_trader/config/live.py
+++ nautilus_trader/config/live.py
@@ -181,9 +181,9 @@ class TradingNodeConfig(NautilusKernelConfig):
         The execution client configurations.
     strategies : list[ImportableStrategyConfig]
         The strategy configurations for the node.
-    load_strategy_state : bool, default True
+    load_state : bool, default True
         If trading strategy state should be loaded from the database on start.
-    save_strategy_state : bool, default True
+    save_state : bool, default True
         If trading strategy state should be saved to the database on stop.
     log_level : str, default "INFO"
         The stdout log level for the node.
