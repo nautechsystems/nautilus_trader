19a8318bdf1dfe24c219d25ab72988f17e36941a
limx0 <bradley.mcelroy@live.com>
2022-11-30 08:24:03 +1100

    minor cleanup

--- nautilus_trader/backtest/node.py
+++ nautilus_trader/backtest/node.py
@@ -198,7 +198,7 @@ class BacktestNode:
 
     def _load_engine_data(self, engine: BacktestEngine, data) -> None:
         if is_nautilus_class(data["type"]):
-            engine.add_data(data["data"], data.get("client_id"))
+            engine.add_data(data=data["data"])
         else:
             if "client_id" not in data:
                 raise ValueError(
