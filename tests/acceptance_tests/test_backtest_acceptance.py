c80d3e7508a479e9bdb2ba91c69d8814838f5516
Chris Sellers <chris@cjdsellers.io>
2022-12-06 21:29:32 +1100

    Fix test

--- tests/acceptance_tests/test_backtest_acceptance.py
+++ tests/acceptance_tests/test_backtest_acceptance.py
@@ -715,6 +715,6 @@ class TestBacktestAcceptanceTestsMarketMaking:
         # TODO - Unsure why this is not deterministic ?
         assert self.engine.iteration in (7812, 8199, 9319)
         assert self.engine.portfolio.account(self.venue).balance_total(GBP) == Money(
-            "10000.00",
+            "9999.68",
             GBP,
         )
