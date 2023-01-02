53ec8671e46cdbd4305530eb4eefe42883750774
Bshara Zahran <bshara23@gmail.com>
2023-01-02 14:09:24 +1100

    Refine poetry all extras install

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export PROFILE_MODE=true
-poetry install --with test --extras "betfair docker ib redis"
+poetry install --with test --all-extras
 poetry run pytest --ignore=tests/performance_tests --cov-report=term --cov-report=xml --cov=nautilus_trader --new-first --failed-first
