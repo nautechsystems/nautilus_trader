4c64e0a9af1c483939b9afcfa0bb5da7f526d3ff
Bshara Zahran <bshara23@gmail.com>
2023-01-02 18:38:58 +1100

    Import cleanups

--- nautilus_trader/adapters/interactive_brokers/parsing/execution.py
+++ nautilus_trader/adapters/interactive_brokers/parsing/execution.py
@@ -12,6 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
+
 from itertools import groupby
 
 from ib_insync import AccountValue
