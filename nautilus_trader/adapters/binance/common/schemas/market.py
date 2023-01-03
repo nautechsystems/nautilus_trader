1cd8caabd5995ad8cce603945c04e3ba03a9672c
Reece Kibble <reecek@uniciant.com>
2023-01-03 08:42:34 +0000

    binance/common/schemas/market.py: Add remaining market responses from endpoints.

--- nautilus_trader/adapters/binance/common/schemas/market.py
+++ nautilus_trader/adapters/binance/common/schemas/market.py
@@ -23,14 +23,57 @@ from nautilus_trader.core.datetime import millis_to_nanos
 from nautilus_trader.model.data.bar import BarType
 from nautilus_trader.model.data.tick import TradeTick
 from nautilus_trader.model.enums import AggressorSide
+from nautilus_trader.model.enums import BookType
 from nautilus_trader.model.identifiers import InstrumentId
 from nautilus_trader.model.identifiers import TradeId
 from nautilus_trader.model.objects import Price
 from nautilus_trader.model.objects import Quantity
+from nautilus_trader.model.orderbook.data import OrderBookSnapshot
+
+
+class BinanceTime(msgspec.Struct, frozen=True):
+    """
+    Schema of current server time
+    GET response of `time`
+    """
+
+    serverTime: int
+
+
+class BinanceDepth(msgspec.Struct, frozen=True):
+    """
+    Schema of a binance orderbook depth.
+    GET response of `depth`
+    """
+
+    lastUpdateId: int
+    bids: list[tuple[str, str]]
+    asks: list[tuple[str, str]]
+
+    symbol: Optional[str] = None  # COIN-M FUTURES only
+    pair: Optional[str] = None  # COIN-M FUTURES only
+
+    E: Optional[int] = None  # FUTURES only, Message output time
+    T: Optional[int] = None  # FUTURES only, Transaction time
+
+    def _parse_to_order_book_snapshot(
+        self,
+        instrument_id: InstrumentId,
+        ts_init: int,
+    ) -> OrderBookSnapshot:
+        return OrderBookSnapshot(
+            instrument_id=instrument_id,
+            book_type=BookType.L2_MBP,
+            bids=[[float(o[0]), float(o[1])] for o in self.bids or []],
+            asks=[[float(o[0]), float(o[1])] for o in self.asks or []],
+            ts_event=ts_init,
+            ts_init=ts_init,
+            update_id=self.lastUpdateId or 0,
+        )
 
 
 class BinanceTrade(msgspec.Struct, frozen=True):
-    """Schema of a single trade. Used in recent and historical trade lookups."""
+    """Schema of a single trade."""
 
     id: int
     price: str
@@ -44,7 +87,7 @@ class BinanceTrade(msgspec.Struct, frozen=True):
         self,
         instrument_id: InstrumentId,
         ts_init: int,
-    ) -> list[TradeTick]:
+    ) -> TradeTick:
         """Parse Binance trade to internal TradeTick"""
         return TradeTick(
             instrument_id=instrument_id,
@@ -55,6 +98,30 @@ class BinanceTrade(msgspec.Struct, frozen=True):
             ts_event=millis_to_nanos(self.time),
             ts_init=ts_init,
         )
+
+
+class BinanceTrades(msgspec.Struct, frozen=True):
+    """
+    Schema of list of trades.
+    GET response of `trades` and `historicalTrades`
+    """
+
+    trades: list[BinanceTrade]
+
+    def _parse_to_trade_ticks(
+        self,
+        instrument_id: InstrumentId,
+        ts_init: int,
+    ) -> list[TradeTick]:
+        """Parse Binance response to internal TradeTicks"""
+        ticks: list[TradeTick] = [
+            trade._parse_to_trade_tick(
+                instrument_id=instrument_id,
+                ts_init=ts_init,
+            )
+            for trade in self.trades
+        ]
+        return ticks
 
 
 class BinanceAggTrade(msgspec.Struct, frozen=True):
@@ -68,6 +135,15 @@ class BinanceAggTrade(msgspec.Struct, frozen=True):
     T: int  # Timestamp
     m: bool  # Was the buyer the maker?
     M: Optional[bool] = None  # SPOT/MARGIN only, was the trade the best price match?
+
+
+class BinanceAggTrades(msgspec.Struct, frozen=True):
+    """
+    Schema of list of aggregate trades
+    GET response of `aggTrades`
+    """
+
+    trades: list[BinanceAggTrade]
 
 
 class BinanceKline(msgspec.Struct, array_like=True):
@@ -108,20 +184,40 @@ class BinanceKline(msgspec.Struct, array_like=True):
         )
 
 
+class BinanceKlines(msgspec.Struct, frozen=True):
+    """
+    Schema of list of binance klines
+    GET response of `klines`
+    """
+
+    klines: list[BinanceKline]
+
+    def _parse_to_binance_bars(
+        self,
+        bar_type: BarType,
+        ts_init: int,
+    ) -> list[BinanceBar]:
+        """Parse klines to BinanceBars"""
+        bars: list[BinanceBar] = [
+            kline._parse_to_binance_bar(bar_type, ts_init) for kline in self.klines
+        ]
+        return bars
+
+
 class BinanceTicker24hr(msgspec.Struct, frozen=True):
     """Schema of single Binance 24hr ticker (FULL/MINI)"""
 
-    symbol: str
-    lastPrice: str
-    openPrice: str
-    highPrice: str
-    lowPrice: str
-    volume: str
-    openTime: int
-    closeTime: int
-    firstId: int
-    lastId: int
-    count: int
+    symbol: Optional[str]
+    lastPrice: Optional[str]
+    openPrice: Optional[str]
+    highPrice: Optional[str]
+    lowPrice: Optional[str]
+    volume: Optional[str]
+    openTime: Optional[int]
+    closeTime: Optional[int]
+    firstId: Optional[int]
+    lastId: Optional[int]
+    count: Optional[int]
 
     priceChange: Optional[str] = None  # FULL response only (SPOT/MARGIN)
     priceChangePercent: Optional[str] = None  # FULL response only (SPOT/MARGIN)
@@ -140,22 +236,52 @@ class BinanceTicker24hr(msgspec.Struct, frozen=True):
     quoteVolume: Optional[str] = None  # SPOT/MARGIN & USD-M FUTURES only
 
 
+class BinanceTicker24hrs(BinanceTicker24hr):
+    """
+    GET response of `ticker/24hr`
+    Single BinanceTicker24hr or list of BinanceTicker24hr,
+    depending on parameters.
+    """
+
+    tickers: Optional[list[BinanceTicker24hr]] = None
+
+
 class BinanceTickerPrice(msgspec.Struct, frozen=True):
     """Schema of single Binance Price Ticker"""
 
-    symbol: str
-    price: str
+    symbol: Optional[str]
+    price: Optional[str]
     time: Optional[int] = None  # FUTURES only
     ps: Optional[str] = None  # COIN-M FUTURES only, pair
 
 
-class BinanceTickerBook(msgspec.Struct):
+class BinanceTickerPrices(BinanceTickerPrice):
+    """
+    GET response of `ticker/price`
+    Single BinanceTickerPrice or list of BinanceTickerPrice,
+    depending on parameters and exchange type.
+    """
+
+    tickers: Optional[list[BinanceTickerPrice]] = None
+
+
+class BinanceTickerBook(msgspec.Struct, frozen=True):
     """Schema of a single Binance Order Book Ticker"""
 
-    symbol: str
-    bidPrice: str
-    bidQty: str
-    askPrice: str
-    askQty: str
+    symbol: Optional[str]
+    bidPrice: Optional[str]
+    bidQty: Optional[str]
+    askPrice: Optional[str]
+    askQty: Optional[str]
     pair: Optional[str] = None  # USD-M FUTURES only
     time: Optional[int] = None  # FUTURES only, transaction time
+
+
+class BinanceTickerBooks(BinanceTickerBook):
+    """
+    GET response of `ticker/bookTicker`
+    Single BinanceTickerBook or list of BinanceTickerBook,
+    depending on parameters and exchange type.
+    """
+
+    tickers: Optional[list[BinanceTickerBook]] = None
