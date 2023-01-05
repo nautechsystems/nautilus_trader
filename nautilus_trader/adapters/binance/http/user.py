223e9d86a5b18435c07205f8a87b667f30356a63
Reece Kibble <reecek@uniciant.com>
2023-01-05 07:29:54 +0000

    binance/http/user.py: Implement ListenKey endpoint

--- nautilus_trader/adapters/binance/http/user.py
+++ nautilus_trader/adapters/binance/http/user.py
@@ -13,14 +13,120 @@
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
 
-from typing import Any
+from typing import Optional
 
 import msgspec
 
 from nautilus_trader.adapters.binance.common.enums import BinanceAccountType
-from nautilus_trader.adapters.binance.common.schemas.schemas import BinanceListenKey
+from nautilus_trader.adapters.binance.common.enums import BinanceMethodType
+from nautilus_trader.adapters.binance.common.enums import BinanceSecurityType
+from nautilus_trader.adapters.binance.common.schemas.user import BinanceListenKey
 from nautilus_trader.adapters.binance.http.client import BinanceHttpClient
+from nautilus_trader.adapters.binance.http.endpoint import BinanceHttpEndpoint
 from nautilus_trader.core.correctness import PyCondition
+
+
+class BinanceListenKeyHttp(BinanceHttpEndpoint):
+    """
+    Endpoint for managing user data streams (listenKey)
+
+    `POST /api/v3/userDataStream`
+    `POST /sapi/v3/userDataStream`
+    `POST /sapi/v3/userDataStream/isolated`
+    `POST /fapi/v1/listenKey`
+    `POST /dapi/v1/listenKey`
+
+    `PUT /api/v3/userDataStream`
+    `PUT /sapi/v3/userDataStream`
+    `PUT /sapi/v3/userDataStream/isolated`
+    `PUT /fapi/v1/listenKey`
+    `PUT /dapi/v1/listenKey`
+
+    `DELETE /api/v3/userDataStream`
+    `DELETE /sapi/v3/userDataStream`
+    `DELETE /sapi/v3/userDataStream/isolated`
+    `DELETE /fapi/v1/listenKey`
+    `DELETE /dapi/v1/listenKey`
+
+    References
+    ----------
+    https://binance-docs.github.io/apidocs/spot/en/#listen-key-spot
+    https://binance-docs.github.io/apidocs/spot/en/#listen-key-margin
+    https://binance-docs.github.io/apidocs/futures/en/#start-user-data-stream-user_stream
+    https://binance-docs.github.io/apidocs/delivery/en/#start-user-data-stream-user_stream
+
+    """
+
+    def __init__(
+        self,
+        client: BinanceHttpClient,
+        url_path: str,
+    ):
+        methods = {
+            BinanceMethodType.POST: BinanceSecurityType.USER_STREAM,
+            BinanceMethodType.PUT: BinanceSecurityType.USER_STREAM,
+            BinanceMethodType.DELETE: BinanceSecurityType.USER_STREAM,
+        }
+        super().__init__(
+            client,
+            methods,
+            url_path,
+        )
+        self.post_resp_decoder = msgspec.json.Decoder(BinanceListenKey)
+        self.put_resp_decoder = msgspec.json.Decoder()
+        self.delete_resp_decoder = msgspec.json.Decoder()
+
+    class PostParameters(msgspec.Struct, omit_defaults=True, frozen=True):
+        """
+        POST parameters for creating listenkeys
+
+        Parameters
+        ----------
+        symbol : str
+            The trading pair. Only required for ISOLATED MARGIN accounts!
+        """
+
+        symbol: Optional[str] = None  # MARGIN_ISOLATED only, mandatory
+
+    class PutDeleteParameters(msgspec.Struct, omit_defaults=True, frozen=True):
+        """
+        PUT & DELETE parameters for managing listenkeys
+
+        Parameters
+        ----------
+        symbol : str
+            The trading pair. Only required for ISOLATED MARGIN accounts!
+        listenKey : str
+            The listenkey to manage. Only required for SPOT/MARGIN accounts!
+        """
+
+        symbol: Optional[str] = None  # MARGIN_ISOLATED only, mandatory
+        listenKey: Optional[BinanceListenKey] = None  # SPOT/MARGIN only, mandatory
+
+    async def _post(self, parameters: Optional[PostParameters] = None) -> BinanceListenKey:
+        method_type = BinanceMethodType.POST
+        raw = await self._method(method_type, parameters)
+        return self.post_resp_decoder.decode(raw)
+
+    async def _put(self, parameters: Optional[PutDeleteParameters] = None) -> dict:
+        method_type = BinanceMethodType.PUT
+        raw = await self._method(method_type, parameters)
+        return self.put_resp_decoder.decode(raw)
+
+    async def _delete(self, parameters: Optional[PutDeleteParameters] = None) -> dict:
+        method_type = BinanceMethodType.DELETE
+        raw = await self._method(method_type, parameters)
+        return self.delete_resp_decoder.decode(raw)
+
+    async def create_listen_key(self, parameters: Optional[PostParameters]) -> BinanceListenKey:
+        key = await self._post(parameters)
+        return key
+
+    async def keepalive_listen_key(self, parameters: Optional[PutDeleteParameters] = None):
+        await self._put(parameters)
+
+    async def delete_listen_key(self, parameters: Optional[PutDeleteParameters] = None):
+        await self._delete(parameters)
 
 
 class BinanceUserDataHttpAPI:
@@ -50,113 +156,22 @@ class BinanceUserDataHttpAPI:
 
         if account_type == BinanceAccountType.SPOT:
             self.base_endpoint = "/api/v3/"
-            self.listen_key_endpoint = self.base_endpoint + "userDataStream"
+            listen_key_url = self.base_endpoint + "userDataStream"
         elif account_type == BinanceAccountType.MARGIN_CROSS:
             self.base_endpoint = "/sapi/v1/"
-            self.listen_key_endpoint = self.base_endpoint + "userDataStream"
+            listen_key_url = self.base_endpoint + "userDataStream"
+        elif account_type == BinanceAccountType.MARGIN_ISOLATED:
+            self.base_endpoint = "/sapi/v1/"
+            listen_key_url = self.base_endpoint + "userDataStream/isolated"
         elif account_type == BinanceAccountType.FUTURES_USDT:
             self.base_endpoint = "/fapi/v1/"
-            self.listen_key_endpoint = self.base_endpoint + "listenKey"
+            listen_key_url = self.base_endpoint + "listenKey"
         elif account_type == BinanceAccountType.FUTURES_COIN:
             self.base_endpoint = "/dapi/v1/"
-            self.listen_key_endpoint = self.base_endpoint + "listenKey"
+            listen_key_url = self.base_endpoint + "listenKey"
         else:
             raise RuntimeError(  # pragma: no cover (design-time error)
                 f"invalid `BinanceAccountType`, was {account_type}",  # pragma: no cover (design-time error)  # noqa
             )
 
-    async def create_listen_key(self) -> BinanceListenKey:
-        """
-        Create a new listen key for the Binance account type.
-
-        Start a new user data stream. The stream will close after 60 minutes
-        unless a keepalive is sent. If the account has an active listenKey,
-        that listenKey will be returned and its validity will be extended for 60
-        minutes.
-
-        Create a ListenKey (USER_STREAM).
-
-        Returns
-        -------
-        BinanceListenKey
-
-        References
-        ----------
-        https://binance-docs.github.io/apidocs/spot/en/#listen-key-spot
-        https://binance-docs.github.io/apidocs/spot/en/#listen-key-margin
-        https://binance-docs.github.io/apidocs/futures/en/#start-user-data-stream-user_stream
-        https://binance-docs.github.io/apidocs/delivery/en/#start-user-data-stream-user_stream
-
-        """
-        raw: bytes = await self.client.send_request(
-            http_method="POST",
-            url_path=self.listen_key_endpoint,
-        )
-
-        return msgspec.json.decode(raw, type=BinanceListenKey)
-
-    async def ping_listen_key(self, key: str) -> dict[str, Any]:
-        """
-        Ping/Keep-alive a listen key for the Binance account type.
-
-        Keep-alive a user data stream to prevent a time-out. User data streams
-        will close after 60 minutes. It's recommended to send a ping about every
-        30 minutes.
-
-        Ping/Keep-alive a ListenKey (USER_STREAM).
-
-        Parameters
-        ----------
-        key : str
-            The listen key for the request.
-
-        Returns
-        -------
-        dict[str, Any]
-
-        References
-        ----------
-        https://binance-docs.github.io/apidocs/spot/en/#listen-key-spot
-        https://binance-docs.github.io/apidocs/spot/en/#listen-key-margin
-        https://binance-docs.github.io/apidocs/futures/en/#keepalive-user-data-stream-user_stream
-        https://binance-docs.github.io/apidocs/delivery/en/#keepalive-user-data-stream-user_stream
-
-        """
-        raw: bytes = await self.client.send_request(
-            http_method="PUT",
-            url_path=self.listen_key_endpoint,
-            payload={"listenKey": key},
-        )
-
-        return msgspec.json.decode(raw)
-
-    async def close_listen_key(self, key: str) -> dict[str, Any]:
-        """
-        Close a listen key for the Binance account type.
-
-        Close a ListenKey (USER_STREAM).
-
-        Parameters
-        ----------
-        key : str
-            The listen key for the request.
-
-        Returns
-        -------
-        dict[str, Any]
-
-        References
-        ----------
-        https://binance-docs.github.io/apidocs/spot/en/#listen-key-spot
-        https://binance-docs.github.io/apidocs/spot/en/#listen-key-margin
-        https://binance-docs.github.io/apidocs/futures/en/#close-user-data-stream-user_stream
-        https://binance-docs.github.io/apidocs/delivery/en/#close-user-data-stream-user_stream
-
-        """
-        raw: bytes = await self.client.send_request(
-            http_method="DELETE",
-            url_path=self.listen_key_endpoint,
-            payload={"listenKey": key},
-        )
-
-        return msgspec.json.decode(raw)
+        self.endpoint_listenkey = BinanceListenKeyHttp(client, listen_key_url)
