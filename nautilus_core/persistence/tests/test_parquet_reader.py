6996978fa8c60551171b6d7731aba8ae844d8e18
Bshara Zahran <bshara23@gmail.com>
2023-01-21 15:36:44 +1100

    Initial cleanups

--- nautilus_core/persistence/tests/test_parquet_reader.py
+++ nautilus_core/persistence/tests/test_parquet_reader.py
@@ -1,16 +1,33 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2023 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
 import os
 
-from nautilus.persistence import ParquetReader
-from nautilus.persistence import ParquetReaderType
-from nautilus.persistence import ParquetType
-
 from nautilus_trader import PACKAGE_ROOT
+from nautilus_trader.core.nautilus import persistence
 from nautilus_trader.model.data.tick import QuoteTick
 
 
 def test_python_parquet_reader():
     parquet_data_path = os.path.join(PACKAGE_ROOT, "tests/test_data/quote_tick_data.parquet")
-    reader = ParquetReader(parquet_data_path, 100, ParquetType.QuoteTick, ParquetReaderType.File)
+    reader = persistence.ParquetReader(
+        parquet_data_path,
+        100,
+        persistence.ParquetType.QuoteTick,
+        persistence.ParquetReaderType.File,
+    )
 
     total_count = 0
     for chunk in reader:
