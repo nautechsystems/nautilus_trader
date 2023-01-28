5e479fcc59ab786fdbf1ff9be27fef2f9d2a6615
Chris Sellers <chris@cjdsellers.io>
2023-01-28 12:10:35 +1100

    Build out core Order type

--- nautilus_core/model/src/orders/mod.rs
+++ nautilus_core/model/src/orders/mod.rs
@@ -29,6 +29,7 @@ use crate::identifiers::strategy_id::StrategyId;
 use crate::identifiers::trade_id::TradeId;
 use crate::identifiers::trader_id::TraderId;
 use crate::identifiers::venue_order_id::VenueOrderId;
+use crate::types::price::Price;
 use crate::types::quantity::Quantity;
 use nautilus_core::time::UnixNanos;
 use nautilus_core::uuid::UUID4;
@@ -170,6 +171,8 @@ struct Order {
     venue_order_ids: Vec<VenueOrderId>,
     trade_ids: Vec<TradeId>,
     fsm: StateMachine<OrderFsm>,
+    previous_status: Option<OrderStatus>,
+    triggered_price: Option<Price>,
     pub status: OrderStatus,
     pub trader_id: TraderId,
     pub strategy_id: StrategyId,
@@ -182,10 +185,18 @@ struct Order {
     pub side: OrderSide,
     pub order_type: OrderType,
     pub quantity: Quantity,
+    pub price: Option<Price>,
+    pub trigger_price: Option<Price>,
+    pub trigger_type: Option<TriggerType>,
     pub time_in_force: TimeInForce,
+    pub expire_time: Option<UnixNanos>,
     pub liquidity_side: Option<LiquiditySide>,
     pub is_post_only: bool,
     pub is_reduce_only: bool,
+    pub display_qty: Option<Quantity>,
+    pub limit_offset: Option<Price>,
+    pub trailing_offset: Option<Price>,
+    pub trailing_offset_type: Option<TriggerType>,
     pub emulation_trigger: Option<TriggerType>,
     pub contingency_type: Option<ContingencyType>,
     pub order_list_id: Option<OrderListId>,
@@ -197,6 +208,7 @@ struct Order {
     pub avg_px: Option<f64>,
     pub slippage: Option<f64>,
     pub init_id: UUID4,
+    pub ts_triggered: Option<UnixNanos>,
     pub ts_init: UnixNanos,
     pub ts_last: UnixNanos,
 }
@@ -217,6 +229,8 @@ impl Order {
             venue_order_ids: Vec::new(),
             trade_ids: Vec::new(),
             fsm: StateMachine::new(),
+            previous_status: None,
+            triggered_price: None,
             status: OrderStatus::Initialized,
             trader_id: init.trader_id,
             strategy_id: init.strategy_id,
@@ -229,10 +243,18 @@ impl Order {
             side: init.order_side,
             order_type: init.order_type,
             quantity: init.quantity.clone(),
+            price: init.price,
+            trigger_price: init.trigger_price,
+            trigger_type: init.trigger_type,
             time_in_force: init.time_in_force,
+            expire_time: None,
             liquidity_side: None,
             is_post_only: init.post_only,
             is_reduce_only: init.reduce_only,
+            display_qty: None,
+            limit_offset: None,
+            trailing_offset: None,
+            trailing_offset_type: None,
             emulation_trigger: init.emulation_trigger,
             contingency_type: init.contingency_type,
             order_list_id: init.order_list_id,
@@ -244,6 +266,7 @@ impl Order {
             avg_px: None,
             slippage: None,
             init_id: init.event_id,
+            ts_triggered: None,
             ts_init: init.ts_event,
             ts_last: init.ts_event,
         }
@@ -258,9 +281,17 @@ impl Order {
             order_side: self.side,
             order_type: self.order_type,
             quantity: self.quantity.clone(),
+            price: self.price.clone(),
+            trigger_price: self.triggered_price.clone(),
+            trigger_type: self.trigger_type,
             time_in_force: self.time_in_force,
+            expire_time: self.expire_time,
             post_only: self.is_post_only,
             reduce_only: self.is_reduce_only,
+            display_qty: self.display_qty.clone(),
+            limit_offset: self.limit_offset.clone(),
+            trailing_offset: self.trailing_offset.clone(),
+            trailing_offset_type: self.trailing_offset_type,
             emulation_trigger: self.emulation_trigger,
             contingency_type: self.contingency_type,
             order_list_id: self.order_list_id.clone(),
@@ -389,9 +420,9 @@ impl Order {
 
         match (self.side, side) {
             (OrderSide::Buy, PositionSide::Long) => false,
-            (OrderSide::Buy, PositionSide::Short) => self.leaves_qty > position_qty,
+            (OrderSide::Buy, PositionSide::Short) => self.leaves_qty <= position_qty,
             (OrderSide::Sell, PositionSide::Short) => false,
-            (OrderSide::Sell, PositionSide::Long) => self.leaves_qty > position_qty,
+            (OrderSide::Sell, PositionSide::Long) => self.leaves_qty <= position_qty,
             _ => true,
         }
     }
@@ -425,13 +456,15 @@ impl Order {
 ////////////////////////////////////////////////////////////////////////////////
 #[cfg(test)]
 mod tests {
+    use rstest::*;
+
     use super::*;
     use crate::enums::*;
-    use crate::events::order::{stub_order_denied, stub_order_initialized};
+    use crate::events::order::*;
 
     #[test]
     fn test_order_initialized() {
-        let init = stub_order_initialized();
+        let init = OrderInitializedBuilder::new().build();
         let order = Order::new(init.clone());
 
         assert_eq!(order.status, OrderStatus::Initialized);
@@ -440,13 +473,81 @@ mod tests {
         assert_eq!(order.event_count(), 0);
         assert!(order.venue_order_ids.is_empty());
         assert!(order.trade_ids.is_empty());
+        assert!(order.is_buy());
+        assert!(!order.is_sell());
+        assert!(!order.is_passive());
+        assert!(order.is_aggressive());
+        assert!(!order.is_emulated());
+        assert!(!order.is_contingency());
+        assert!(!order.is_parent_order());
+        assert!(!order.is_child_order());
+        assert!(!order.is_open());
+        assert!(!order.is_closed());
+        assert!(!order.is_inflight());
+        assert!(!order.is_pending_update());
+        assert!(!order.is_pending_cancel());
+    }
+
+    #[rstest(
+        order_side,
+        expected_side,
+        case(OrderSide::Buy, OrderSide::Sell),
+        case(OrderSide::Sell, OrderSide::Buy),
+        case(OrderSide::NoOrderSide, OrderSide::NoOrderSide)
+    )]
+    fn test_order_opposite_side(order_side: OrderSide, expected_side: OrderSide) {
+        let result = Order::opposite_side(order_side);
+        assert_eq!(result, expected_side)
+    }
+
+    #[rstest(
+        position_side,
+        expected_side,
+        case(PositionSide::Long, OrderSide::Sell),
+        case(PositionSide::Short, OrderSide::Buy),
+        case(PositionSide::NoPositionSide, OrderSide::NoOrderSide)
+    )]
+    fn test_closing_side(position_side: PositionSide, expected_side: OrderSide) {
+        let result = Order::closing_side(position_side);
+        assert_eq!(result, expected_side)
+    }
+
+    #[rustfmt::skip]
+    #[rstest(
+        order_side, order_qty, position_side, position_qty, expected,
+        case(OrderSide::Buy, Quantity::from(100), PositionSide::Long, Quantity::from(50), false),
+        case(OrderSide::Buy, Quantity::from(50), PositionSide::Short, Quantity::from(50), true),
+        case(OrderSide::Buy, Quantity::from(50), PositionSide::Short, Quantity::from(100), true),
+        case(OrderSide::Buy, Quantity::from(50), PositionSide::Flat, Quantity::from(0), false),
+        case(OrderSide::Sell, Quantity::from(50), PositionSide::Flat, Quantity::from(0), false),
+        case(OrderSide::Sell, Quantity::from(50), PositionSide::Long, Quantity::from(50), true),
+        case(OrderSide::Sell, Quantity::from(50), PositionSide::Long, Quantity::from(100), true),
+        case(OrderSide::Sell, Quantity::from(100), PositionSide::Short, Quantity::from(50), false),
+    )]
+    fn test_would_reduce_only(
+        order_side: OrderSide,
+        order_qty: Quantity,
+        position_side: PositionSide,
+        position_qty: Quantity,
+        expected: bool,
+    ) {
+        let init = OrderInitializedBuilder::new()
+            .order_side(order_side)
+            .quantity(order_qty)
+            .build();
+        let order = Order::new(init);
+
+        assert_eq!(
+            order.would_reduce_only(position_side, position_qty),
+            expected
+        );
     }
 
     #[test]
     fn test_order_state_transition_denied() {
-        let init = stub_order_initialized();
+        let init = OrderInitializedBuilder::new().build();
+        let denied = OrderDeniedBuilder::new(&init).build();
         let mut order = Order::new(init);
-        let denied = stub_order_denied();
         let event = OrderEvent::OrderDenied(denied);
 
         let _ = order.apply(event.clone());
