3a9da769689637e0142f8bcc67c48ba459c08a1e
Chris Sellers <chris@cjdsellers.io>
2023-02-12 09:07:13 -0700

    Remove redundant import

--- nautilus_trader/infrastructure/cache.pyx
+++ nautilus_trader/infrastructure/cache.pyx
@@ -13,7 +13,6 @@
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
 
-import pickle
 import warnings
 from typing import Optional
 
