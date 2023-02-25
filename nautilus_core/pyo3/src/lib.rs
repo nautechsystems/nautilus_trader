bb8f30f45ded7d042df1003544549654bfc0ea84
Bshara Zahran <bshara23@gmail.com>
2023-02-25 18:53:52 +1100

    Expose enums with pyo3

--- nautilus_core/pyo3/src/lib.rs
+++ nautilus_core/pyo3/src/lib.rs
@@ -13,6 +13,7 @@
 //  limitations under the License.
 // -------------------------------------------------------------------------------------------------
 
+use nautilus_model::model;
 use nautilus_persistence::persistence;
 use pyo3::{prelude::*, types::PyDict};
 
@@ -22,6 +23,7 @@ use pyo3::{prelude::*, types::PyDict};
 /// refer: https://github.com/PyO3/pyo3/issues/2644
 #[pymodule]
 fn nautilus_pyo3(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    // Persistence
     let submodule = pyo3::wrap_pymodule!(persistence);
     m.add_wrapped(submodule)?;
     let sys = PyModule::import(py, "sys")?;
@@ -29,6 +31,16 @@ fn nautilus_pyo3(py: Python<'_>, m: &PyModule) -> PyResult<()> {
     sys_modules.set_item(
         "nautilus_trader.core.nautilus_pyo3.persistence",
         m.getattr("persistence")?,
+    )?;
+
+    // Model
+    let submodule = pyo3::wrap_pymodule!(model);
+    m.add_wrapped(submodule)?;
+    let sys = PyModule::import(py, "sys")?;
+    let sys_modules: &PyDict = sys.getattr("modules")?.downcast()?;
+    sys_modules.set_item(
+        "nautilus_trader.core.nautilus_pyo3.model",
+        m.getattr("model")?,
     )?;
     Ok(())
 }
