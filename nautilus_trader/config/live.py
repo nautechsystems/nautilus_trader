234c24968da72da62481c30a9953693235417ea5
Chris Sellers <chris@cjdsellers.io>
2023-03-08 18:17:51 +1100

    Use thousands separators

--- nautilus_trader/config/live.py
+++ nautilus_trader/config/live.py
@@ -34,11 +34,11 @@ class LiveDataEngineConfig(DataEngineConfig, frozen=True):
 
     Parameters
     ----------
-    qsize : PositiveInt, default 10000
+    qsize : PositiveInt, default 10_000
         The queue size for the engines internal queue buffers.
     """
 
-    qsize: PositiveInt = 10000
+    qsize: PositiveInt = 10_000
 
 
 class LiveRiskEngineConfig(RiskEngineConfig, frozen=True):
@@ -47,11 +47,11 @@ class LiveRiskEngineConfig(RiskEngineConfig, frozen=True):
 
     Parameters
     ----------
-    qsize : PositiveInt, default 10000
+    qsize : PositiveInt, default 10_000
         The queue size for the engines internal queue buffers.
     """
 
-    qsize: PositiveInt = 10000
+    qsize: PositiveInt = 10_000
 
 
 class LiveExecEngineConfig(ExecEngineConfig, frozen=True):
@@ -65,21 +65,21 @@ class LiveExecEngineConfig(ExecEngineConfig, frozen=True):
     reconciliation_lookback_mins : NonNegativeInt, optional
         The maximum lookback minutes to reconcile state for.
         If ``None`` or 0 then will use the maximum lookback available from the venues.
-    inflight_check_interval_ms : NonNegativeInt, default 5000
+    inflight_check_interval_ms : NonNegativeInt, default 5_000
         The interval (milliseconds) between checking whether in-flight orders
         have exceeded their time-in-flight threshold.
-    inflight_check_threshold_ms : NonNegativeInt, default 1000
+    inflight_check_threshold_ms : NonNegativeInt, default 1_000
         The threshold (milliseconds) beyond which an in-flight orders status
         is checked with the venue.
-    qsize : PositiveInt, default 10000
+    qsize : PositiveInt, default 10_000
         The queue size for the engines internal queue buffers.
     """
 
     reconciliation: bool = True
     reconciliation_lookback_mins: Optional[NonNegativeInt] = None
-    inflight_check_interval_ms: NonNegativeInt = 5000
-    inflight_check_threshold_ms: NonNegativeInt = 1000
-    qsize: PositiveInt = 10000
+    inflight_check_interval_ms: NonNegativeInt = 5_000
+    inflight_check_threshold_ms: NonNegativeInt = 1_000
+    qsize: PositiveInt = 10_000
 
 
 class RoutingConfig(NautilusConfig, frozen=True):
