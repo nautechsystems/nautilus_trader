7b930288784b68db97800723b9bd80daa94d2090
Chris Sellers <chris@cjdsellers.io>
2023-03-11 10:28:28 +1100

    Fix test-coverage script

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -2,4 +2,4 @@
 
 export PROFILE_MODE=true
 poetry install --with test --all-extras
-poetry run pytest --ignore=tests/performance_tests --cov-report=term --cov-report=xml --cov=nautilus_trader --new-first --failed-first
+poetry run pytest --ignore=tests/performance_tests -k "not no_ci" --cov-report=term --cov-report=xml --cov=nautilus_trader --new-first --failed-first
