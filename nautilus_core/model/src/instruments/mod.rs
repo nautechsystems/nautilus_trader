85f02b518e622a75e0fd7810e3017d4c4fe58bcf
Chris Sellers <chris@cjdsellers.io>
2023-04-02 15:06:30 +1000

    Build out core domain model

---
+++ nautilus_core/model/src/instruments/mod.rs
@@ -0,0 +1,47 @@
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2023 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
+
+use rust_decimal::Decimal;
+
+use crate::{
+    enums::{AssetClass, AssetType},
+    identifiers::{instrument_id::InstrumentId, symbol::Symbol},
+    types::{currency::Currency, price::Price, quantity::Quantity},
+};
+
+pub struct Instrument {
+    pub id: InstrumentId,
+    pub native_symbol: Symbol,
+    pub asset_class: AssetClass,
+    pub asset_type: AssetType,
+    pub quote_currency: Currency,
+    pub base_currency: Option<Currency>,
+    pub cost_currency: Currency,
+    pub is_inverse: bool,
+    pub price_precision: u8,
+    pub size_precision: u8,
+    pub price_increment: Price,
+    pub size_increment: Quantity,
+    pub multiplier: Quantity,
+    pub lot_size: Option<Quantity>,
+    pub max_quantity: Option<Quantity>,
+    pub min_quantity: Option<Quantity>,
+    pub max_price: Option<Price>,
+    pub min_price: Option<Price>,
+    pub margin_init: Decimal,
+    pub margin_maint: Decimal,
+    pub maker_fee: Decimal,
+    pub taker_fee: Decimal,
+}
