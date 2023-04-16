1a6b59fecf8b408ac3862c03363248c57dbd7e40
Bshara Zahran <bshara23@gmail.com>
2023-04-16 15:59:11 +1000

    Improve GTD timer cancelation

--- nautilus_trader/trading/strategy.pyx
+++ nautilus_trader/trading/strategy.pyx
@@ -1026,9 +1026,10 @@ cdef class Strategy(Actor):
 
     cdef void _cancel_gtd_expiry(self, Order order):
         cdef str timer_name = self._get_gtd_expiry_timer_name(order.client_order_id)
+        cdef str expire_time_str = f" @ {order.expire_time}" if hasattr(order, "expire_time") else ""
         if timer_name in self._clock.timer_names:
             self._log.info(
-                f"Canceling managed GTD expiry timer for {order.client_order_id} @ {order.expire_time}.",
+                f"Canceling managed GTD expiry timer for {order.client_order_id}{expire_time_str}.",
                 LogColor.BLUE,
             )
             self._clock.cancel_timer(name=timer_name)
