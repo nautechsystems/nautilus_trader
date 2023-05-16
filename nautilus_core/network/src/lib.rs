6ce59350fb069d0d9d13b7235fd8dafaddfe61e7
Bshara Zahran <bshara23@gmail.com>
2023-05-16 18:27:46 +1000

    Add body for core HTTP client

--- nautilus_core/network/src/lib.rs
+++ nautilus_core/network/src/lib.rs
@@ -17,7 +17,7 @@ use std::{collections::HashMap, str::FromStr};
 
 use hyper::{Body, Client, Method, Request, Response};
 use hyper_tls::HttpsConnector;
-use pyo3::{prelude::*, types::PyBytes};
+use pyo3::{exceptions::PyTypeError, prelude::*, types::PyBytes, types::PyDict};
 
 /// HttpClient makes HTTP requests to exchanges.
 ///
@@ -84,14 +84,15 @@ impl HttpClient {
         method_str: String,
         url: String,
         headers: HashMap<String, String>,
+        body: Option<&'py PyDict>,
         py: Python<'py>,
     ) -> PyResult<&'py PyAny> {
         let method: Method =
             Method::from_str(&method_str.to_lowercase()).expect("Invalid HTTP method {method_str}");
+        let body: Option<HashMap<String, serde_json::Value>> = py_dict_to_rust_map(body)?;
         let client = slf.clone();
-
         pyo3_asyncio::tokio::future_into_py(py, async move {
-            match client.send_request(method, url, headers).await {
+            match client.send_request(method, url, headers, body).await {
                 Ok(res) => Ok(res),
                 Err(_) => {
                     // TODO: log error
@@ -105,11 +106,13 @@ impl HttpClient {
         slf: PyRef<'_, Self>,
         url: String,
         headers: HashMap<String, String>,
+        body: Option<&'py PyDict>,
         py: Python<'py>,
     ) -> PyResult<&'py PyAny> {
+        let body: Option<HashMap<String, serde_json::Value>> = py_dict_to_rust_map(body)?;
         let client = slf.clone();
         pyo3_asyncio::tokio::future_into_py(py, async move {
-            match client.send_request(Method::GET, url, headers).await {
+            match client.send_request(Method::GET, url, headers, body).await {
                 Ok(res) => Ok(res),
                 Err(_) => {
                     // TODO: log error
@@ -123,11 +126,13 @@ impl HttpClient {
         slf: PyRef<'_, Self>,
         url: String,
         headers: HashMap<String, String>,
+        body: Option<&'py PyDict>,
         py: Python<'py>,
     ) -> PyResult<&'py PyAny> {
+        let body: Option<HashMap<String, serde_json::Value>> = py_dict_to_rust_map(body)?;
         let client = slf.clone();
         pyo3_asyncio::tokio::future_into_py(py, async move {
-            match client.send_request(Method::POST, url, headers).await {
+            match client.send_request(Method::POST, url, headers, body).await {
                 Ok(res) => Ok(res),
                 Err(_) => {
                     // TODO: log error
@@ -141,11 +146,13 @@ impl HttpClient {
         slf: PyRef<'_, Self>,
         url: String,
         headers: HashMap<String, String>,
+        body: Option<&'py PyDict>,
         py: Python<'py>,
     ) -> PyResult<&'py PyAny> {
+        let body: Option<HashMap<String, serde_json::Value>> = py_dict_to_rust_map(body)?;
         let client = slf.clone();
         pyo3_asyncio::tokio::future_into_py(py, async move {
-            match client.send_request(Method::PATCH, url, headers).await {
+            match client.send_request(Method::PATCH, url, headers, body).await {
                 Ok(res) => Ok(res),
                 Err(_) => {
                     // TODO: log error
@@ -159,11 +166,16 @@ impl HttpClient {
         slf: PyRef<'_, Self>,
         url: String,
         headers: HashMap<String, String>,
+        body: Option<&'py PyDict>,
         py: Python<'py>,
     ) -> PyResult<&'py PyAny> {
+        let body: Option<HashMap<String, serde_json::Value>> = py_dict_to_rust_map(body)?;
         let client = slf.clone();
         pyo3_asyncio::tokio::future_into_py(py, async move {
-            match client.send_request(Method::DELETE, url, headers).await {
+            match client
+                .send_request(Method::DELETE, url, headers, body)
+                .await
+            {
                 Ok(res) => Ok(res),
                 Err(_) => {
                     // TODO: log error
@@ -180,6 +192,7 @@ impl HttpClient {
         method: Method,
         url: String,
         headers: HashMap<String, String>,
+        body: Option<HashMap<String, serde_json::Value>>,
     ) -> Result<HttpResponse, Box<dyn std::error::Error + Send + Sync>> {
         let mut req_builder = Request::builder().method(method).uri(url);
 
@@ -187,7 +200,15 @@ impl HttpClient {
             req_builder = req_builder.header(header_name, header_value);
         }
 
-        let req = req_builder.body(Body::empty())?;
+        let req = if let Some(body) = body {
+            let body = serde_json::to_string(&body)?;
+            req_builder
+                .header("Content-Type", "application/json")
+                .body(Body::from(body))?
+        } else {
+            req_builder.body(Body::empty())?
+        };
+
         let res = self.client.request(req).await?;
         self.to_response(res).await
     }
@@ -214,6 +235,27 @@ impl HttpClient {
     }
 }
 
+fn py_dict_to_rust_map(
+    py_dict: Option<&PyDict>,
+) -> PyResult<Option<HashMap<String, serde_json::Value>>> {
+    match py_dict {
+        Some(dict) => {
+            let mut rust_map = HashMap::new();
+            for (key, value) in dict {
+                let key_str: String = key.extract().map_err(|e| {
+                    PyErr::new::<PyTypeError, _>(format!("Failed to extract `body` key: {}", e))
+                })?;
+                let value_str: String = value.extract().map_err(|e| {
+                    PyErr::new::<PyTypeError, _>(format!("Failed to extract `body` value: {}", e))
+                })?;
+                rust_map.insert(key_str, serde_json::Value::String(value_str));
+            }
+            Ok(Some(rust_map))
+        }
+        None => Ok(None),
+    }
+}
+
 // Uncomment to change for module name for reduced debug builds in testing
 #[pymodule]
 pub fn nautilus_network(_: Python<'_>, m: &PyModule) -> PyResult<()> {
@@ -229,23 +271,50 @@ pub fn nautilus_network(_: Python<'_>, m: &PyModule) -> PyResult<()> {
 mod tests {
     use super::*;
     use httptest::{matchers::*, responders::*, Expectation, Server};
+    use hyper::StatusCode;
+    use pyo3::types::IntoPyDict;
+    use serde_json::Value;
+
+    #[test]
+    fn test_py_dict_to_rust_map() {
+        pyo3::prepare_freethreaded_python();
+
+        Python::with_gil(|py| {
+            let py_dict = [("key1", "value1"), ("key2", "value2")].into_py_dict(py);
+            let result = py_dict_to_rust_map(Some(py_dict)).unwrap();
+
+            let mut expected = HashMap::new();
+            expected.insert(String::from("key1"), Value::String(String::from("value1")));
+            expected.insert(String::from("key2"), Value::String(String::from("value2")));
+
+            assert_eq!(result, Some(expected));
+        });
+    }
+
+    #[test]
+    fn test_py_dict_to_rust_map_none() {
+        let result = py_dict_to_rust_map(None::<&PyDict>).unwrap();
+        let expected = None;
+
+        assert_eq!(result, expected);
+    }
 
     #[tokio::test]
     async fn test_get() {
         let server = Server::run();
         server.expect(
             Expectation::matching(request::method_path("GET", "/get"))
-                .respond_with(status_code(200).body("hello-world!")),
+                .respond_with(status_code(StatusCode::OK.into()).body("hello-world!")),
         );
         let url = format!("http://{}", server.addr());
 
         let client = HttpClient::default();
         let response = client
-            .send_request(Method::GET, format!("{}/get", url), HashMap::new())
+            .send_request(Method::GET, format!("{}/get", url), HashMap::new(), None)
             .await
             .unwrap();
 
-        assert_eq!(response.status, 200);
+        assert_eq!(response.status, StatusCode::OK);
         assert_eq!(String::from_utf8_lossy(&response.body), "hello-world!");
     }
 
@@ -254,17 +323,51 @@ mod tests {
         let server = Server::run();
         server.expect(
             Expectation::matching(request::method_path("POST", "/post"))
-                .respond_with(status_code(200)),
+                .respond_with(status_code(StatusCode::OK.into())),
         );
         let url = format!("http://{}", server.addr());
 
         let client = HttpClient::default();
         let response = client
-            .send_request(Method::POST, format!("{}/post", url), HashMap::new())
+            .send_request(Method::POST, format!("{}/post", url), HashMap::new(), None)
             .await
             .unwrap();
 
-        assert_eq!(response.status, 200);
+        assert_eq!(response.status, StatusCode::OK);
+    }
+
+    #[tokio::test]
+    async fn test_post_with_body() {
+        let server = Server::run();
+        server.expect(
+            Expectation::matching(request::method_path("POST", "/post"))
+                .respond_with(status_code(StatusCode::OK.into())),
+        );
+        let url = format!("http://{}", server.addr());
+
+        let client = HttpClient::default();
+
+        let mut body = HashMap::new();
+        body.insert(
+            "key1".to_string(),
+            serde_json::Value::String("value1".to_string()),
+        );
+        body.insert(
+            "key2".to_string(),
+            serde_json::Value::String("value2".to_string()),
+        );
+
+        let response = client
+            .send_request(
+                Method::POST,
+                format!("{}/post", url),
+                HashMap::new(),
+                Some(body),
+            )
+            .await
+            .unwrap();
+
+        assert_eq!(response.status, StatusCode::OK);
     }
 
     #[tokio::test]
@@ -272,17 +375,22 @@ mod tests {
         let server = Server::run();
         server.expect(
             Expectation::matching(request::method_path("PATCH", "/patch"))
-                .respond_with(status_code(200)),
+                .respond_with(status_code(StatusCode::OK.into())),
         );
         let url = format!("http://{}", server.addr());
 
         let client = HttpClient::default();
         let response = client
-            .send_request(Method::PATCH, format!("{}/patch", url), HashMap::new())
+            .send_request(
+                Method::PATCH,
+                format!("{}/patch", url),
+                HashMap::new(),
+                None,
+            )
             .await
             .unwrap();
 
-        assert_eq!(response.status, 200);
+        assert_eq!(response.status, StatusCode::OK);
     }
 
     #[tokio::test]
@@ -290,16 +398,21 @@ mod tests {
         let server = Server::run();
         server.expect(
             Expectation::matching(request::method_path("DELETE", "/delete"))
-                .respond_with(status_code(200)),
+                .respond_with(status_code(StatusCode::OK.into())),
         );
         let url = format!("http://{}", server.addr());
 
         let client = HttpClient::default();
         let response = client
-            .send_request(Method::DELETE, format!("{}/delete", url), HashMap::new())
+            .send_request(
+                Method::DELETE,
+                format!("{}/delete", url),
+                HashMap::new(),
+                None,
+            )
             .await
             .unwrap();
 
-        assert_eq!(response.status, 200);
+        assert_eq!(response.status, StatusCode::OK);
     }
 }
