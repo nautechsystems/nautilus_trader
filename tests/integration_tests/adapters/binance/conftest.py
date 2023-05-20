9b148248b7c9278a5fb1cda7e89b999bf8d4af4f
Chris Sellers <chris@cjdsellers.io>
2023-05-20 17:48:42 +1000

    Fix Binance integration tests

--- tests/integration_tests/adapters/binance/conftest.py
+++ tests/integration_tests/adapters/binance/conftest.py
@@ -42,7 +42,6 @@ def live_logger(live_clock):
 @pytest.fixture(scope="session")
 def binance_http_client(loop, live_clock, live_logger):
     client = BinanceHttpClient(
-        loop=asyncio.get_event_loop(),
         clock=live_clock,
         logger=live_logger,
         key="SOME_BINANCE_API_KEY",
