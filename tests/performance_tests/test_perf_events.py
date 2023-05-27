f7b172b60d6d04dc91ed0f42193aed22c4c43516
Bshara Zahran <bshara23@gmail.com>
2023-05-28 09:58:36 +1000

    Add events performance tests

---
+++ tests/performance_tests/test_perf_events.py
@@ -0,0 +1,76 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2023 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
+import msgspec
+
+from nautilus_trader.core.uuid import UUID4
+from nautilus_trader.model.events.order import OrderDenied
+from nautilus_trader.model.identifiers import ClientOrderId
+from nautilus_trader.model.identifiers import InstrumentId
+from nautilus_trader.model.identifiers import StrategyId
+from nautilus_trader.model.identifiers import Symbol
+from nautilus_trader.model.identifiers import TraderId
+from nautilus_trader.model.identifiers import Venue
+from nautilus_trader.test_kit.performance import PerformanceBench
+
+
+STUB_ORDER_DENIED = OrderDenied(
+    trader_id=TraderId("TRADER-001"),
+    strategy_id=StrategyId("SCALPER-001"),
+    instrument_id=InstrumentId(Symbol("BTCUSDT"), Venue("BINANCE")),
+    client_order_id=ClientOrderId("O-2020872378423"),
+    reason="Exceeded MAX_ORDER_SUBMIT_RATE",
+    event_id=UUID4(),
+    ts_init=0,
+)
+
+
+def stub_order_denied() -> OrderDenied:
+    uuid = UUID4()
+    reason = "Exceeded MAX_ORDER_SUBMIT_RATE"
+    return OrderDenied(
+        trader_id=TraderId("TRADER-001"),
+        strategy_id=StrategyId("SCALPER-001"),
+        instrument_id=InstrumentId(Symbol("BTCUSDT"), Venue("BINANCE")),
+        client_order_id=ClientOrderId("O-2020872378423"),
+        reason=reason,
+        event_id=uuid,
+        ts_init=0,
+    )
+
+
+def test_order_denied_to_dict():
+    def call_to_dict() -> None:
+        OrderDenied.to_dict(STUB_ORDER_DENIED)
+
+    PerformanceBench.profile_function(
+        target=call_to_dict,
+        runs=100_000,
+        iterations=1,
+    )
+    # ~0.0ms / ~1.8μs / 1841ns minimum of 100,000 runs @ 1 iteration each run.
+
+
+def test_order_denied_to_dict_then_msgspec_to_json():
+    def call_to_dict_then_json() -> None:
+        denied_dict = OrderDenied.to_dict(STUB_ORDER_DENIED)
+        msgspec.json.encode(denied_dict)
+
+    PerformanceBench.profile_function(
+        target=call_to_dict_then_json,
+        runs=100_000,
+        iterations=1,
+    )
+    # ~0.0ms / ~2.4μs / 2441ns minimum of 100,000 runs @ 1 iteration each run.
