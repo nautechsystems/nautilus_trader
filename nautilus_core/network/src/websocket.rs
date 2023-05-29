6b73903b0e3c5c8db80d9b3931250b848ca16a1f
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2023-05-29 20:10:49 +0800

    Web sockets first implementation and tests

--- nautilus_core/network/src/websocket.rs
+++ nautilus_core/network/src/websocket.rs
@@ -12,3 +12,106 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 // -------------------------------------------------------------------------------------------------
+
+use std::{net::TcpStream, panic, sync::Mutex};
+
+use pyo3::prelude::*;
+use tungstenite::{connect, stream::MaybeTlsStream, Message, WebSocket};
+
+#[pyclass]
+struct WebSocketClient {
+    stream: Mutex<WebSocket<MaybeTlsStream<TcpStream>>>,
+}
+
+impl WebSocketClient {
+    pub fn connect(url: &str) -> Self {
+        match connect(url) {
+            Ok((stream, _resp)) => WebSocketClient {
+                stream: Mutex::new(stream),
+            },
+            Err(err) => {
+                panic!("Cannot connect to websocket server {}", err);
+            }
+        }
+    }
+
+    pub fn send(&self, msg: Message) {
+        if let Ok(mut stream) = self.stream.lock() {
+            if let Err(err) = stream.write_message(msg) {
+                panic!("Message could not be sent {}", err);
+            };
+        }
+    }
+
+    pub fn recv(&self) -> Option<Vec<u8>> {
+        if let Ok(mut stream) = self.stream.lock() {
+            match stream.read_message() {
+                Ok(Message::Text(txt)) => Some(txt.into_bytes()),
+                Ok(Message::Binary(data)) => Some(data),
+                Ok(_) => None,
+                Err(err) => {
+                    panic!("Error with stream {}", err);
+                }
+            }
+        } else {
+            None
+        }
+    }
+}
+
+#[pymethods]
+impl WebSocketClient {
+    #[new]
+    pub fn new(url: String) -> Self {
+        WebSocketClient::connect(&url)
+    }
+
+    pub fn send_bytes(slf: PyRef<'_, Self>, data: Vec<u8>) {
+        slf.send(Message::Binary(data));
+    }
+
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+        slf
+    }
+
+    /// Each iteration returns a chunk of values read from the parquet file.
+    fn __next__(slf: PyRef<'_, Self>) -> Option<PyObject> {
+        slf.recv()
+            .map(|data| Python::with_gil(|py| data.into_py(py)))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{net::TcpListener, thread};
+
+    use tungstenite::accept;
+
+    use super::WebSocketClient;
+
+    #[test]
+    fn test_client() {
+        thread::spawn(|| {
+            let server = TcpListener::bind("127.0.0.1:9001").unwrap();
+            let conn = server.incoming().next().unwrap();
+            let mut websocket = accept(conn.unwrap()).unwrap();
+
+            // echo 10 messages before shutting down
+            for _ in 0..10 {
+                let msg = websocket.read_message().unwrap();
+
+                // We do not want to send back ping/pong messages.
+                if msg.is_binary() || msg.is_text() {
+                    websocket.write_message(msg).unwrap();
+                }
+            }
+        });
+
+        let client = WebSocketClient::connect("ws://127.0.0.1:9001");
+
+        for _ in 0..10 {
+            client.send(tungstenite::Message::Text("ping".to_string()));
+            assert_eq!(client.recv(), Some("ping".to_string().into_bytes()));
+        }
+    }
+}
