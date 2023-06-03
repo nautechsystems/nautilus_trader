a6aa35049f40d7ac6221b39c8cd5abf514e8a9ed
Bshara Zahran <bshara23@gmail.com>
2023-06-03 17:12:01 +1000

    Tidy up task handling

--- nautilus_trader/live/execution_engine.py
+++ nautilus_trader/live/execution_engine.py
@@ -220,11 +220,11 @@ class LiveExecutionEngine(ExecutionEngine):
         if self._cmd_queue_task:
             self._log.debug(f"Canceling {self._cmd_queue_task.get_name()}...")
             self._cmd_queue_task.cancel()
-            self._cmd_queue_task.done()
+            self._cmd_queue_task = None
         if self._evt_queue_task:
             self._log.debug(f"Canceling {self._evt_queue_task.get_name()}...")
             self._evt_queue_task.cancel()
-            self._evt_queue_task.done()
+            self._evt_queue_task = None
 
     def execute(self, command: TradingCommand) -> None:
         """
