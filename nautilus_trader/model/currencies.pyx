a8bd0e9a15e855248a210527d7e46ade3507cf8a
Bshara Zahran <bshara23@gmail.com>
2023-06-10 17:27:48 +1000

    Single source built-in currency definitions

---
+++ nautilus_trader/model/currencies.pyx
@@ -0,0 +1,90 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2023 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
+from nautilus_trader.model.currency cimport Currency
+
+
+# Fiat currencies
+AUD = Currency.get_currency_from_internal_map("AUD")
+BRL = Currency.get_currency_from_internal_map("BRL")
+CAD = Currency.get_currency_from_internal_map("CAD")
+CHF = Currency.get_currency_from_internal_map("CHF")
+CNY = Currency.get_currency_from_internal_map("CNY")
+CNH = Currency.get_currency_from_internal_map("CNH")
+CZK = Currency.get_currency_from_internal_map("CZK")
+DKK = Currency.get_currency_from_internal_map("DKK")
+EUR = Currency.get_currency_from_internal_map("EUR")
+GBP = Currency.get_currency_from_internal_map("GBP")
+HKD = Currency.get_currency_from_internal_map("HKD")
+HUF = Currency.get_currency_from_internal_map("HUF")
+ILS = Currency.get_currency_from_internal_map("ILS")
+INR = Currency.get_currency_from_internal_map("INR")
+JPY = Currency.get_currency_from_internal_map("JPY")
+KRW = Currency.get_currency_from_internal_map("KRW")
+MXN = Currency.get_currency_from_internal_map("MXN")
+NOK = Currency.get_currency_from_internal_map("NOK")
+NZD = Currency.get_currency_from_internal_map("NZD")
+PLN = Currency.get_currency_from_internal_map("PLN")
+RUB = Currency.get_currency_from_internal_map("RUB")
+SAR = Currency.get_currency_from_internal_map("SAR")
+SEK = Currency.get_currency_from_internal_map("SEK")
+SGD = Currency.get_currency_from_internal_map("SGD")
+THB = Currency.get_currency_from_internal_map("THB")
+TRY = Currency.get_currency_from_internal_map("TRY")
+USD = Currency.get_currency_from_internal_map("USD")
+XAG = Currency.get_currency_from_internal_map("XAG")
+XAU = Currency.get_currency_from_internal_map("XAU")
+ZAR = Currency.get_currency_from_internal_map("ZAR")
+
+# Crypto currencies
+ONEINCH = Currency.get_currency_from_internal_map("1INCH")
+AAVE = Currency.get_currency_from_internal_map("AAVE")
+ACA = Currency.get_currency_from_internal_map("ACA")
+ADA = Currency.get_currency_from_internal_map("ADA")
+AVAX = Currency.get_currency_from_internal_map("AVAX")
+BCH = Currency.get_currency_from_internal_map("BCH")
+BTTC = Currency.get_currency_from_internal_map("BTTC")
+BNB = Currency.get_currency_from_internal_map("BNB")
+BRZ = Currency.get_currency_from_internal_map("BRZ")
+BSV = Currency.get_currency_from_internal_map("BSV")
+BTC = Currency.get_currency_from_internal_map("BTC")
+BUSD = Currency.get_currency_from_internal_map("BUSD")
+XBT = Currency.get_currency_from_internal_map("XBT")
+DASH = Currency.get_currency_from_internal_map("DASH")
+DOGE = Currency.get_currency_from_internal_map("DOGE")
+DOT = Currency.get_currency_from_internal_map("DOT")
+EOS = Currency.get_currency_from_internal_map("EOS")
+ETH = Currency.get_currency_from_internal_map("ETH")
+ETHW = Currency.get_currency_from_internal_map("ETHW")
+EZ = Currency.get_currency_from_internal_map("EZ")
+FTT = Currency.get_currency_from_internal_map("FTT")
+JOE = Currency.get_currency_from_internal_map("JOE")
+LINK = Currency.get_currency_from_internal_map("LINK")
+LTC = Currency.get_currency_from_internal_map("LTC")
+LUNA = Currency.get_currency_from_internal_map("LUNA")
+NBT = Currency.get_currency_from_internal_map("NBT")
+SOL = Currency.get_currency_from_internal_map("SOL")
+TRX = Currency.get_currency_from_internal_map("TRX")
+TRYB = Currency.get_currency_from_internal_map("TRYB")
+VTC = Currency.get_currency_from_internal_map("VTC")
+XLM = Currency.get_currency_from_internal_map("XLM")
+XMR = Currency.get_currency_from_internal_map("XMR")
+XRP = Currency.get_currency_from_internal_map("XRP")
+XTZ = Currency.get_currency_from_internal_map("XTZ")
+USDC = Currency.get_currency_from_internal_map("USDC")
+USDT = Currency.get_currency_from_internal_map("USDT")
+WSB = Currency.get_currency_from_internal_map("WSB")
+XEC = Currency.get_currency_from_internal_map("XEC")
+ZEC = Currency.get_currency_from_internal_map("ZEC")
