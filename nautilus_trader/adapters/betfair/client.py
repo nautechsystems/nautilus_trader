8e8bd7accabeb1859ef834534ab1a0e19154d688
Bshara Zahran <bshara23@gmail.com>
2023-06-21 20:13:16 +1000

    Fix imports

--- nautilus_trader/adapters/betfair/client.py
+++ nautilus_trader/adapters/betfair/client.py
@@ -12,6 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
+
 from typing import Literal, Optional
 
 from betfair_parser.endpoints import ENDPOINTS
