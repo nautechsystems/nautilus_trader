dba7be46152f8a5713d727da6283ff179174b04c
Chris Sellers <chris@cjdsellers.io>
2023-07-01 07:59:54 +1000

    Extend BinanceSpotPermissions enum members

--- nautilus_trader/adapters/binance/spot/enums.py
+++ nautilus_trader/adapters/binance/spot/enums.py
@@ -47,6 +47,17 @@ class BinanceSpotPermissions(Enum):
     TRD_GRP_007 = "TRD_GRP_007"
     TRD_GRP_008 = "TRD_GRP_008"
     TRD_GRP_009 = "TRD_GRP_009"
+    TRD_GRP_010 = "TRD_GRP_010"
+    TRD_GRP_011 = "TRD_GRP_011"
+    TRD_GRP_012 = "TRD_GRP_012"
+    TRD_GRP_013 = "TRD_GRP_013"
+    TRD_GRP_014 = "TRD_GRP_014"
+    TRD_GRP_015 = "TRD_GRP_015"
+    TRD_GRP_016 = "TRD_GRP_016"
+    TRD_GRP_017 = "TRD_GRP_017"
+    TRD_GRP_018 = "TRD_GRP_018"
+    TRD_GRP_019 = "TRD_GRP_019"
+    TRD_GRP_020 = "TRD_GRP_020"
 
 
 @unique
