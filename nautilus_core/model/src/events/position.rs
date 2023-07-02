9683267e7eb13f0eb55bb36246ad0d91e2ac8675
Chris Sellers <chris@cjdsellers.io>
2023-07-03 09:42:46 +1000

    Add position event

--- nautilus_core/model/src/events/position.rs
+++ nautilus_core/model/src/events/position.rs
@@ -80,6 +80,33 @@ pub struct PositionChanged {
 
 #[repr(C)]
 #[derive(Clone, PartialEq, Debug)]
+pub struct PositionState {
+    pub trader_id: TraderId,
+    pub strategy_id: StrategyId,
+    pub instrument_id: InstrumentId,
+    pub position_id: PositionId,
+    pub account_id: AccountId,
+    pub opening_order_id: ClientOrderId,
+    pub entry: OrderSide,
+    pub side: PositionSide,
+    pub signed_qty: f64,
+    pub quantity: Quantity,
+    pub peak_quantity: Quantity,
+    pub last_qty: Quantity,
+    pub last_px: Price,
+    pub currency: Currency,
+    pub avg_px_open: f64,
+    pub avg_px_closed: f64,
+    pub realized_return: f64,
+    pub realized_pnl: Money,
+    pub unrealized_pnl: Money,
+    pub ts_opened: UnixNanos,
+    pub ts_event: UnixNanos,
+    pub ts_init: UnixNanos,
+}
+
+#[repr(C)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct PositionClosed {
     pub trader_id: TraderId,
     pub strategy_id: StrategyId,
