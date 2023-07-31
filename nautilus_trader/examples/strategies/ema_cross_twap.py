cd90e4cb6e3a93d6240e4f6eccd05cf17ddb9ec0
Chris Sellers <chris@cjdsellers.io>
2023-07-31 16:40:20 +1000

    Refine log call

--- nautilus_trader/examples/strategies/ema_cross_twap.py
+++ nautilus_trader/examples/strategies/ema_cross_twap.py
@@ -252,7 +252,7 @@ class EMACrossTWAP(Strategy):
         # Check if indicators ready
         if not self.indicators_initialized():
             self.log.info(
-                f"Waiting for indicators to warm up " f"[{self.cache.bar_count(self.bar_type)}]...",
+                f"Waiting for indicators to warm up [{self.cache.bar_count(self.bar_type)}]...",
                 color=LogColor.BLUE,
             )
             return  # Wait for indicators to warm up...
