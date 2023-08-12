4f4d9151694654769be103677190d70fcfe764ee
Chris Sellers <chris@cjdsellers.io>
2023-08-12 16:42:15 +1000

    Continue core order and events

--- nautilus_core/model/src/orders/limit.rs
+++ nautilus_core/model/src/orders/limit.rs
@@ -19,6 +19,7 @@ use std::{
 };
 
 use nautilus_core::{time::UnixNanos, uuid::UUID4};
+use ustr::Ustr;
 
 use super::base::{Order, OrderCore};
 use crate::{
@@ -70,9 +71,9 @@ impl LimitOrder {
         linked_order_ids: Option<Vec<ClientOrderId>>,
         parent_order_id: Option<ClientOrderId>,
         exec_algorithm_id: Option<ExecAlgorithmId>,
-        exec_algorithm_params: Option<HashMap<String, String>>,
+        exec_algorithm_params: Option<HashMap<Ustr, Ustr>>,
         exec_spawn_id: Option<ClientOrderId>,
-        tags: Option<String>,
+        tags: Option<Ustr>,
         init_id: UUID4,
         ts_init: UnixNanos,
     ) -> Self {
@@ -285,7 +286,7 @@ impl Order for LimitOrder {
         self.exec_algorithm_id
     }
 
-    fn exec_algorithm_params(&self) -> Option<HashMap<String, String>> {
+    fn exec_algorithm_params(&self) -> Option<HashMap<Ustr, Ustr>> {
         self.exec_algorithm_params.clone()
     }
 
@@ -293,8 +294,8 @@ impl Order for LimitOrder {
         self.exec_spawn_id
     }
 
-    fn tags(&self) -> Option<String> {
-        self.tags.clone()
+    fn tags(&self) -> Option<Ustr> {
+        self.tags
     }
 
     fn filled_qty(&self) -> Quantity {
@@ -352,9 +353,9 @@ impl From<OrderInitialized> for LimitOrder {
                 .expect("Error initializing order: `price` was `None` for `LimitOrder"),
             event.time_in_force,
             event.expire_time,
-            event.post_only,
-            event.reduce_only,
-            event.quote_quantity,
+            event.post_only != 0,      // Temporary hack
+            event.reduce_only != 0,    // Temporary hack
+            event.quote_quantity != 0, // Temporary hack
             event.display_qty,
             event.emulation_trigger,
             event.trigger_instrument_id,
