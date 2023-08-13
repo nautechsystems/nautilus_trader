ec8805f36d5c839a3d7dc1bbf5097dc684705d16
limx0 <bradley.mcelroy@live.com>
2023-08-13 19:26:12 +1000

    Don't drop on bids/asks

--- nautilus_trader/model/orderbook/book.pyx
+++ nautilus_trader/model/orderbook/book.pyx
@@ -373,7 +373,7 @@ cdef class OrderBook(Data):
         for i in range(raw_levels_vec.len):
             levels.append(Level.from_mem_c(raw_levels[i]))
 
-        vec_levels_drop(raw_levels_vec)
+        # vec_levels_drop(raw_levels_vec)
 
         return levels
 
@@ -397,7 +397,7 @@ cdef class OrderBook(Data):
         for i in range(raw_levels_vec.len):
             levels.append(Level.from_mem_c(raw_levels[i]))
 
-        vec_levels_drop(raw_levels_vec)
+        # vec_levels_drop(raw_levels_vec)
 
         return levels
 
