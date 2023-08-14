db4e238b05a791fdaab8c00f2a2b804d95a14ba1
Chris Sellers <chris@cjdsellers.io>
2023-08-14 10:30:27 +1000

    Refine core EMA indicator

--- nautilus_core/indicators/src/ema.rs
+++ nautilus_core/indicators/src/ema.rs
@@ -30,8 +30,8 @@ pub struct ExponentialMovingAverage {
     pub alpha: f64,
     pub value: f64,
     pub count: usize,
-    _has_inputs: bool,
-    _is_initialized: bool,
+    has_inputs: bool,
+    is_initialized: bool,
 }
 
 impl Indicator for ExponentialMovingAverage {
@@ -40,89 +40,37 @@ impl Indicator for ExponentialMovingAverage {
     }
 
     fn has_inputs(&self) -> bool {
-        self._has_inputs
+        self.has_inputs
     }
 
     fn is_initialized(&self) -> bool {
-        self._is_initialized
+        self.is_initialized
     }
 
     fn handle_quote_tick(&mut self, tick: &QuoteTick) {
-        self.update_raw(tick.extract_price(self.price_type).into())
+        self.py_update_raw(tick.extract_price(self.price_type).into())
     }
 
     fn handle_trade_tick(&mut self, tick: &TradeTick) {
-        self.update_raw((&tick.price).into())
+        self.py_update_raw((&tick.price).into())
     }
 
     fn handle_bar(&mut self, bar: &Bar) {
-        self.update_raw((&bar.close).into())
+        self.py_update_raw((&bar.close).into())
     }
 
     fn reset(&mut self) {
         self.value = 0.0;
         self.count = 0;
-        self._has_inputs = false;
-        self._is_initialized = false;
+        self.has_inputs = false;
+        self.is_initialized = false;
     }
 }
 
-#[pymethods]
 impl ExponentialMovingAverage {
-    #[must_use]
-    #[new]
-    pub fn new(period: usize, price_type: Option<PriceType>) -> Self {
-        Self {
-            period,
-            price_type: price_type.unwrap_or(PriceType::Last),
-            alpha: 2.0 / (period as f64 + 1.0),
-            value: 0.0,
-            count: 0,
-            _has_inputs: false,
-            _is_initialized: false,
-        }
-    }
-
-    #[getter]
-    #[pyo3(name = "name")]
-    #[must_use]
-    pub fn name_py(&self) -> String {
-        self.name()
-    }
-
-    #[pyo3(name = "has_inputs")]
-    fn has_inputs_py(&self) -> bool {
-        self.has_inputs()
-    }
-
-    #[pyo3(name = "is_initialized")]
-    fn is_initialized_py(&self) -> bool {
-        self._is_initialized
-    }
-
-    #[pyo3(name = "handle_quote_tick")]
-    fn handle_quote_tick_py(&mut self, tick: &QuoteTick) {
-        self.update_raw(tick.extract_price(self.price_type).into())
-    }
-
-    #[pyo3(name = "handle_trade_tick")]
-    fn handle_trade_tick_py(&mut self, tick: &TradeTick) {
-        self.update_raw((&tick.price).into())
-    }
-
-    #[pyo3(name = "handle_bar")]
-    fn handle_bar_py(&mut self, bar: &Bar) {
-        self.update_raw((&bar.close).into())
-    }
-
-    #[pyo3(name = "reset")]
-    fn reset_py(&mut self) {
-        self.reset()
-    }
-
-    pub fn update_raw(&mut self, value: f64) {
-        if !self._has_inputs {
-            self._has_inputs = true;
+    fn update_raw(&mut self, value: f64) {
+        if !self.has_inputs {
+            self.has_inputs = true;
             self.value = value;
         }
 
@@ -130,9 +78,66 @@ impl ExponentialMovingAverage {
         self.count += 1;
 
         // Initialization logic
-        if !self._is_initialized && self.count >= self.period {
-            self._is_initialized = true;
+        if !self.is_initialized && self.count >= self.period {
+            self.is_initialized = true;
         }
+    }
+}
+
+#[pymethods]
+impl ExponentialMovingAverage {
+    #[new]
+    fn new(period: usize, price_type: Option<PriceType>) -> Self {
+        Self {
+            period,
+            price_type: price_type.unwrap_or(PriceType::Last),
+            alpha: 2.0 / (period as f64 + 1.0),
+            value: 0.0,
+            count: 0,
+            has_inputs: false,
+            is_initialized: false,
+        }
+    }
+
+    #[getter]
+    #[pyo3(name = "name")]
+    fn py_name(&self) -> String {
+        self.name()
+    }
+
+    #[pyo3(name = "has_inputs")]
+    fn py_has_inputs(&self) -> bool {
+        self.has_inputs()
+    }
+
+    #[pyo3(name = "is_initialized")]
+    fn py_is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+
+    #[pyo3(name = "handle_quote_tick")]
+    fn py_handle_quote_tick(&mut self, tick: &QuoteTick) {
+        self.py_update_raw(tick.extract_price(self.price_type).into())
+    }
+
+    #[pyo3(name = "handle_trade_tick")]
+    fn py_handle_trade_tick(&mut self, tick: &TradeTick) {
+        self.update_raw((&tick.price).into())
+    }
+
+    #[pyo3(name = "handle_bar")]
+    fn py_handle_bar(&mut self, bar: &Bar) {
+        self.update_raw((&bar.close).into())
+    }
+
+    #[pyo3(name = "reset")]
+    fn py_reset(&mut self) {
+        self.reset()
+    }
+
+    #[pyo3(name = "update_raw")]
+    fn py_update_raw(&mut self, value: f64) {
+        self.update_raw(value)
     }
 }
 
@@ -147,15 +152,15 @@ mod tests {
     fn test_ema_initialized() {
         let ema = ExponentialMovingAverage::new(20, Some(PriceType::Mid));
         let display_str = format!("{ema:?}");
-        assert_eq!(display_str, "ExponentialMovingAverage { period: 20, price_type: Mid, alpha: 0.09523809523809523, value: 0.0, count: 0, _has_inputs: false, _is_initialized: false }");
+        assert_eq!(display_str, "ExponentialMovingAverage { period: 20, price_type: Mid, alpha: 0.09523809523809523, value: 0.0, count: 0, has_inputs: false, is_initialized: false }");
     }
 
     #[test]
     fn test_ema_update_raw() {
         let mut ema = ExponentialMovingAverage::new(3, Some(PriceType::Mid));
-        ema.update_raw(1.0);
-        ema.update_raw(2.0);
-        ema.update_raw(3.0);
+        ema.py_update_raw(1.0);
+        ema.py_update_raw(2.0);
+        ema.py_update_raw(3.0);
 
         assert!(ema.has_inputs());
         assert!(ema.is_initialized());
@@ -166,9 +171,9 @@ mod tests {
     #[test]
     fn test_ema_reset() {
         let mut ema = ExponentialMovingAverage::new(3, Some(PriceType::Mid));
-        ema.update_raw(1.0);
-        ema.update_raw(2.0);
-        ema.update_raw(3.0);
+        ema.py_update_raw(1.0);
+        ema.py_update_raw(2.0);
+        ema.py_update_raw(3.0);
 
         ema.reset();
 
