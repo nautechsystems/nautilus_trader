95c89f96b504dd4ac1ee531de3f521d12c8b1466
object <105505815+objectin@users.noreply.github.com>
2023-09-07 19:32:19 +0900

    Fix

--- nautilus_core/model/src/orderbook/book.rs
+++ nautilus_core/model/src/orderbook/book.rs
@@ -256,7 +256,7 @@ impl OrderBook {
         let mut matched_volume: f64 = 0.0;
 
         for (book_price, level) in levels {
-            if book_price == &BookPrice::Limit(price) {
+            if book_price == price {
                 matched_volume += level.volume();
             }
         }
