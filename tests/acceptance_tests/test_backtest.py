6b1a696d04bb8791ecef244280ff2b23eb5bbb85
limx0 <bradley.mcelroy@live.com>
2023-09-17 12:39:01 +1000

    Fixing tests

--- tests/acceptance_tests/test_backtest.py
+++ tests/acceptance_tests/test_backtest.py
@@ -756,7 +756,7 @@ class TestBacktestAcceptanceTestsOrderBookImbalance:
         self.engine.run()
 
         # Assert
-        assert self.engine.iteration in (8199, 7812)
+        assert self.engine.iteration in (8198, 7812)
 
 
 class TestBacktestAcceptanceTestsMarketMaking:
