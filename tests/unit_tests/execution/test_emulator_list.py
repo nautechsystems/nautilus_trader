b00cf53cd7996a272e5fa728e56f318651f30778
Bshara Zahran <bshara23@gmail.com>
2023-09-19 21:40:37 +1000

    Fix OrderEmulator handling of CancelOrder

--- tests/unit_tests/execution/test_emulator_list.py
+++ tests/unit_tests/execution/test_emulator_list.py
@@ -1213,6 +1213,48 @@ class TestOrderEmulatorWithOrderLists:
         assert bracket.orders[1].status == OrderStatus.EMULATED
         assert bracket.orders[2].status == OrderStatus.EMULATED
 
+    def test_restart_emulator_then_cancel_bracket(self):
+        # Arrange - Prepare market
+        bracket = self.strategy.order_factory.bracket(
+            instrument_id=ETHUSDT_PERP_BINANCE.id,
+            order_side=OrderSide.BUY,
+            quantity=ETHUSDT_PERP_BINANCE.make_qty(10),
+            entry_price=ETHUSDT_PERP_BINANCE.make_price(5000.0),
+            sl_trigger_price=ETHUSDT_PERP_BINANCE.make_price(4900.0),
+            tp_price=ETHUSDT_PERP_BINANCE.make_price(5100.0),
+            entry_order_type=OrderType.LIMIT,
+            emulation_trigger=TriggerType.BID_ASK,
+            contingency_type=ContingencyType.OUO,
+        )
+
+        self.strategy.submit_order_list(
+            order_list=bracket,
+            position_id=PositionId("P-001"),
+        )
+
+        tick = TestDataStubs.quote_tick(
+            instrument=ETHUSDT_PERP_BINANCE,
+            bid_price=5000.0,
+            ask_price=5000.0,
+        )
+
+        self.data_engine.process(tick)
+        self.exchange.process_quote_tick(tick)
+        self.exchange.process(0)
+
+        self.emulator.stop()
+        self.emulator.reset()
+        self.emulator.start()
+
+        # Act
+        self.strategy.cancel_order(bracket.orders[1])
+
+        # Assert
+        entry_order = self.cache.order(bracket.orders[0].client_order_id)
+        assert entry_order.status == OrderStatus.FILLED
+        assert bracket.orders[1].status == OrderStatus.CANCELED
+        assert bracket.orders[2].status == OrderStatus.CANCELED
+
     def test_restart_emulator_with_closed_parent_position(self):
         # Arrange - Prepare market
         bracket = self.strategy.order_factory.bracket(
