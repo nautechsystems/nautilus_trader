5b3b5c65891507705b6e5c96543943d120b3a2c5
David Blom <davidsblom@gmail.com>
2023-10-28 08:38:53 +0200

    data config: validate that the end time is set after the start time

--- nautilus_trader/backtest/node.py
+++ nautilus_trader/backtest/node.py
@@ -151,6 +151,16 @@ class BacktestNode:
             for data_config in config.data:
                 if data_config.instrument_id is None:
                     continue  # No instrument associated with data
+
+                if (
+                    data_config.start_time is not None
+                    and data_config.end_time is not None
+                    and data_config.end_time < data_config.start_time
+                ):
+                    raise ValueError(
+                        f"Invalid data config: end_time ({data_config.end_time}) is before start_time ({data_config.start_time})."
+                    )
+
                 instrument_id: InstrumentId = InstrumentId.from_str(data_config.instrument_id)
                 if instrument_id.venue not in venue_ids:
                     raise ValueError(
