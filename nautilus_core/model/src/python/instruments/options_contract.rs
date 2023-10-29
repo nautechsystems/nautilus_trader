dd4832d9bd38c840a7b30b0b924b634e59ce56c8
Filip Macek <filip_macek@hotmail.com>
2023-10-29 22:00:21 +0100

    init options contract rust pyo3

--- nautilus_core/model/src/python/instruments/options_contract.rs
+++ nautilus_core/model/src/python/instruments/options_contract.rs
@@ -13,7 +13,3 @@
 //  limitations under the License.
 // -------------------------------------------------------------------------------------------------
 
-pub mod crypto_future;
-pub mod crypto_perpetual;
-pub mod currency_pair;
-pub mod options_contract;
