3afb5524f1555d9bbfa6b148224a3946d6d7147c
Filip Macek <filip_macek@hotmail.com>
2023-10-29 23:00:06 +0100

    refactor existing python pyo3 tests for instruments

--- tests/unit_tests/model/instruments/test_currency_pair_pyo3.py
+++ tests/unit_tests/model/instruments/test_currency_pair_pyo3.py
@@ -20,35 +20,36 @@ from nautilus_trader.test_kit.rust.instruments import TestInstrumentProviderPyo3
 btcusdt_binance = TestInstrumentProviderPyo3.btcusdt_binance()
 
 
-class TestCurrencyPairPyo3:
-    def test_equality(self):
-        item_1 = TestInstrumentProviderPyo3.btcusdt_binance()
-        item_2 = TestInstrumentProviderPyo3.btcusdt_binance()
-        assert item_1 == item_2
+def test_equality():
+    item_1 = TestInstrumentProviderPyo3.btcusdt_binance()
+    item_2 = TestInstrumentProviderPyo3.btcusdt_binance()
+    assert item_1 == item_2
 
-    def test_hash(self):
-        assert hash(btcusdt_binance) == hash(btcusdt_binance)
 
-    def test_to_dict(self):
-        dict = btcusdt_binance.to_dict()
-        assert CurrencyPair.from_dict(dict) == btcusdt_binance
-        assert dict == {
-            "type": "CurrencyPair",
-            "id": "BTCUSDT.BINANCE",
-            "raw_symbol": "BTCUSDT",
-            "base_currency": "BTC",
-            "quote_currency": "USDT",
-            "price_precision": 2,
-            "size_precision": 6,
-            "price_increment": "0.01",
-            "size_increment": "0.000001",
-            "margin_maint": 0.0,
-            "margin_init": 0.0,
-            "maker_fee": 0.0,
-            "taker_fee": 0.0,
-            "lot_size": None,
-            "max_quantity": "9000",
-            "min_quantity": "0.00001",
-            "min_price": "0.01",
-            "max_price": "1000000",
-        }
+def test_hash():
+    assert hash(btcusdt_binance) == hash(btcusdt_binance)
+
+
+def test_to_dict():
+    dict = btcusdt_binance.to_dict()
+    assert CurrencyPair.from_dict(dict) == btcusdt_binance
+    assert dict == {
+        "type": "CurrencyPair",
+        "id": "BTCUSDT.BINANCE",
+        "raw_symbol": "BTCUSDT",
+        "base_currency": "BTC",
+        "quote_currency": "USDT",
+        "price_precision": 2,
+        "size_precision": 6,
+        "price_increment": "0.01",
+        "size_increment": "0.000001",
+        "margin_maint": 0.0,
+        "margin_init": 0.0,
+        "maker_fee": 0.0,
+        "taker_fee": 0.0,
+        "lot_size": None,
+        "max_quantity": "9000",
+        "min_quantity": "0.00001",
+        "min_price": "0.01",
+        "max_price": "1000000",
+    }
