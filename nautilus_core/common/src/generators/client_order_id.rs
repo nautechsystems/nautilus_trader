66acfe6418876bd0cf2166f873ee9c7688f184a6
Bshara Zahran <bshara23@gmail.com>
2023-11-05 11:37:51 -0800

    Minor cleanups

--- nautilus_core/common/src/generators/client_order_id.rs
+++ nautilus_core/common/src/generators/client_order_id.rs
@@ -13,21 +13,6 @@
 //  limitations under the License.
 // -------------------------------------------------------------------------------------------------
 
-// -------------------------------------------------------------------------------------------------
-//  Copyright (C) 2015-2023 Nautech Systems Pty Ltd. All rights reserved.
-//  https://nautechsystems.io
-//
-//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
-//  You may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-// -------------------------------------------------------------------------------------------------
-
 use std::sync::{Arc, Mutex};
 
 use chrono::{Datelike, NaiveDateTime, Timelike};
@@ -37,7 +22,6 @@ use nautilus_model::identifiers::{
 
 use crate::{clock::Clock, generators::IdentifierGenerator};
 
-#[derive()]
 #[repr(C)]
 pub struct ClientOrderIdGenerator {
     trader_id: TraderId,
@@ -68,6 +52,7 @@ impl IdentifierGenerator<ClientOrderId> for ClientOrderIdGenerator {
         let mut c = self.count.lock().unwrap();
         *c = count;
     }
+
     fn reset(&mut self) {
         let mut c = self.count.lock().unwrap();
         *c = 0;
@@ -77,6 +62,7 @@ impl IdentifierGenerator<ClientOrderId> for ClientOrderIdGenerator {
         let c = self.count.lock().unwrap();
         *c
     }
+
     fn generate(&mut self) -> ClientOrderId {
         let datetime_tag = self.get_datetime_tag();
         let trader_tag = self.trader_id.get_tag();
