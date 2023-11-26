2618d81d0fbc1f8bc2c0e6a9461246f162ac88a4
Bshara Zahran <bshara23@gmail.com>
2023-11-26 12:20:09 +1100

    Refine RedisCacheDatabase scaffolding in Rust

--- nautilus_core/infrastructure/src/cache.rs
+++ nautilus_core/infrastructure/src/cache.rs
@@ -32,9 +32,11 @@ impl DatabaseOperation {
 }
 
 pub trait CacheDatabase {
-    fn new(trader_id: TraderId, config: HashMap<String, Value>) -> Self;
-    fn read(&mut self, op_type: String) -> Vec<Vec<u8>>;
-    fn write(&mut self, op_type: String, payload: Vec<Vec<u8>>) -> Result<(), String>;
+    type DatabaseType;
+
+    fn new(trader_id: TraderId, config: HashMap<String, Value>) -> Result<Self::DatabaseType>;
+    fn read(&mut self, op_type: String) -> Result<Vec<Vec<u8>>>;
+    fn write(&mut self, op_type: String, payload: Vec<Vec<u8>>) -> Result<String>;
     fn handle_ops(
         trader_id: TraderId,
         config: HashMap<String, Value>,
--- nautilus_core/infrastructure/src/python/cache.rs
+++ nautilus_core/infrastructure/src/python/cache.rs
@@ -12,3 +12,52 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 // -------------------------------------------------------------------------------------------------
+
+use std::collections::HashMap;
+
+use nautilus_core::python::{to_pyruntime_err, to_pyvalue_err};
+use nautilus_model::identifiers::trader_id::TraderId;
+use pyo3::{prelude::*, types::PyDict, PyResult};
+
+use crate::{cache::CacheDatabase, redis::RedisCacheDatabase};
+
+#[pymethods]
+impl RedisCacheDatabase {
+    #[new]
+    fn py_new(trader_id: TraderId, config: &PyDict) -> PyResult<Self> {
+        let mut config_map = HashMap::new();
+        for (key, value) in config {
+            // Extract key and value as strings
+            let key_str = key.extract::<String>()?;
+            let value_str = value.extract::<String>()?;
+
+            // Convert the value to a serde_json::Value
+            let value_json: serde_json::Value =
+                serde_json::from_str(&value_str).map_err(to_pyvalue_err)?;
+
+            // Insert into the HashMap
+            config_map.insert(key_str, value_json);
+        }
+
+        match Self::new(trader_id, config_map) {
+            Ok(cache) => Ok(cache),
+            Err(e) => Err(to_pyruntime_err(e.to_string())),
+        }
+    }
+
+    #[pyo3(name = "read")]
+    fn py_read(&mut self, op_type: String) -> PyResult<Vec<Vec<u8>>> {
+        match self.read(op_type) {
+            Ok(result) => Ok(result),
+            Err(e) => Err(to_pyruntime_err(e)),
+        }
+    }
+
+    #[pyo3(name = "write")]
+    fn py_write(&mut self, op_type: String, payload: Vec<Vec<u8>>) -> PyResult<String> {
+        match self.write(op_type, payload) {
+            Ok(ok) => Ok(ok),
+            Err(e) => Err(to_pyvalue_err(e)),
+        }
+    }
+}
