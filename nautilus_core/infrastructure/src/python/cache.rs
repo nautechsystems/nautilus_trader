fc07af108a1179687e9c492205d9f9c6f863fccf
Bshara Zahran <bshara23@gmail.com>
2023-11-27 19:39:38 +1100

    Continue RedisCacheDatabase in Rust

--- nautilus_core/infrastructure/src/python/cache.rs
+++ nautilus_core/infrastructure/src/python/cache.rs
@@ -15,7 +15,10 @@
 
 use std::collections::HashMap;
 
-use nautilus_core::python::{to_pyruntime_err, to_pyvalue_err};
+use nautilus_core::{
+    python::{to_pyruntime_err, to_pyvalue_err},
+    uuid::UUID4,
+};
 use nautilus_model::identifiers::trader_id::TraderId;
 use pyo3::{prelude::*, PyResult};
 use serde_json::Value;
@@ -25,11 +28,11 @@ use crate::{cache::CacheDatabase, redis::RedisCacheDatabase};
 #[pymethods]
 impl RedisCacheDatabase {
     #[new]
-    fn py_new(trader_id: TraderId, config_json: Vec<u8>) -> PyResult<Self> {
+    fn py_new(trader_id: TraderId, instance_id: UUID4, config_json: Vec<u8>) -> PyResult<Self> {
         let config: HashMap<String, Value> =
             serde_json::from_slice(&config_json).map_err(to_pyvalue_err)?;
 
-        match Self::new(trader_id, config) {
+        match Self::new(trader_id, instance_id, config) {
             Ok(cache) => Ok(cache),
             Err(e) => Err(to_pyruntime_err(e.to_string())),
         }
