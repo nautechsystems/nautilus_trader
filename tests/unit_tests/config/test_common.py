50dd17ee0b14849aa7d59107b6ad269e6fb8d49f
Bshara Zahran <bshara23@gmail.com>
2023-12-11 08:42:04 +1100

    Refactor config tests

--- tests/unit_tests/config/test_common.py
+++ tests/unit_tests/config/test_common.py
@@ -13,10 +13,9 @@
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
 
-import unittest
-
 import msgspec.json
 import pandas as pd
+import pytest
 
 from nautilus_trader.config import ImportableConfig
 from nautilus_trader.config.common import CUSTOM_DECODINGS
@@ -33,234 +32,248 @@ from nautilus_trader.model.objects import Price
 from nautilus_trader.model.objects import Quantity
 
 
-class TestConfigCommon(unittest.TestCase):
-    def test_importable_config_simple(self):
-        # Arrange
-        raw = msgspec.json.encode(
-            {
-                "path": "nautilus_trader.adapters.binance.config:BinanceDataClientConfig",
-                "config": {
-                    "api_key": "abc",
-                },
+def test_importable_config_simple() -> None:
+    # Arrange
+    raw = msgspec.json.encode(
+        {
+            "path": "nautilus_trader.adapters.binance.config:BinanceDataClientConfig",
+            "config": {
+                "api_key": "abc",
             },
-        )
+        },
+    )
 
-        # Act
-        config = msgspec.json.decode(raw, type=ImportableConfig).create()
+    # Act
+    config = msgspec.json.decode(raw, type=ImportableConfig).create()
 
-        # Assert
-        assert config.api_key == "abc"
+    # Assert
+    assert config.api_key == "abc"
 
-    def test_register_custom_encodings(self):
-        # Arrange
-        test_encoder = str
-        register_json_encoding(Price, test_encoder)
 
-        # Assert
-        assert CUSTOM_ENCODINGS[Price] == test_encoder
+def test_register_custom_encodings() -> None:
+    # Arrange
+    test_encoder = str
 
-    def test_register_custom_decodings(self):
-        # Arrange
-        test_decoder = Price.from_str
-        register_json_decoding(Price, test_decoder)
+    # Act
+    register_json_encoding(Price, test_encoder)
 
-        # Assert
-        assert CUSTOM_DECODINGS[Price] == test_decoder
+    # Assert
+    assert CUSTOM_ENCODINGS[Price] == test_encoder
 
-    # Test for UUID4
-    def test_encoding_uuid4(self):
-        # Arrange
-        obj = UUID4()
 
-        # Act
-        result = msgspec_encoding_hook(obj)
+def test_register_custom_decodings() -> None:
+    # Arrange
+    test_decoder = Price.from_str
+    register_json_decoding(Price, test_decoder)
 
-        # Assert
-        assert result == obj.value
+    # Assert
+    assert CUSTOM_DECODINGS[Price] == test_decoder
 
-    def test_decoding_uuid4(self):
-        # Arrange
-        obj_type = UUID4
-        obj = "b07bf5fa-cee6-49eb-91b1-a08d09d22a1a"
 
-        # Act
-        result = msgspec_decoding_hook(obj_type, obj)
+def test_encoding_uuid4() -> None:
+    # Arrange
+    obj = UUID4()
 
-        # Assert
-        assert result == UUID4(obj)
+    # Act
+    result = msgspec_encoding_hook(obj)
 
-    def test_encoding_component_id(self):
-        # Arrange
-        obj = ComponentId("TRADER-001")
+    # Assert
+    assert result == obj.value
 
-        # Act
-        result = msgspec_encoding_hook(obj)
 
-        # Assert
-        assert result == obj.value
+def test_decoding_uuid4() -> None:
+    # Arrange
+    obj_type = UUID4
+    obj = "b07bf5fa-cee6-49eb-91b1-a08d09d22a1a"
 
-    def test_decoding_component_id(self):
-        # Arrange
-        obj_type = ComponentId
-        obj = "TRADER-001"
+    # Act
+    result = msgspec_decoding_hook(obj_type, obj)
 
-        # Act
-        result = msgspec_decoding_hook(obj_type, obj)
+    # Assert
+    assert result == UUID4(obj)
 
-        # Assert
-        assert result == ComponentId(obj)
 
-    def test_encoding_instrument_id(self):
-        # Arrange
-        obj = InstrumentId.from_str("AUD/USD.SIM")
+def test_encoding_component_id() -> None:
+    # Arrange
+    obj = ComponentId("TRADER-001")
 
-        # Act
-        result = msgspec_encoding_hook(obj)
+    # Act
+    result = msgspec_encoding_hook(obj)
 
-        # Assert
-        assert result == str(obj)
+    # Assert
+    assert result == obj.value
 
-    def test_decoding_instrument_id(self):
-        # Arrange
-        obj_type = InstrumentId
-        obj = "AUD/USD.SIM"
 
-        # Act
-        result = msgspec_decoding_hook(obj_type, obj)
+def test_decoding_component_id() -> None:
+    # Arrange
+    obj_type = ComponentId
+    obj = "TRADER-001"
 
-        # Assert
-        assert result == InstrumentId.from_str(obj)
+    # Act
+    result = msgspec_decoding_hook(obj_type, obj)
 
-    def test_encoding_bar_type(self):
-        # Arrange
-        obj = BarType.from_str("AUD/USD.SIM-100-TICK-MID-INTERNAL")
+    # Assert
+    assert result == ComponentId(obj)
 
-        # Act
-        result = msgspec_encoding_hook(obj)
 
-        # Assert
-        assert result == str(obj)
+def test_encoding_instrument_id() -> None:
+    # Arrange
+    obj = InstrumentId.from_str("AUD/USD.SIM")
 
-    def test_decoding_bar_type(self):
-        # Arrange
-        obj_type = BarType
-        obj = "AUD/USD.SIM-100-TICK-MID-INTERNAL"
+    # Act
+    result = msgspec_encoding_hook(obj)
 
-        # Act
-        result = msgspec_decoding_hook(obj_type, obj)
+    # Assert
+    assert result == str(obj)
 
-        # Assert
-        assert result == BarType.from_str(obj)
 
-    def test_encoding_price(self):
-        # Arrange
-        obj = Price.from_str("1.2345")
+def test_decoding_instrument_id() -> None:
+    # Arrange
+    obj_type = InstrumentId
+    obj = "AUD/USD.SIM"
 
-        # Act
-        result = msgspec_encoding_hook(obj)
+    # Act
+    result = msgspec_decoding_hook(obj_type, obj)
 
-        # Assert
-        assert result == str(obj)
+    # Assert
+    assert result == InstrumentId.from_str(obj)
 
-    def test_decoding_price(self):
-        # Arrange
-        obj_type = Price
-        obj = "1.2345"
 
-        # Act
-        result = msgspec_decoding_hook(obj_type, obj)
+def test_encoding_bar_type() -> None:
+    # Arrange
+    obj = BarType.from_str("AUD/USD.SIM-100-TICK-MID-INTERNAL")
 
-        # Assert
-        assert result == Price.from_str(obj)
+    # Act
+    result = msgspec_encoding_hook(obj)
 
-    def test_encoding_quatity(self):
-        # Arrange
-        obj = Quantity.from_str("100000")
+    # Assert
+    assert result == str(obj)
 
-        # Act
-        result = msgspec_encoding_hook(obj)
 
-        # Assert
-        assert result == str(obj)
+def test_decoding_bar_type() -> None:
+    # Arrange
+    obj_type = BarType
+    obj = "AUD/USD.SIM-100-TICK-MID-INTERNAL"
 
-    def test_decoding_quatity(self):
-        # Arrange
-        obj_type = Quantity
-        obj = "100000"
+    # Act
+    result = msgspec_decoding_hook(obj_type, obj)
 
-        # Act
-        result = msgspec_decoding_hook(obj_type, obj)
+    # Assert
+    assert result == BarType.from_str(obj)
 
-        # Assert
-        assert result == Quantity.from_str(obj)
 
-    def test_encoding_timestamp(self):
-        # Arrange
-        obj = pd.Timestamp("2023-01-01")
+def test_encoding_price() -> None:
+    # Arrange
+    obj = Price.from_str("1.2345")
 
-        # Act
-        result = msgspec_encoding_hook(obj)
+    # Act
+    result = msgspec_encoding_hook(obj)
 
-        # Assert
-        assert result == obj.isoformat()
+    # Assert
+    assert result == str(obj)
 
-    def test_decoding_timestamp(self):
-        # Arrange
-        obj_type = pd.Timestamp
-        obj = "2023-01-01T00:00:00"
 
-        # Act
-        result = msgspec_decoding_hook(obj_type, obj)
+def test_decoding_price() -> None:
+    # Arrange
+    obj_type = Price
+    obj = "1.2345"
 
-        # Assert
-        assert result == pd.Timestamp(obj)
+    # Act
+    result = msgspec_decoding_hook(obj_type, obj)
 
-    def test_encoding_timedelta(self):
-        # Arrange
-        obj = pd.Timedelta("1 HOUR")
+    # Assert
+    assert result == Price.from_str(obj)
 
-        # Act
-        result = msgspec_encoding_hook(obj)
 
-        # Assert
-        assert result == obj.isoformat()
+def test_encoding_quatity() -> None:
+    # Arrange
+    obj = Quantity.from_str("100000")
 
-    def test_decoding_timedelta(self):
-        # Arrange
-        obj_type = pd.Timedelta
-        obj = "P0DT1H0M0S"
+    # Act
+    result = msgspec_encoding_hook(obj)
 
-        # Act
-        result = msgspec_decoding_hook(obj_type, obj)
+    # Assert
+    assert result == str(obj)
 
-        # Assert
-        assert result == pd.Timedelta(obj)
 
-    def test_encoding_unsupported_type(self):
-        # Arrange
-        unsupported_obj = [1, 2, 3]
+def test_decoding_quatity() -> None:
+    # Arrange
+    obj_type = Quantity
+    obj = "100000"
 
-        # Act & Assert
-        with self.assertRaises(TypeError) as context:
-            msgspec_encoding_hook(unsupported_obj)
+    # Act
+    result = msgspec_decoding_hook(obj_type, obj)
+
+    # Assert
+    assert result == Quantity.from_str(obj)
+
+
+def test_encoding_timestamp() -> None:
+    # Arrange
+    obj = pd.Timestamp("2023-01-01")
+
+    # Act
+    result = msgspec_encoding_hook(obj)
+
+    # Assert
+    assert result == obj.isoformat()
+
+
+def test_decoding_timestamp() -> None:
+    # Arrange
+    obj_type = pd.Timestamp
+    obj = "2023-01-01T00:00:00"
+
+    # Act
+    result = msgspec_decoding_hook(obj_type, obj)
+
+    # Assert
+    assert result == pd.Timestamp(obj)
+
+
+def test_encoding_timedelta() -> None:
+    # Arrange
+    obj = pd.Timedelta("1 HOUR")
+
+    # Act
+    result = msgspec_encoding_hook(obj)
+
+    # Assert
+    assert result == obj.isoformat()
+
+
+def test_decoding_timedelta() -> None:
+    # Arrange
+    obj_type = pd.Timedelta
+    obj = "P0DT1H0M0S"
+
+    # Act
+    result = msgspec_decoding_hook(obj_type, obj)
+
+    # Assert
+    assert result == pd.Timedelta(obj)
+
+
+def test_encoding_unsupported_type() -> None:
+    # Arrange
+    unsupported_obj: list[int] = [1, 2, 3]
+
+    # Act, Assert
+    with pytest.raises(TypeError) as exinfo:
+        msgspec_encoding_hook(unsupported_obj)
 
         # Verifying the exception message
-        self.assertIn(
-            "Encoding objects of type <class 'list'> is unsupported",
-            str(context.exception),
-        )
+        assert str(exinfo) == "Encoding objects of type <class 'list'> is unsupported"
 
-    def test_decoding_unsupported_type(self):
-        # Arrange
-        unsupported_type = list
-        unsupported_obj = "[1, 2, 3]"
 
-        # Act & Assert
-        with self.assertRaises(TypeError) as context:
-            msgspec_decoding_hook(unsupported_type, unsupported_obj)
+def test_decoding_unsupported_type() -> None:
+    # Arrange
+    unsupported_type = list
+    unsupported_obj = "[1, 2, 3]"
+
+    # Act, Assert
+    with pytest.raises(TypeError) as exinfo:
+        msgspec_decoding_hook(unsupported_type, unsupported_obj)
 
         # Verifying the exception message
-        self.assertIn(
-            "Decoding objects of type <class 'list'> is unsupported",
-            str(context.exception),
-        )
+        assert str(exinfo) == "Decoding objects of type <class 'list'> is unsupported"
