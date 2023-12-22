061e8f36d76ed0fb3519332c2f88d76a19eae2fa
Chris Sellers <chris@cjdsellers.io>
2023-12-22 22:18:06 +1100

    Fix DatabentoInstrumentProvider

--- tests/integration_tests/adapters/databento/sandbox/sandbox_instrument_provider.py
+++ tests/integration_tests/adapters/databento/sandbox/sandbox_instrument_provider.py
@@ -41,7 +41,7 @@ async def test_databento_instrument_provider():
     await provider.load_async(InstrumentId.from_str("ESH4.GLBX"))
 
     instrument_ids = [
-        InstrumentId.from_str("ESZ3.GLBX"),
+        # InstrumentId.from_str("ESZ3.GLBX"),
         InstrumentId.from_str("ESH4.GLBX"),
         InstrumentId.from_str("ESM4.GLBX"),
         # InstrumentId.from_str("AAPL.XNAS"),
