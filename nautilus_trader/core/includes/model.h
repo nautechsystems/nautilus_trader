2dd51870028c3aedee7e97174843195267e620d3
Bshara Zahran <bshara23@gmail.com>
2024-01-01 12:27:17 +1100

    Add core OrderBook apply deltas function

--- nautilus_trader/core/includes/model.h
+++ nautilus_trader/core/includes/model.h
@@ -2061,6 +2061,8 @@ void orderbook_clear_asks(struct OrderBook_API *book, uint64_t ts_event, uint64_
 
 void orderbook_apply_delta(struct OrderBook_API *book, struct OrderBookDelta_t delta);
 
+void orderbook_apply_deltas(struct OrderBook_API *book, struct OrderBookDeltas_t deltas);
+
 CVec orderbook_bids(struct OrderBook_API *book);
 
 CVec orderbook_asks(struct OrderBook_API *book);
