030c32fe6b9704afdcc8b38cd7c9da2f25d00246
Bshara Zahran <bshara23@gmail.com>
2024-01-07 17:29:16 +1100

    Add catalog performance tests

--- nautilus_trader/test_kit/mocks/data.py
+++ nautilus_trader/test_kit/mocks/data.py
@@ -16,18 +16,17 @@
 from pathlib import Path
 from typing import Literal
 
-from nautilus_trader.common.clock import TestClock
-from nautilus_trader.common.logging import Logger
-from nautilus_trader.common.providers import InstrumentProvider
 from nautilus_trader.persistence.catalog.parquet import ParquetDataCatalog
 from nautilus_trader.persistence.catalog.singleton import clear_singleton_instances
 from nautilus_trader.persistence.wranglers import QuoteTickDataWrangler
+from nautilus_trader.persistence.wranglers import TradeTickDataWrangler
 from nautilus_trader.test_kit.providers import TestDataProvider
 from nautilus_trader.test_kit.providers import TestInstrumentProvider
 from nautilus_trader.trading.filters import NewsEvent
 
 
 _AUDUSD_SIM = TestInstrumentProvider.default_fx_ccy("AUD/USD")
+_ETHUSDT_BINANCE = TestInstrumentProvider.ethusdt_binance()
 
 
 class NewsEventData(NewsEvent):
@@ -63,16 +62,16 @@ def setup_catalog(
 
 
 def load_catalog_with_stub_quote_ticks_audusd(catalog: ParquetDataCatalog) -> None:
-    clock = TestClock()
-    logger = Logger(clock)
-
-    instrument_provider = InstrumentProvider(
-        logger=logger,
-    )
-    instrument_provider.add(_AUDUSD_SIM)
-
     wrangler = QuoteTickDataWrangler(_AUDUSD_SIM)
     ticks = wrangler.process(TestDataProvider().read_csv_ticks("truefx/audusd-ticks.csv"))
     ticks.sort(key=lambda x: x.ts_init)  # CAUTION: data was not originally sorted
     catalog.write_data([_AUDUSD_SIM])
+    catalog.write_data(ticks)
+
+
+def load_catalog_with_stub_trade_ticks_ethusdt(catalog: ParquetDataCatalog) -> None:
+    wrangler = TradeTickDataWrangler(_ETHUSDT_BINANCE)
+    ticks = wrangler.process(TestDataProvider().read_csv_ticks("binance/ethusdt-trades.csv"))
+    # ticks.sort(key=lambda x: x.ts_init)
+    catalog.write_data([_ETHUSDT_BINANCE])
     catalog.write_data(ticks)
