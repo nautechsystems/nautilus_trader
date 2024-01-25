55c93c65d9f88b8a7628fa36ea2f5a19ad0c3c3a
rsmb7z <105105941+rsmb7z@users.noreply.github.com>
2024-01-25 20:20:10 +0300

    Fix type assignment in StrategyFactory for StrategyConfig

--- tests/unit_tests/backtest/test_node.py
+++ tests/unit_tests/backtest/test_node.py
@@ -13,7 +13,6 @@
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
 
-from decimal import Decimal
 
 import msgspec
 import pytest
@@ -25,7 +24,6 @@ from nautilus_trader.config import BacktestRunConfig
 from nautilus_trader.config import BacktestVenueConfig
 from nautilus_trader.config import ImportableStrategyConfig
 from nautilus_trader.config import LoggingConfig
-from nautilus_trader.model.data import BarType
 from nautilus_trader.model.data import QuoteTick
 from nautilus_trader.model.identifiers import InstrumentId
 from nautilus_trader.persistence.funcs import parse_bytes
@@ -57,11 +55,11 @@ class TestBacktestNode:
                 strategy_path="nautilus_trader.examples.strategies.ema_cross:EMACross",
                 config_path="nautilus_trader.examples.strategies.ema_cross:EMACrossConfig",
                 config={
-                    "instrument_id": InstrumentId.from_str("AUD/USD.SIM"),
-                    "bar_type": BarType.from_str("AUD/USD.SIM-100-TICK-MID-INTERNAL"),
+                    "instrument_id": "AUD/USD.SIM",
+                    "bar_type": "AUD/USD.SIM-100-TICK-MID-INTERNAL",
                     "fast_ema_period": 10,
                     "slow_ema_period": 20,
-                    "trade_size": Decimal(1_000_000),
+                    "trade_size": "1_000_000",
                     "order_id_tag": "001",
                 },
             ),
