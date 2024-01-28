c0410d6b184c780f6e619bd367c7321db2b3c4f2
Chris Sellers <chris@cjdsellers.io>
2024-01-29 07:58:19 +1100

    Add pyo3 conversion performance benchmarks

---
+++ tests/performance_tests/test_perf_pyo3_conversion.py
@@ -0,0 +1,108 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2024 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
+from nautilus_trader.model.data import Bar
+from nautilus_trader.model.data import OrderBookDelta
+from nautilus_trader.model.data import QuoteTick
+from nautilus_trader.model.data import TradeTick
+from nautilus_trader.test_kit.rust.data_pyo3 import TestDataProviderPyo3
+
+
+def test_pyo3_delta_to_legacy_cython(benchmark):
+    pyo3_delta = TestDataProviderPyo3.order_book_delta()
+
+    benchmark.pedantic(
+        target=OrderBookDelta.from_pyo3,
+        args=(pyo3_delta,),
+        rounds=100_000,
+        iterations=1,
+    )
+
+
+def test_pyo3_deltas_to_legacy_cython_list(benchmark):
+    pyo3_deltas = [TestDataProviderPyo3.order_book_delta()] * 10_000
+
+    benchmark.pedantic(
+        target=OrderBookDelta.from_pyo3_list,
+        args=(pyo3_deltas,),
+        rounds=10,
+        iterations=1,
+    )
+
+
+def test_pyo3_quote_to_legacy_cython(benchmark):
+    pyo3_quote = TestDataProviderPyo3.quote_tick()
+
+    benchmark.pedantic(
+        target=QuoteTick.from_pyo3,
+        args=(pyo3_quote,),
+        rounds=100_000,
+        iterations=1,
+    )
+
+
+def test_pyo3_quotes_to_legacy_cython_list(benchmark):
+    pyo3_quotes = [TestDataProviderPyo3.quote_tick()] * 10_000
+
+    benchmark.pedantic(
+        target=QuoteTick.from_pyo3_list,
+        args=(pyo3_quotes,),
+        rounds=10,
+        iterations=1,
+    )
+
+
+def test_pyo3_trade_to_legacy_cython(benchmark):
+    pyo3_trade = TestDataProviderPyo3.trade_tick()
+
+    benchmark.pedantic(
+        target=TradeTick.from_pyo3,
+        args=(pyo3_trade,),
+        rounds=100_000,
+        iterations=1,
+    )
+
+
+def test_pyo3_trades_to_legacy_cython_list(benchmark):
+    pyo3_trades = [TestDataProviderPyo3.trade_tick()] * 10_000
+
+    benchmark.pedantic(
+        target=TradeTick.from_pyo3_list,
+        args=(pyo3_trades,),
+        rounds=10,
+        iterations=1,
+    )
+
+
+def test_pyo3_bar_to_legacy_cython(benchmark):
+    pyo3_bar = TestDataProviderPyo3.bar_5decimal()
+
+    benchmark.pedantic(
+        target=Bar.from_pyo3,
+        args=(pyo3_bar,),
+        rounds=100_000,
+        iterations=1,
+    )
+
+
+def test_pyo3_bars_to_legacy_cython_list(benchmark):
+    pyo3_bars = [TestDataProviderPyo3.bar_5decimal()] * 10_000
+
+    benchmark.pedantic(
+        target=Bar.from_pyo3_list,
+        args=(pyo3_bars,),
+        rounds=10,
+        iterations=1,
+    )
