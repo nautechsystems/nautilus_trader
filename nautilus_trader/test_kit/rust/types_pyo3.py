c9705cf3784b756294f3cf41ef61d9854a03ade4
Chris Sellers <chris@cjdsellers.io>
2024-01-28 23:09:17 +1100

    Cleanup pyo3 testkit and type stubs

--- nautilus_trader/test_kit/rust/types_pyo3.py
+++ nautilus_trader/test_kit/rust/types_pyo3.py
@@ -12,8 +12,10 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
+
 from nautilus_trader.core.nautilus_pyo3 import AccountBalance
 from nautilus_trader.core.nautilus_pyo3 import Currency
+from nautilus_trader.core.nautilus_pyo3 import InstrumentId
 from nautilus_trader.core.nautilus_pyo3 import MarginBalance
 from nautilus_trader.core.nautilus_pyo3 import Money
 from nautilus_trader.test_kit.rust.identifiers_pyo3 import TestIdProviderPyo3
@@ -46,16 +48,16 @@ class TestTypesProviderPyo3:
 
     @staticmethod
     def account_balance(
-        total=Money.from_str("1525000 USD"),
-        locked=Money.from_str("25000 USD"),
-        free=Money.from_str("1500000 USD"),
+        total: Money = Money.from_str("1525000 USD"),
+        locked: Money = Money.from_str("25000 USD"),
+        free: Money = Money.from_str("1500000 USD"),
     ) -> AccountBalance:
         return AccountBalance(total, locked, free)
 
     @staticmethod
     def margin_balance(
-        initial=Money(1, Currency.from_str("USD")),
-        maintenance=Money(1, Currency.from_str("USD")),
-        instrument=TestIdProviderPyo3.audusd_id(),
+        initial: Money = Money(1, Currency.from_str("USD")),
+        maintenance: Money = Money(1, Currency.from_str("USD")),
+        instrument_id: InstrumentId = TestIdProviderPyo3.audusd_id(),
     ) -> MarginBalance:
-        return MarginBalance(initial, maintenance, instrument)
+        return MarginBalance(initial, maintenance, instrument_id)
