01c90d19c87fe168b6e8d5000fef4461ff89860f
Chris Sellers <chris@cjdsellers.io>
2024-01-28 12:30:03 +1100

    Refine DatabentoLiveClient

--- nautilus_core/adapters/src/databento/python/live.rs
+++ nautilus_core/adapters/src/databento/python/live.rs
@@ -21,7 +21,7 @@ use anyhow::Result;
 use databento::live::Subscription;
 use dbn::{PitSymbolMap, RType, Record, SymbolIndex};
 use indexmap::IndexMap;
-use log::error;
+use log::{error, info};
 use nautilus_core::python::to_pyruntime_err;
 use nautilus_core::{
     python::to_pyvalue_err,
@@ -164,8 +164,8 @@ impl DatabentoLiveClient {
                         continue;
                     }
                     RType::System => {
-                        eprintln!("{:?}", record); // TODO: Just print stderr for now
-                        error!("{:?}", record);
+                        println!("{:?}", record); // TODO: Just print stderr for now
+                        info!("{:?}", record);
                         continue;
                     }
                     RType::InstrumentDef => {
@@ -193,39 +193,40 @@ impl DatabentoLiveClient {
                         }
                         continue;
                     }
-                    _ => {} // Fall through
+                    _ => {
+                        let raw_symbol = symbol_map
+                            .get_for_rec(&record)
+                            .expect("Cannot resolve raw_symbol from `symbol_map`");
+
+                        let symbol = Symbol::from_str_unchecked(raw_symbol);
+                        let publisher_id = record.publisher().unwrap() as PublisherId;
+                        let venue_str = publishers.get(&publisher_id).unwrap().venue.as_str();
+                        let venue = Venue::from_str_unchecked(venue_str);
+
+                        let instrument_id = InstrumentId::new(symbol, venue);
+                        let ts_init = clock.get_time_ns();
+
+                        let (data, _) =
+                            parse_record(&record, rtype, instrument_id, 2, Some(ts_init))
+                                .map_err(to_pyvalue_err)?;
+
+                        // TODO: Improve the efficiency of this constant GIL aquisition
+                        Python::with_gil(|py| {
+                            let py_obj = match data {
+                                Data::Delta(delta) => delta.into_py(py),
+                                Data::Depth10(depth) => depth.into_py(py),
+                                Data::Quote(quote) => quote.into_py(py),
+                                Data::Trade(trade) => trade.into_py(py),
+                                _ => panic!("Invalid data element, was {:?}", data),
+                            };
+
+                            match callback.call1(py, (py_obj,)) {
+                                Ok(_) => {}
+                                Err(e) => eprintln!("Error on callback, {:?}", e), // Just print error for now
+                            };
+                        })
+                    }
                 }
-
-                let raw_symbol = symbol_map
-                    .get_for_rec(&record)
-                    .expect("Cannot resolve raw_symbol from `symbol_map`");
-
-                let symbol = Symbol::from_str_unchecked(raw_symbol);
-                let publisher_id = record.publisher().unwrap() as PublisherId;
-                let venue_str = publishers.get(&publisher_id).unwrap().venue.as_str();
-                let venue = Venue::from_str_unchecked(venue_str);
-
-                let instrument_id = InstrumentId::new(symbol, venue);
-                let ts_init = clock.get_time_ns();
-
-                let (data, _) = parse_record(&record, rtype, instrument_id, 2, Some(ts_init))
-                    .map_err(to_pyvalue_err)?;
-
-                // TODO: Improve the efficiency of this constant GIL aquisition
-                Python::with_gil(|py| {
-                    let py_obj = match data {
-                        Data::Delta(delta) => delta.into_py(py),
-                        Data::Depth10(depth) => depth.into_py(py),
-                        Data::Quote(quote) => quote.into_py(py),
-                        Data::Trade(trade) => trade.into_py(py),
-                        _ => panic!("Invalid data element, was {:?}", data),
-                    };
-
-                    match callback.call1(py, (py_obj,)) {
-                        Ok(_) => {}
-                        Err(e) => eprintln!("Error on callback, {:?}", e), // Just print error for now
-                    };
-                })
             }
             Ok(())
         })
