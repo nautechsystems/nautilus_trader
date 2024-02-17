7df18edb4dd5ed1150d1c1160421391648b132f7
Bshara Zahran <bshara23@gmail.com>
2024-02-18 09:11:15 +1100

    Remove sleeps from interactive_brokers tests

--- tests/integration_tests/adapters/interactive_brokers/client/test_client_connection.py
+++ tests/integration_tests/adapters/interactive_brokers/client/test_client_connection.py
@@ -1,4 +1,18 @@
-import asyncio
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2024 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
 from unittest.mock import AsyncMock
 from unittest.mock import Mock
 from unittest.mock import patch
@@ -44,7 +58,6 @@ async def test_connect_socket(ib_client):
 
         # Act
         await ib_client._connect_socket()
-        asyncio.sleep(0.1)
 
         # Assert
         mock_connection_instance.connect.assert_called_once()
