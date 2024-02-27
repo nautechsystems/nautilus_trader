adfb20689fbf7f7b5793c2538cb33080f53afd4c
Bshara Zahran <bshara23@gmail.com>
2024-02-27 20:50:31 +1100

    WIP fixing tests

--- tests/integration_tests/adapters/betfair/test_betfair_data.py
+++ tests/integration_tests/adapters/betfair/test_betfair_data.py
@@ -214,7 +214,7 @@ def test_market_bsp(data_client, mock_data_engine_process):
     provider = data_client.instrument_provider
     for mc in stream_decode(update[0]).mc:
         market_def = msgspec.structs.replace(mc.market_definition, market_id=mc.id)
-        instruments = make_instruments(market=market_def, currency="GBP")
+        instruments = make_instruments(market=market_def, currency="GBP", ts_event=0, ts_init=0)
         provider.add_bulk(instruments)
 
     # Act
