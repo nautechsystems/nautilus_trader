5096043959b4bc9a63f97682fe916de226ffd8fe
Bshara Zahran <bshara23@gmail.com>
2024-03-18 07:49:20 +1100

    Minor refinements

--- nautilus_trader/adapters/interactive_brokers/client/wrapper.py
+++ nautilus_trader/adapters/interactive_brokers/client/wrapper.py
@@ -1,3 +1,18 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2021 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
 from decimal import Decimal
 from typing import TYPE_CHECKING
 
@@ -44,7 +59,7 @@ class InteractiveBrokersEWrapper(EWrapper):
         self,
         nautilus_logger: Logger,
         client: "InteractiveBrokersClient",
-    ):
+    ) -> None:
         super().__init__()
         self._log = nautilus_logger
         self._client = client
@@ -60,7 +75,13 @@ class InteractiveBrokersEWrapper(EWrapper):
             prms = fnParams
         self._log.debug(f"Msg handled: function={fnName} data={prms}")
 
-    def error(self, reqId: TickerId, errorCode: int, errorString: str, advancedOrderRejectJson=""):
+    def error(
+        self,
+        reqId: TickerId,
+        errorCode: int,
+        errorString: str,
+        advancedOrderRejectJson="",
+    ):
         """
         Call this event in response to an error in communication or when TWS needs to
         send a message to the client.
