8ebce9cce8bc3d1a6fecf7c03bf5083a984121b2
Filip Macek <filip_macek@hotmail.com>
2024-03-24 10:17:25 +0100

    create function convert_pyobject_to_instrument_type

--- nautilus_core/model/src/python/instruments/mod.rs
+++ nautilus_core/model/src/python/instruments/mod.rs
@@ -39,9 +39,40 @@ pub fn convert_instrument_to_pyobject(
     }
 }
 
-
+pub fn convert_pyobject_to_instrument_type(
+    py: Python,
+    instrument: PyObject,
+) -> PyResult<InstrumentType> {
+    let instrument_type = instrument.getattr(py, "instrument_type")?.extract::<String>(py)?;
+    if instrument_type == "CryptoFuture" {
+        let crypto_future = instrument.extract::<CryptoFuture>(py)?;
+        Ok(InstrumentType::CryptoFuture(crypto_future))
+    } else if instrument_type == "CryptoPerpetual" {
+        let crypto_perpetual = instrument.extract::<CryptoPerpetual>(py)?;
+        Ok(InstrumentType::CryptoPerpetual(crypto_perpetual))
+    } else if instrument_type == "CurrencyPair" {
+        let currency_pair = instrument.extract::<CurrencyPair>(py)?;
+        Ok(InstrumentType::CurrencyPair(currency_pair))
+    } else if instrument_type == "Equity" {
+        let equity = instrument.extract::<Equity>(py)?;
+        Ok(InstrumentType::Equity(equity))
+    } else if instrument_type == "FuturesContract" {
+        let futures_contract = instrument.extract::<FuturesContract>(py)?;
+        Ok(InstrumentType::FuturesContract(futures_contract))
+    } else if instrument_type == "FuturesSpread" {
+        let futures_spread = instrument.extract::<FuturesSpread>(py)?;
+        Ok(InstrumentType::FuturesSpread(futures_spread))
+    } else if instrument_type == "OptionsContract" {
+        let options_contract = instrument.extract::<OptionsContract>(py)?;
+        Ok(InstrumentType::OptionsContract(options_contract))
+    } else if instrument_type == "OptionsSpread" {
+        let options_spread = instrument.extract::<CryptoFuture>(py)?;
+        Ok(InstrumentType::CryptoFuture(options_spread))
+    } else{
+        Err(to_pyvalue_err("Error in conversion from pyobject to instrument type"))
+    }
+}
     
-
 
 pub mod crypto_future;
 pub mod crypto_perpetual;
