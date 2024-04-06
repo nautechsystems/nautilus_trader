26bfcbc2bd3d4ae68e03b47b099d8b633fc78fb2
Chris Sellers <chris@cjdsellers.io>
2024-04-06 14:47:52 +1100

    Implement Bybit trade history

--- nautilus_trader/adapters/bybit/endpoints/trade/trade_history.py
+++ nautilus_trader/adapters/bybit/endpoints/trade/trade_history.py
@@ -16,42 +16,47 @@
 import msgspec
 
 from nautilus_trader.adapters.bybit.common.enums import BybitEndpointType
+from nautilus_trader.adapters.bybit.common.enums import BybitExecType
+from nautilus_trader.adapters.bybit.common.enums import BybitProductType
 from nautilus_trader.adapters.bybit.endpoints.endpoint import BybitHttpEndpoint
 from nautilus_trader.adapters.bybit.http.client import BybitHttpClient
-from nautilus_trader.adapters.bybit.schemas.position import BybitPositionResponseStruct
+from nautilus_trader.adapters.bybit.schemas.trade import BybitTradeHistoryResponseStruct
 from nautilus_trader.core.nautilus_pyo3 import HttpMethod
 
 
-class PositionInfoGetParams(msgspec.Struct, omit_defaults=True, frozen=False):
-    category: str | None = None
+class BybitTradeHistoryGetParams(msgspec.Struct, omit_defaults=True, frozen=False):
+    category: BybitProductType
     symbol: str | None = None
     baseCoin: str | None = None
-    settleCoin: str | None = None
+    orderId: str | None = None
+    orderLinkId: str | None = None
+    startTime: int | None = None
+    endtime: int | None = None
+    execType: BybitExecType | None = None
     limit: int | None = None
     cursor: str | None = None
 
 
-class BybitPositionInfoEndpoint(BybitHttpEndpoint):
+class BybitTradeHistoryEndpoint(BybitHttpEndpoint):
     def __init__(
         self,
         client: BybitHttpClient,
         base_endpoint: str,
     ) -> None:
-        url_path = base_endpoint + "/position/list"
+        url_path = base_endpoint + "/execution/list"
         super().__init__(
             client=client,
-            endpoint_type=BybitEndpointType.ACCOUNT,
+            endpoint_type=BybitEndpointType.TRADE,
             url_path=url_path,
         )
-        self._get_resp_decoder = msgspec.json.Decoder(BybitPositionResponseStruct)
+        self._get_resp_decoder = msgspec.json.Decoder(BybitTradeHistoryResponseStruct)
 
-    async def get(self, params: PositionInfoGetParams) -> BybitPositionResponseStruct:
+    async def get(self, params: BybitTradeHistoryGetParams) -> BybitTradeHistoryResponseStruct:
         method_type = HttpMethod.GET
         raw = await self._method(method_type, params)
         try:
             return self._get_resp_decoder.decode(raw)
         except Exception as e:
-            decoded_raw = raw.decode("utf-8")
             raise RuntimeError(
-                f"Failed to decode response position info response: {decoded_raw}",
+                f"Failed to decode response from {self.url_path}: {raw.decode()}",
             ) from e
