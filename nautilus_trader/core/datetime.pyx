702a9192fce92f9719ffaea26e812a8346f9b9b8
rsmb7z <105105941+rsmb7z@users.noreply.github.com>
2024-04-13 11:19:12 +0300

    Ensure as_utc_index precision

--- nautilus_trader/core/datetime.pyx
+++ nautilus_trader/core/datetime.pyx
@@ -38,6 +38,8 @@ from libc.stdint cimport uint64_t
 
 from nautilus_trader.core.correctness cimport Condition
 
+from pandas.api.types import is_datetime64_ns_dtype
+
 
 # UNIX epoch is the UTC time at 00:00:00 on 1/1/1970
 # https://en.wikipedia.org/wiki/Unix_time
@@ -246,12 +248,17 @@ cpdef object as_utc_index(data: pd.DataFrame):
     if data.empty:
         return data
 
+    # Ensure the index is localized to UTC
     if data.index.tzinfo is None:  # tz-naive
-        return data.tz_localize(pytz.utc)
+        data = data.tz_localize(pytz.utc)
     elif data.index.tzinfo != pytz.utc:
-        return data.tz_convert(None).tz_localize(pytz.utc)
-    else:
-        return data  # Already UTC
+        data = data.tz_convert(None).tz_localize(pytz.utc)
+
+    # Check if the index is in nanosecond resolution, convert if not
+    if not is_datetime64_ns_dtype(data.index.dtype):
+        data.index = data.index.astype('datetime64[ns, UTC]')
+
+    return data
 
 
 cpdef str format_iso8601(datetime dt):
