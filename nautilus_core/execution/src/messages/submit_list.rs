51672dd5859e9e2ee5d93fa799cc7f7b4c7388b2
Bshara Zahran <bshara23@gmail.com>
2024-04-14 17:21:16 +1000

    Scaffold OrderList in Rust

--- nautilus_core/execution/src/messages/submit_list.rs
+++ nautilus_core/execution/src/messages/submit_list.rs
@@ -12,3 +12,81 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 // -------------------------------------------------------------------------------------------------
+
+use std::fmt::Display;
+
+use nautilus_core::{time::UnixNanos, uuid::UUID4};
+use nautilus_model::{
+    identifiers::{
+        client_id::ClientId, client_order_id::ClientOrderId, exec_algorithm_id::ExecAlgorithmId,
+        instrument_id::InstrumentId, position_id::PositionId, strategy_id::StrategyId,
+        trader_id::TraderId, venue_order_id::VenueOrderId,
+    },
+    orders::list::OrderList,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub struct SubmitOrderList {
+    pub trader_id: TraderId,
+    pub client_id: ClientId,
+    pub strategy_id: StrategyId,
+    pub instrument_id: InstrumentId,
+    pub client_order_id: ClientOrderId,
+    pub venue_order_id: VenueOrderId,
+    pub order_list: OrderList,
+    pub exec_algorith_id: Option<ExecAlgorithmId>,
+    pub position_id: Option<PositionId>,
+    pub command_id: UUID4,
+    pub ts_init: UnixNanos,
+}
+
+impl SubmitOrderList {
+    #[allow(clippy::too_many_arguments)]
+    pub fn new(
+        trader_id: TraderId,
+        client_id: ClientId,
+        strategy_id: StrategyId,
+        instrument_id: InstrumentId,
+        client_order_id: ClientOrderId,
+        venue_order_id: VenueOrderId,
+        order_list: OrderList,
+        exec_algorith_id: Option<ExecAlgorithmId>,
+        position_id: Option<PositionId>,
+        command_id: UUID4,
+        ts_init: UnixNanos,
+    ) -> anyhow::Result<Self> {
+        Ok(Self {
+            trader_id,
+            client_id,
+            strategy_id,
+            instrument_id,
+            client_order_id,
+            venue_order_id,
+            order_list,
+            exec_algorith_id,
+            position_id,
+            command_id,
+            ts_init,
+        })
+    }
+}
+
+impl Display for SubmitOrderList {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "SubmitOrderList(instrument_id={}, order_list=TBD, position_id={})",
+            self.instrument_id,
+            self.position_id
+                .map_or("None".to_string(), |position_id| format!("{position_id}")),
+        )
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Tests
+////////////////////////////////////////////////////////////////////////////////
+#[cfg(test)]
+mod tests {}
---
+++ nautilus_core/model/src/orders/list.rs
@@ -0,0 +1,41 @@
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2024 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
+
+use nautilus_core::time::UnixNanos;
+use serde::{Deserialize, Serialize};
+
+use super::base::OrderAny;
+use crate::identifiers::{
+    instrument_id::InstrumentId, order_list_id::OrderListId, strategy_id::StrategyId,
+};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[cfg_attr(
+    feature = "python",
+    pyo3::pyclass(module = "nautilus_trader.core.nautilus_pyo3.model")
+)]
+pub struct OrderList {
+    pub id: OrderListId,
+    pub instrument_id: InstrumentId,
+    pub strategy_id: StrategyId,
+    pub orders: Vec<OrderAny>,
+    pub ts_init: UnixNanos,
+}
+
+impl PartialEq for OrderList {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
