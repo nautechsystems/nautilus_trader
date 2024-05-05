6dee30f5161049cd5befceaad7c61c08be16a2ac
Bshara Zahran <bshara23@gmail.com>
2024-05-05 16:33:12 +1000

    Fix typo

--- nautilus_core/model/src/stubs.rs
+++ nautilus_core/model/src/stubs.rs
@@ -14,7 +14,6 @@
 // -------------------------------------------------------------------------------------------------
 
 //! Type stubs to facilitate testing.
-
 use rstest::fixture;
 use rust_decimal::prelude::ToPrimitive;
 
@@ -53,7 +52,7 @@ pub fn calculate_commission<T: Instrument>(
     } else if liquidity_side == LiquiditySide::Taker {
         notional * instrument.taker_fee().to_f64().unwrap()
     } else {
-        panic!("Invalid liquid side {liquidity_side}")
+        panic!("Invalid liquidity side {liquidity_side}")
     };
     if instrument.is_inverse() && !use_quote_for_inverse.unwrap_or(false) {
         Ok(Money::new(commission, instrument.base_currency().unwrap()).unwrap())
