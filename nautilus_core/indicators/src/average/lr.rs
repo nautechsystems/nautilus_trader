6af3582ba366c9579327c2c9d9c1157e255d374d
pushkarm029 <pushkarmishra029@gmail.com>
2024-06-27 12:56:10 +0530

    init
    Signed-off-by: pushkarm029 <pushkarmishra029@gmail.com>

---
+++ nautilus_core/indicators/src/average/lr.rs
@@ -0,0 +1,224 @@
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2024 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//   You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
+
+use std::fmt::{Debug, Display};
+
+use nautilus_model::data::bar::Bar;
+
+use crate::indicator::Indicator;
+
+#[repr(C)]
+#[derive(Debug)]
+#[cfg_attr(
+    feature = "python",
+    pyo3::pyclass(module = "nautilus_trader.core.nautilus_pyo3.indicators")
+)]
+
+pub struct LinearRegression {
+    pub period: usize,
+    pub slope: f64,
+    pub intercept: f64,
+    pub degree: f64,
+    pub cfo: f64,
+    pub r2: f64,
+    pub value: f64,
+    pub initialized: bool,
+    has_inputs: bool,
+    inputs: Vec<f64>,
+}
+
+impl Display for LinearRegression {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}({})", self.name(), self.period,)
+    }
+}
+
+impl Indicator for LinearRegression {
+    fn name(&self) -> String {
+        stringify!(LinearRegression).to_string()
+    }
+
+    fn has_inputs(&self) -> bool {
+        self.has_inputs
+    }
+
+    fn initialized(&self) -> bool {
+        self.initialized
+    }
+
+    fn handle_bar(&mut self, bar: &Bar) {
+        self.update_raw((&bar.close).into());
+    }
+
+    fn reset(&mut self) {
+        self.slope = 0.0;
+        self.intercept = 0.0;
+        self.degree = 0.0;
+        self.cfo = 0.0;
+        self.r2 = 0.0;
+        self.inputs.clear();
+        self.value = 0.0;
+        self.has_inputs = false;
+        self.initialized = false;
+    }
+}
+
+impl LinearRegression {
+    /// Creates a new [`LinearRegression`] instance.
+    pub fn new(period: usize) -> anyhow::Result<Self> {
+        Ok(Self {
+            period,
+            slope: 0.0,
+            intercept: 0.0,
+            degree: 0.0,
+            cfo: 0.0,
+            r2: 0.0,
+            value: 0.0,
+            inputs: Vec::with_capacity(period),
+            has_inputs: false,
+            initialized: false,
+        })
+    }
+
+    pub fn update_raw(&mut self, close: f64) {
+        self.inputs.push(close);
+
+        if !self.initialized {
+            self.has_inputs = true;
+            if self.inputs.len() >= self.period {
+                self.initialized = true;
+            } else {
+                return;
+            }
+        }
+
+        // let x_arr
+        let x_arr: Vec<f64> = (1..=self.period).map(|x| x as f64).collect();
+        let y_arr: Vec<f64> = self.inputs.clone();
+        let x_sum: f64 = 0.5 * self.period as f64 * (self.period as f64 + 1.0);
+        let x2_sum: f64 = x_sum * (2.0 * self.period as f64 + 1.0) / 3.0;
+        let divisor: f64 = self.period as f64 * x2_sum - x_sum * x_sum;
+        let y_sum: f64 = y_arr.iter().sum::<f64>();
+        let xy_sum: f64 = x_arr
+            .iter()
+            .zip(y_arr.iter())
+            .map(|(x, y)| x * y)
+            .sum::<f64>();
+
+        self.slope = (self.period as f64 * xy_sum - x_sum * y_sum) / divisor;
+        self.intercept = (y_sum * x2_sum - x_sum * xy_sum) / divisor;
+
+        let residuals: Vec<f64> = x_arr
+            .into_iter()
+            .zip(y_arr.clone())
+            .map(|(x, y)| self.slope * x + self.intercept - y)
+            .collect();
+
+        self.value = residuals.last().unwrap() + y_arr.last().unwrap();
+        self.degree = 180.0 / std::f64::consts::PI * self.slope.atan();
+        self.cfo = 100.0 * residuals.last().unwrap() / y_arr.last().unwrap();
+        let mean: f64 = y_arr.clone().iter().sum::<f64>() / y_arr.len() as f64;
+        self.r2 = 1.0
+            - residuals.iter().map(|r| r * r).sum::<f64>()
+                / y_arr.iter().map(|y| (y - mean) * (y - mean)).sum::<f64>();
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Tests
+////////////////////////////////////////////////////////////////////////////////
+#[cfg(test)]
+mod tests {
+    use nautilus_model::data::bar::Bar;
+    use rstest::rstest;
+
+    use crate::{
+        average::lr::LinearRegression,
+        indicator::Indicator,
+        stubs::{bar_ethusdt_binance_minute_bid, indicator_lr_10},
+    };
+
+    #[rstest]
+    fn test_psl_initialized(indicator_lr_10: LinearRegression) {
+        let display_str = format!("{indicator_lr_10}");
+        assert_eq!(display_str, "LinearRegression(10)");
+        assert_eq!(indicator_lr_10.period, 10);
+        assert!(!indicator_lr_10.initialized);
+        assert!(!indicator_lr_10.has_inputs);
+    }
+
+    #[rstest]
+    fn test_value_with_one_input(mut indicator_lr_10: LinearRegression) {
+        indicator_lr_10.update_raw(1.0);
+        assert_eq!(indicator_lr_10.value, 0.0);
+    }
+
+    #[rstest]
+    fn test_value_with_three_inputs(mut indicator_lr_10: LinearRegression) {
+        indicator_lr_10.update_raw(1.0);
+        indicator_lr_10.update_raw(2.0);
+        indicator_lr_10.update_raw(3.0);
+        assert_eq!(indicator_lr_10.value, 0.0);
+    }
+
+    #[rstest]
+    fn test_value_with_ten_inputs(mut indicator_lr_10: LinearRegression) {
+        indicator_lr_10.update_raw(1.00000);
+        indicator_lr_10.update_raw(1.00010);
+        indicator_lr_10.update_raw(1.00030);
+        indicator_lr_10.update_raw(1.00040);
+        indicator_lr_10.update_raw(1.00050);
+        indicator_lr_10.update_raw(1.00060);
+        indicator_lr_10.update_raw(1.00050);
+        indicator_lr_10.update_raw(1.00040);
+        indicator_lr_10.update_raw(1.00030);
+        indicator_lr_10.update_raw(1.00010);
+        indicator_lr_10.update_raw(1.00000);
+        assert_eq!(indicator_lr_10.value, 0.8003072727272715);
+    }
+
+    #[rstest]
+    fn test_initialized_with_required_input(mut indicator_lr_10: LinearRegression) {
+        for i in 1..10 {
+            indicator_lr_10.update_raw(f64::from(i));
+        }
+        assert!(!indicator_lr_10.initialized);
+        indicator_lr_10.update_raw(10.0);
+        assert!(indicator_lr_10.initialized);
+    }
+
+    #[rstest]
+    fn test_handle_bar(mut indicator_lr_10: LinearRegression, bar_ethusdt_binance_minute_bid: Bar) {
+        indicator_lr_10.handle_bar(&bar_ethusdt_binance_minute_bid);
+        assert_eq!(indicator_lr_10.value, 0.0);
+        assert!(indicator_lr_10.has_inputs);
+        assert!(!indicator_lr_10.initialized);
+    }
+
+    #[rstest]
+    fn test_reset(mut indicator_lr_10: LinearRegression) {
+        indicator_lr_10.update_raw(1.0);
+        indicator_lr_10.reset();
+        assert_eq!(indicator_lr_10.value, 0.0);
+        assert_eq!(indicator_lr_10.inputs.len(), 0);
+        assert_eq!(indicator_lr_10.slope, 0.0);
+        assert_eq!(indicator_lr_10.intercept, 0.0);
+        assert_eq!(indicator_lr_10.degree, 0.0);
+        assert_eq!(indicator_lr_10.cfo, 0.0);
+        assert_eq!(indicator_lr_10.r2, 0.0);
+        assert!(!indicator_lr_10.has_inputs);
+        assert!(!indicator_lr_10.initialized);
+    }
+}
---
+++ nautilus_core/indicators/src/python/average/lr.rs
@@ -0,0 +1,110 @@
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2024 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
+
+use nautilus_core::python::to_pyvalue_err;
+use nautilus_model::data::{bar::Bar, quote::QuoteTick, trade::TradeTick};
+use pyo3::{exceptions::PyPermissionError, prelude::*};
+
+use crate::{
+    average::{lr::LinearRegression, MovingAverageType},
+    indicator::Indicator,
+};
+
+#[pymethods]
+impl LinearRegression {
+    #[new]
+    pub fn py_new(period: usize) -> PyResult<Self> {
+        Self::new(period).map_err(to_pyvalue_err)
+    }
+
+    fn __repr__(&self) -> String {
+        format!("LinearRegression({})", self.period)
+    }
+
+    #[getter]
+    #[pyo3(name = "name")]
+    fn py_name(&self) -> String {
+        self.name()
+    }
+
+    #[getter]
+    #[pyo3(name = "period")]
+    fn py_period(&self) -> usize {
+        self.period
+    }
+
+    #[getter]
+    #[pyo3(name = "slope")]
+    fn py_slope(&self) -> f64 {
+        self.slope
+    }
+
+    #[getter]
+    #[pyo3(name = "intercept")]
+    fn py_intercept(&self) -> f64 {
+        self.intercept
+    }
+
+    #[getter]
+    #[pyo3(name = "degree")]
+    fn py_degree(&self) -> f64 {
+        self.degree
+    }
+
+    #[getter]
+    #[pyo3(name = "cfo")]
+    fn py_cfo(&self) -> f64 {
+        self.cfo
+    }
+
+    #[getter]
+    #[pyo3(name = "r2")]
+    fn py_r2(&self) -> f64 {
+        self.r2
+    }
+
+    #[getter]
+    #[pyo3(name = "has_inputs")]
+    fn py_has_inputs(&self) -> bool {
+        self.has_inputs()
+    }
+
+    #[getter]
+    #[pyo3(name = "value")]
+    fn py_value(&self) -> f64 {
+        self.value
+    }
+
+    #[getter]
+    #[pyo3(name = "initialized")]
+    fn py_initialized(&self) -> bool {
+        self.initialized
+    }
+
+    #[pyo3(name = "update_raw")]
+    fn py_update_raw(&mut self, close: f64) {
+        self.update_raw(close);
+    }
+
+    #[pyo3(name = "handle_bar")]
+    fn py_handle_bar(&mut self, bar: &Bar) {
+        self.handle_bar(bar);
+    }
+
+    #[pyo3(name = "reset")]
+    fn py_reset(&mut self) {
+        self.reset();
+    }
+}
