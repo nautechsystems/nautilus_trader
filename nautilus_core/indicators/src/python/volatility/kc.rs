6f57face92cd5f8b188cd727e88fb19eb24563d5
Bshara Zahran <bshara23@gmail.com>
2024-06-28 13:24:18 +0530

    Port KeltnerChannel indicator to Rust (#1746)

---
+++ nautilus_core/indicators/src/python/volatility/kc.rs
@@ -0,0 +1,123 @@
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2024 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
+
+use nautilus_core::python::to_pyvalue_err;
+use nautilus_model::data::{bar::Bar, quote::QuoteTick, trade::TradeTick};
+use pyo3::{exceptions::PyPermissionError, prelude::*};
+
+use super::atr;
+use crate::{average::MovingAverageType, indicator::Indicator, volatility::kc::KeltnerChannel};
+
+#[pymethods]
+impl KeltnerChannel {
+    #[new]
+    pub fn py_new(
+        period: usize,
+        k_multiplier: f64,
+        ma_type: Option<MovingAverageType>,
+        ma_type_atr: Option<MovingAverageType>,
+        use_previous: Option<bool>,
+        atr_floor: Option<f64>,
+    ) -> PyResult<Self> {
+        Self::new(
+            period,
+            k_multiplier,
+            ma_type,
+            ma_type_atr,
+            use_previous,
+            atr_floor,
+        )
+        .map_err(to_pyvalue_err)
+    }
+
+    fn __repr__(&self) -> String {
+        format!("KeltnerChannel({})", self.period)
+    }
+
+    #[getter]
+    #[pyo3(name = "name")]
+    fn py_name(&self) -> String {
+        self.name()
+    }
+
+    #[getter]
+    #[pyo3(name = "period")]
+    fn py_period(&self) -> usize {
+        self.period
+    }
+
+    #[getter]
+    #[pyo3(name = "k_multiplier")]
+    fn py_k_multiplier(&self) -> f64 {
+        self.k_multiplier
+    }
+
+    #[getter]
+    #[pyo3(name = "use_previous")]
+    fn py_use_previous(&self) -> bool {
+        self.use_previous
+    }
+
+    #[getter]
+    #[pyo3(name = "atr_floor")]
+    fn py_atr_floor(&self) -> f64 {
+        self.atr_floor
+    }
+
+    #[getter]
+    #[pyo3(name = "has_inputs")]
+    fn py_has_inputs(&self) -> bool {
+        self.has_inputs()
+    }
+
+    #[getter]
+    #[pyo3(name = "upper")]
+    fn py_upper(&self) -> f64 {
+        self.upper
+    }
+
+    #[getter]
+    #[pyo3(name = "middle")]
+    fn py_middle(&self) -> f64 {
+        self.middle
+    }
+
+    #[getter]
+    #[pyo3(name = "lower")]
+    fn py_lower(&self) -> f64 {
+        self.lower
+    }
+
+    #[getter]
+    #[pyo3(name = "initialized")]
+    fn py_initialized(&self) -> bool {
+        self.initialized
+    }
+
+    #[pyo3(name = "update_raw")]
+    fn py_update_raw(&mut self, high: f64, low: f64, close: f64) {
+        self.update_raw(high, low, close);
+    }
+
+    #[pyo3(name = "handle_bar")]
+    fn py_handle_bar(&mut self, bar: &Bar) {
+        self.handle_bar(bar);
+    }
+
+    #[pyo3(name = "reset")]
+    fn py_reset(&mut self) {
+        self.reset();
+    }
+}
