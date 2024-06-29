37fac43f24c58def8d12f2cc8f1a93f1c47f30e2
Chris Sellers <chris@cjdsellers.io>
2024-06-29 17:25:08 +1000

    Update Bybit examples

--- examples/live/bybit/bybit_ema_cross_with_trailing_stop.py
+++ examples/live/bybit/bybit_ema_cross_with_trailing_stop.py
@@ -41,6 +41,11 @@ product_type = BybitProductType.LINEAR
 symbol = f"ETHUSDT-{product_type.value.upper()}"
 trade_size = Decimal("0.010")
 
+# INVERSE
+# product_type = BybitProductType.INVERSE
+# symbol = f"XRPUSD-{product_type.value.upper()}"  # Use for inverse
+# trade_size = Decimal("100")  # Use for inverse
+
 # Configure the trading node
 config_node = TradingNodeConfig(
     trader_id=TraderId("TESTER-001"),
@@ -91,7 +96,7 @@ strat_config = EMACrossTrailingStopConfig(
     trailing_atr_multiple=3.0,
     trailing_offset_type="BASIS_POINTS",
     trigger_type="LAST_TRADE",
-    trade_size=Decimal("0.010"),
+    trade_size=trade_size,
 )
 # Instantiate your strategy
 strategy = EMACrossTrailingStop(config=strat_config)
