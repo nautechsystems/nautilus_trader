5997aa00f50a96acd9ab219b2015934a9c728eba
Anurag Roy <89125578+NextThread@users.noreply.github.com>
2024-06-30 00:47:24 +0530

    Update socket.rs

--- nautilus_core/network/src/socket.rs
+++ nautilus_core/network/src/socket.rs
@@ -15,7 +15,13 @@
 
 //! A high-performance raw TCP client implementation with TLS capability.
 
-use std::{sync::Arc, time::Duration};
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    time::Duration,
+};
 
 use nautilus_core::python::to_pyruntime_err;
 use pyo3::prelude::*;
@@ -36,7 +42,6 @@ use tracing::{debug, error};
 type TcpWriter = WriteHalf<MaybeTlsStream<TcpStream>>;
 type SharedTcpWriter = Arc<Mutex<WriteHalf<MaybeTlsStream<TcpStream>>>>;
 type TcpReader = ReadHalf<MaybeTlsStream<TcpStream>>;
-use std::sync::atomic::{AtomicBool, Ordering};
 
 /// Configuration for TCP socket connection.
 #[derive(Debug, Clone)]
@@ -302,7 +307,7 @@ impl Drop for SocketClientInner {
 pub struct SocketClient {
     writer: SharedTcpWriter,
     controller_task: task::JoinHandle<()>,
-    disconnect_mode: AtomicBool,
+    disconnect_mode: Arc<AtomicBool>,
     suffix: Vec<u8>,
 }
 
@@ -316,7 +321,7 @@ impl SocketClient {
         let suffix = config.suffix.clone();
         let inner = SocketClientInner::connect_url(config).await?;
         let writer = inner.writer.clone();
-        let disconnect_mode = AtomicBool::new(false);
+        let disconnect_mode = Arc::new(AtomicBool::new(false));
         let controller_task = Self::spawn_controller_task(
             inner,
             disconnect_mode.clone(),
@@ -343,7 +348,7 @@ impl SocketClient {
     ///
     /// Controller task will periodically check the disconnect mode
     /// and shutdown the client if it is not alive.
-    pub fn disconnect(&self) {
+    pub async fn disconnect(&self) {
         self.disconnect_mode.store(true, Ordering::SeqCst);
     }
 
@@ -360,20 +365,17 @@ impl SocketClient {
 
     fn spawn_controller_task(
         mut inner: SocketClientInner,
-        disconnect_mode: AtomicBool,
+        disconnect_mode: Arc<AtomicBool>,
         post_reconnection: Option<PyObject>,
         post_disconnection: Option<PyObject>,
     ) -> task::JoinHandle<()> {
         task::spawn(async move {
-            let mut disconnect_flag;
+            let disconnect_flag = disconnect_mode.load(Ordering::SeqCst);
             loop {
                 sleep(Duration::from_secs(1)).await;
 
                 // Check if client needs to disconnect
-                let guard = disconnect_mode.lock().await;
-                disconnect_flag = *guard;
-                drop(guard);
-
+                let disconnected = disconnect_mode.load(Ordering::SeqCst);
                 match (disconnect_flag, inner.is_alive()) {
                     (false, false) => match inner.reconnect().await {
                         Ok(()) => {
@@ -456,7 +458,7 @@ impl SocketClient {
     /// - Any auto-reconnect job should be aborted before closing the client
     #[pyo3(name = "disconnect")]
     fn py_disconnect<'py>(slf: PyRef<'_, Self>, py: Python<'py>) -> PyResult<Bound<'py, PyAny>> {
-        let disconnect_mode = &slf.disconnect_mode;
+        let disconnect_mode = slf.disconnect_mode.clone();
         debug!("Setting disconnect mode to true");
 
         pyo3_asyncio_0_21::tokio::future_into_py(py, async move {
