463d05f8f22aec915d72f6ff28348d2f9a9c112c
Bshara Zahran <bshara23@gmail.com>
2024-07-06 09:27:56 +1000

    Refine indicators

--- nautilus_core/indicators/src/python/mod.rs
+++ nautilus_core/indicators/src/python/mod.rs
@@ -66,7 +66,7 @@ pub fn indicators(_: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<crate::volatility::dc::DonchianChannel>()?;
     m.add_class::<crate::volatility::rvi::RelativeVolatilityIndex>()?;
     m.add_class::<crate::volatility::kc::KeltnerChannel>()?;
-    m.add_class::<crate::volatility::fuzzy_candlesticks::FuzzyCandlesticks>()?;
+    m.add_class::<crate::volatility::fuzzy::FuzzyCandlesticks>()?;
     m.add_class::<crate::volatility::kp::KeltnerPosition>()?;
     Ok(())
 }
