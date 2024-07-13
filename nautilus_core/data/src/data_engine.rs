fb09beb116e5b786022075eec80a62d738a573f0
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2024-07-13 13:05:59 +0530

    Data engine implementation

--- nautilus_core/data/src/data_engine.rs
+++ nautilus_core/data/src/data_engine.rs
@@ -1,61 +1,136 @@
-use std::{collections::HashMap, rc::Rc, sync::Arc};
+use std::collections::HashMap;
 
 use nautilus_core::uuid::UUID4;
 use nautilus_model::identifiers::ClientId;
 
-struct DataRequest {
+pub struct DataRequest {
+    actor_id: UUID4,
+    req_id: UUID4,
     client_id: ClientId,
 }
 
-struct DataResponse {
-    correlation_id: UUID4,
+pub struct DataResponse {
+    actor_id: UUID4,
+    req_id: UUID4,
+    client_id: ClientId,
 }
 
-trait DataClient: Sync + Send {
-    fn get_response(&self, req: DataRequest) -> DataResponse;
+pub trait DataClient: Sync + Send {
+    fn handle(&self, req: DataRequest) -> DataResponse;
+    fn id(&self) -> ClientId;
 }
 
-trait Actor {
-    fn handle_response(&self, resp: DataResponse);
+pub trait Actor {
+    fn handle(&self, resp: DataResponse);
     fn id(&self) -> UUID4;
 }
 
-struct DataEngine {
-    data_client_task: tokio::task::JoinHandle<()>,
-    req_tx: std::sync::mpsc::Sender<DataRequest>,
+struct SyncDataEngine;
+struct LiveDataEngine;
+
+pub struct DataEngineConfig {
+    clients: HashMap<ClientId, Box<dyn DataClient>>,
+    actors: HashMap<UUID4, Box<dyn Actor>>,
+    req_tx: tokio::sync::mpsc::UnboundedSender<DataRequest>,
+    req_rx: tokio::sync::mpsc::UnboundedReceiver<DataRequest>,
+    resp_tx: tokio::sync::mpsc::Sender<DataResponse>,
+    resp_rx: tokio::sync::mpsc::Receiver<DataResponse>,
 }
 
-impl DataEngine {
-    fn new() -> Self {
-        let (req_tx, req_rx) = std::sync::mpsc::channel::<DataRequest>();
-        let (resp_tx, resp_rx) = std::sync::mpsc::channel::<DataResponse>();
-        let client_mapping: HashMap<ClientId, Arc<dyn DataClient>> = HashMap::new();
-        let actor_mapping: HashMap<UUID4, Rc<dyn Actor>> = HashMap::new();
+impl DataEngineConfig {
+    pub fn new() -> Self {
+        let (req_tx, req_rx) = tokio::sync::mpsc::unbounded_channel::<DataRequest>();
+        let (resp_tx, resp_rx) = tokio::sync::mpsc::channel::<DataResponse>(10);
+        Self {
+            clients: HashMap::new(),
+            actors: HashMap::new(),
+            req_tx,
+            req_rx,
+            resp_tx,
+            resp_rx,
+        }
+    }
+
+    pub fn add_client(&mut self, client: Box<dyn DataClient>) {
+        self.clients.insert(client.id(), client);
+    }
+
+    pub fn add_actor(&mut self, actor: Box<dyn Actor>) {
+        self.actors.insert(actor.id(), actor);
+    }
+}
+
+impl Default for DataEngineConfig {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl SyncDataEngine {
+    fn start(config: DataEngineConfig) {
+        let DataEngineConfig {
+            clients,
+            actors,
+            req_tx: _,
+            req_rx,
+            resp_tx,
+            mut resp_rx,
+        } = config;
 
         // TODO: Run in a separate thread
-        let data_client_task = tokio::task::spawn(async move {
-            while let Ok(req) = req_rx.recv() {
-                // TODO: Move each client request handling to a separate task
-                if let Some(client) = client_mapping.get(&req.client_id) {
-                    let resp = client.get_response(req);
-                    resp_tx.send(resp).unwrap(); // TODO
-                }
-            }
-        });
+        let request_handler_task = request_handler_task(req_rx, resp_tx, clients);
 
-        while let Ok(resp) = resp_rx.recv() {
-            if let Some(actor) = actor_mapping.get(&resp.correlation_id) {
-                actor.handle_response(resp);
+        // TODO: consider which tokio runtime to use for blocking
+        while let Some(resp) = resp_rx.blocking_recv() {
+            if let Some(actor) = actors.get(&resp.actor_id) {
+                actor.handle(resp);
             }
         }
 
-        Self {
-            data_client_task,
-            req_tx,
-        }
+        request_handler_task.abort();
     }
+}
 
-    pub fn send_request(&self, req: DataRequest) {
-        self.req_tx.send(req).unwrap();
+impl LiveDataEngine {
+    fn start(config: DataEngineConfig) {
+        let DataEngineConfig {
+            clients,
+            actors,
+            req_tx: _,
+            req_rx,
+            resp_tx,
+            mut resp_rx,
+        } = config;
+
+        // TODO: Run in a separate thread
+        let request_handler_task = request_handler_task(req_rx, resp_tx, clients);
+
+        // TODO: consider which tokio runtime to use for blocking
+        while let Some(resp) = resp_rx.blocking_recv() {
+            if let Some(actor) = actors.get(&resp.actor_id) {
+                actor.handle(resp);
+            }
+        }
+
+        request_handler_task.abort();
     }
+}
+
+fn request_handler_task(
+    mut req_rx: tokio::sync::mpsc::UnboundedReceiver<DataRequest>,
+    resp_tx: tokio::sync::mpsc::Sender<DataResponse>,
+    clients: HashMap<ClientId, Box<dyn DataClient>>,
+) -> tokio::task::JoinHandle<()> {
+    tokio::spawn(async move {
+        while let Some(req) = req_rx.recv().await {
+            if let Some(client) = clients.get(&req.client_id) {
+                let resp = client.handle(req);
+                // TODO add logging
+                match resp_tx.send(resp).await {
+                    Ok(_) => todo!(),
+                    Err(_) => todo!(),
+                };
+            }
+        }
+    })
 }
