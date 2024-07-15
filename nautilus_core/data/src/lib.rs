ef681039869d11e05008943e412282ac3bcf0bf7
Bshara Zahran <bshara23@gmail.com>
2024-07-15 19:47:30 +1000

    Add initial aggregation module in Rust

--- nautilus_core/data/src/lib.rs
+++ nautilus_core/data/src/lib.rs
@@ -27,6 +27,7 @@
 //! - `ffi`: Enables the C foreign function interface (FFI) from `cbindgen`
 //! - `python`: Enables Python bindings from `pyo3`
 
+pub mod aggregation;
 pub mod client;
 pub mod engine;
 #[allow(dead_code)]
