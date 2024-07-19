8490559621599635fa4e67c280af46760c49aa10
Bshara Zahran <bshara23@gmail.com>
2024-07-20 09:56:16 +1000

    Add UnixNanos.as_i64 method in Rust

--- nautilus_core/core/src/nanos.rs
+++ nautilus_core/core/src/nanos.rs
@@ -38,6 +38,11 @@ impl UnixNanos {
     }
 
     #[must_use]
+    pub const fn as_i64(&self) -> i64 {
+        self.0 as i64
+    }
+
+    #[must_use]
     pub const fn as_f64(&self) -> f64 {
         self.0 as f64
     }
@@ -191,12 +196,14 @@ mod tests {
     fn test_new() {
         let nanos = UnixNanos::from(123);
         assert_eq!(nanos.as_u64(), 123);
+        assert_eq!(nanos.as_i64(), 123);
     }
 
     #[rstest]
     fn test_default() {
         let nanos = UnixNanos::default();
         assert_eq!(nanos.as_u64(), 0);
+        assert_eq!(nanos.as_i64(), 0);
     }
 
     #[rstest]
