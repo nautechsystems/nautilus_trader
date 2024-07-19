118faba5c15b7723a3d77e337a18b732e8568890
Chris Sellers <chris@cjdsellers.io>
2024-07-20 08:50:55 +1000

    Fix instrument None handling

--- nautilus_trader/adapters/interactive_brokers/execution.py
+++ nautilus_trader/adapters/interactive_brokers/execution.py
@@ -386,6 +386,12 @@ class InteractiveBrokersExecutionClient(LiveExecutionClient):
             instrument = await self.instrument_provider.find_with_contract_id(
                 position.contract.conId,
             )
+            if instrument is None:
+                self._log.error(
+                    f"Cannot generate report: instrument not found for contract ID {position.contract.conId}",
+                )
+                continue
+
             avg_px = instrument.make_price(
                 position.avg_cost / instrument.multiplier,
             ).as_decimal()
