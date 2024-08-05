62764774a7a7f95efe818815407fd2409a5a4a8e
Bshara Zahran <bshara23@gmail.com>
2024-08-05 10:16:05 +0100

    Fix customdataclass and add tests (#1828)

--- tests/unit_tests/model/test_custom_data.py
+++ tests/unit_tests/model/test_custom_data.py
@@ -13,6 +13,7 @@
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
 
+
 from nautilus_trader.core.data import Data
 from nautilus_trader.model.custom import customdataclass
 from nautilus_trader.model.identifiers import InstrumentId
@@ -24,21 +25,21 @@ class GreeksTestData(Data):
     delta: float = 0.0
 
     def __repr__(self):
-        return f"{self(type).__name__}(instrument_id={self.instrument_id}, delta={self.delta:.2f}, ts_event={self.ts_event}, ts_init={self._ts_init})"
+        return f"GreeksTestData(instrument_id={self.instrument_id}, delta={self.delta:.2f}, ts_event={self.ts_event}, ts_init={self._ts_init})"
 
 
 def test_customdata_decorator_properties() -> None:
     # Arrange, Act
-    data = GreeksTestData(ts_event=2, ts_init=1)
+    data = GreeksTestData(ts_event=1, ts_init=2)
 
     # Assert
-    assert data.ts_event == 2
-    assert data.ts_init == 1
+    assert data.ts_event == 1
+    assert data.ts_init == 2
 
 
 def test_customdata_decorator_dict() -> None:
     # Arrange
-    data = GreeksTestData(ts_event=2, ts_init=1)
+    data = GreeksTestData(1, 2)
 
     # Act
     data_dict = data.to_dict()
@@ -47,7 +48,54 @@ def test_customdata_decorator_dict() -> None:
     assert data_dict == {
         "instrument_id": "ES.GLBX",
         "delta": 0.0,
-        "ts_event": 2,
-        "ts_init": 1,
+        "ts_event": 1,
+        "ts_init": 2,
     }
-    # assert GreeksTestData.from_dict(data_dict) == data
+
+
+def test_customdata_decorator_dict_identity() -> None:
+    # Arrange
+    data = GreeksTestData(
+        ts_event=1,
+        ts_init=2,
+        instrument_id=InstrumentId.from_str("CL.GLBX"),
+        delta=1000.0,
+    )
+
+    # Act
+    new_data = GreeksTestData.from_dict(data.to_dict())
+
+    # Assert
+    assert new_data == data
+
+
+def test_customdata_decorator_bytes_identity() -> None:
+    # Arrange
+    data = GreeksTestData(
+        ts_event=1,
+        ts_init=2,
+        instrument_id=InstrumentId.from_str("CL.GLBX"),
+        delta=1000.0,
+    )
+
+    # Act
+    new_data = GreeksTestData.from_bytes(data.to_bytes())
+
+    # Assert
+    assert new_data == data
+
+
+def test_customdata_decorator_arrow_identity() -> None:
+    # Arrange
+    data = GreeksTestData(
+        ts_event=1,
+        ts_init=2,
+        instrument_id=InstrumentId.from_str("CL.GLBX"),
+        delta=1000.0,
+    )
+
+    # Act
+    new_data = GreeksTestData.from_arrow(data.to_arrow())[0]
+
+    # Assert
+    assert new_data == data
