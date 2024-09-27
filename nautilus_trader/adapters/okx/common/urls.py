3e95b8c57a69132c87c5be72dcbd08dc0b2953ac
Bshara Zahran <bshara23@gmail.com>
2024-09-27 18:21:23 -0400

    Simplify OKX configs (#1966)

--- nautilus_trader/adapters/okx/common/urls.py
+++ nautilus_trader/adapters/okx/common/urls.py
@@ -14,49 +14,13 @@
 # -------------------------------------------------------------------------------------------------
 
 from nautilus_trader.adapters.env import get_env_key_or
-from nautilus_trader.adapters.okx.common.enums import OKXWsBaseUrlType
 
 
 def get_http_base_url() -> str:
     return get_env_key_or("OKX_BASE_URL_HTTP", "https://www.okx.com")
 
 
-def get_ws_base_url(ws_base_url_type: OKXWsBaseUrlType, is_demo: bool) -> str:
+def get_ws_base_url(is_demo: bool) -> str:
     if is_demo:
-        match ws_base_url_type:
-            case OKXWsBaseUrlType.PUBLIC:
-                return get_env_key_or(
-                    "OKX_DEMO_BASE_URL_PUBLIC_WS",
-                    "wss://wspap.okx.com:8443/ws/v5/public",
-                )
-            case OKXWsBaseUrlType.PRIVATE:
-                return get_env_key_or(
-                    "OKX_DEMO_BASE_URL_PRIVATE_WS",
-                    "wss://wspap.okx.com:8443/ws/v5/private",
-                )
-            case OKXWsBaseUrlType.BUSINESS:
-                return get_env_key_or(
-                    "OKX_DEMO_BASE_URL_BUSINESS_WS",
-                    "wss://wspap.okx.com:8443/ws/v5/business",
-                )
-            case _:
-                raise ValueError(
-                    f"unknown websocket base url type {ws_base_url_type} - must be one of "
-                    f"{list(OKXWsBaseUrlType)}",
-                )
-
-    match ws_base_url_type:
-        case OKXWsBaseUrlType.PUBLIC:
-            return get_env_key_or("OKX_BASE_URL_PUBLIC_WS", "wss://ws.okx.com:8443/ws/v5/public")
-        case OKXWsBaseUrlType.PRIVATE:
-            return get_env_key_or("OKX_BASE_URL_PRIVATE_WS", "wss://ws.okx.com:8443/ws/v5/private")
-        case OKXWsBaseUrlType.BUSINESS:
-            return get_env_key_or(
-                "OKX_BASE_URL_BUSINESS_WS",
-                "wss://ws.okx.com:8443/ws/v5/business",
-            )
-        case _:
-            raise ValueError(
-                f"unknown websocket base url type {ws_base_url_type} - must be one of "
-                f"{list(OKXWsBaseUrlType)}",
-            )
+        return get_env_key_or("OKX_DEMO_BASE_URL_WS", "wss://wspap.okx.com:8443/ws/v5")
+    return get_env_key_or("OKX_BASE_URL_WS", "wss://ws.okx.com:8443/ws/v5")
--- nautilus_trader/adapters/okx/config.py
+++ nautilus_trader/adapters/okx/config.py
@@ -16,9 +16,6 @@
 from nautilus_trader.adapters.okx.common.enums import OKXContractType
 from nautilus_trader.adapters.okx.common.enums import OKXInstrumentType
 from nautilus_trader.adapters.okx.common.enums import OKXMarginMode
-from nautilus_trader.adapters.okx.common.enums import OKXWsBaseUrlType
-from nautilus_trader.adapters.okx.common.urls import get_http_base_url
-from nautilus_trader.adapters.okx.common.urls import get_ws_base_url
 from nautilus_trader.config import LiveDataClientConfig
 from nautilus_trader.config import LiveExecClientConfig
 
@@ -37,35 +34,19 @@ class OKXDataClientConfig(LiveDataClientConfig, frozen=True):
         The passphrase used when creating the OKX API keys.
         If ``None`` then will source the `OKX_PASSPHRASE` environment variable.
     instrument_types : tuple[OKXInstrumentType], optional
-        The OKX instrument types of instruments to load. The default is `[OKXInstrumentType.SWAP]`.
+        The OKX instrument types of instruments to load. The default is `(OKXInstrumentType.SWAP,)`.
         If None, all instrument types are loaded (subject to contract types and their compatibility
         with instrument types).
     contract_types : tuple[OKXInstrumentType], optional
-        The OKX contract types of instruments to load. The default is `[OKXInstrumentType.LINEAR]`.
+        The OKX contract types of instruments to load. The default is `(OKXInstrumentType.LINEAR,)`.
         If None, all contract types are loaded (subject to instrument types and their compatibility
         with contract types).
     base_url_http : str, optional
         The base url to OKX's http api.
-        If ``None`` then will source the `OKX_BASE_URL_HTTP` environment variable.
-    base_url_public_ws : str, optional
-        The base url to OKX's public websocket api.
-        If ``None`` then will source the url from `get_ws_base_url(OKXWsBaseUrlType.PUBLIC)`.
-    base_url_private_ws : str, optional
-        The base url to OKX's private websocket api.
-        If ``None`` then will source the url from `get_ws_base_url(OKXWsBaseUrlType.PRIVATE)`.
-    base_url_business_ws : str, optional
-        The base url to OKX's business websocket api.
-        If ``None`` then will source the url from `get_ws_base_url(OKXWsBaseUrlType.BUSINESS)`.
-    demo_base_url_public_ws : str, optional
-        The base url to OKX's demo public websocket api.
-        If ``None`` then will source the url from `get_ws_base_url(OKXWsBaseUrlType.PUBLIC, True)`.
-    demo_base_url_private_ws : str, optional
-        The base url to OKX's demo private websocket api.
-        If ``None`` then will source the url from `get_ws_base_url(OKXWsBaseUrlType.PRIVATE, True)`.
-    demo_base_url_business_ws : str, optional
-        The base url to OKX's demo business websocket api.
-        If ``None`` then will source the url from
-        `get_ws_base_url(OKXWsBaseUrlType.BUSINESS, True)`.
+        If ``None`` then will source the `get_http_base_url()`.
+    base_url_ws : str, optional
+        The base url to OKX's websocket api.
+        If ``None`` then will source the url from `get_ws_base_url()`.
     is_demo : bool, default False
         If the client is connecting to the OKX demo API.
 
@@ -76,32 +57,9 @@ class OKXDataClientConfig(LiveDataClientConfig, frozen=True):
     passphrase: str | None = None
     instrument_types: tuple[OKXInstrumentType] | None = (OKXInstrumentType.SWAP,)
     contract_types: tuple[OKXContractType] | None = (OKXContractType.LINEAR,)
-    base_url_http: str | None = get_http_base_url()
-    base_url_public_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.PUBLIC, is_demo=False)
-    base_url_private_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.PRIVATE, is_demo=False)
-    base_url_business_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.BUSINESS, is_demo=False)
-    demo_base_url_public_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.PUBLIC, is_demo=True)
-    demo_base_url_private_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.PRIVATE, is_demo=True)
-    demo_base_url_business_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.BUSINESS, is_demo=True)
+    base_url_http: str | None = None
+    base_url_ws: str | None = None
     is_demo: bool = False
-
-    def get_applicable_ws_base_url(self, ws_base_url_type: OKXWsBaseUrlType) -> str | None:
-        if self.is_demo:
-            match ws_base_url_type:
-                case OKXWsBaseUrlType.PUBLIC:
-                    return self.demo_base_url_public_ws
-                case OKXWsBaseUrlType.PRIVATE:
-                    return self.demo_base_url_private_ws
-                case OKXWsBaseUrlType.BUSINESS:
-                    return self.demo_base_url_business_ws
-
-        match ws_base_url_type:
-            case OKXWsBaseUrlType.PUBLIC:
-                return self.base_url_public_ws
-            case OKXWsBaseUrlType.PRIVATE:
-                return self.base_url_private_ws
-            case OKXWsBaseUrlType.BUSINESS:
-                return self.base_url_business_ws
 
 
 class OKXExecClientConfig(LiveExecClientConfig, frozen=True):
@@ -118,35 +76,19 @@ class OKXExecClientConfig(LiveExecClientConfig, frozen=True):
         The passphrase used when creating the OKX API keys.
         If ``None`` then will source the `OKX_PASSPHRASE` environment variable.
     instrument_types : tuple[OKXInstrumentType], optional
-        The OKX instrument types of instruments to load. The default is `[OKXInstrumentType.SWAP]`.
+        The OKX instrument types of instruments to load. The default is `(OKXInstrumentType.SWAP,)`.
         If None, all instrument types are loaded (subject to contract types and their compatibility
         with instrument types).
     contract_types : tuple[OKXInstrumentType], optional
-        The OKX contract types of instruments to load. The default is `[OKXInstrumentType.LINEAR]`.
+        The OKX contract types of instruments to load. The default is `(OKXInstrumentType.LINEAR,)`.
         If None, all contract types are loaded (subject to instrument types and their compatibility
         with contract types).
     base_url_http : str, optional
         The base url to OKX's http api.
-        If ``None`` then will source the `OKX_BASE_URL_HTTP` environment variable.
-    base_url_public_ws : str, optional
-        The base url to OKX's public websocket api.
-        If ``None`` then will source the url from `get_ws_base_url(OKXWsBaseUrlType.PUBLIC)`.
-    base_url_private_ws : str, optional
-        The base url to OKX's private websocket api.
-        If ``None`` then will source the url from `get_ws_base_url(OKXWsBaseUrlType.PRIVATE)`.
-    base_url_business_ws : str, optional
-        The base url to OKX's business websocket api.
-        If ``None`` then will source the url from `get_ws_base_url(OKXWsBaseUrlType.BUSINESS)`.
-    demo_base_url_public_ws : str, optional
-        The base url to OKX's demo public websocket api.
-        If ``None`` then will source the url from `get_ws_base_url(OKXWsBaseUrlType.PUBLIC, True)`.
-    demo_base_url_private_ws : str, optional
-        The base url to OKX's demo private websocket api.
-        If ``None`` then will source the url from `get_ws_base_url(OKXWsBaseUrlType.PRIVATE, True)`.
-    demo_base_url_business_ws : str, optional
-        The base url to OKX's demo business websocket api.
-        If ``None`` then will source the url from
-        `get_ws_base_url(OKXWsBaseUrlType.BUSINESS, True)`.
+        If ``None`` then will source the `get_http_base_url()`.
+    base_url_ws : str, optional
+        The base url to OKX's websocket api.
+        If ``None`` then will source the url from `get_ws_base_url()`.
     margin_mode : OKXMarginMode, [default=OKXMarginMode.CROSS]
         The intended OKX account margin mode (referred to as mgnMode by OKX's docs).
     is_demo : bool, default False
@@ -159,31 +101,8 @@ class OKXExecClientConfig(LiveExecClientConfig, frozen=True):
     passphrase: str | None = None
     instrument_types: tuple[OKXInstrumentType] | None = (OKXInstrumentType.SWAP,)
     contract_types: tuple[OKXContractType] | None = (OKXContractType.LINEAR,)
-    base_url_http: str | None = get_http_base_url()
-    base_url_public_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.PUBLIC, is_demo=False)
-    base_url_private_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.PRIVATE, is_demo=False)
-    base_url_business_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.BUSINESS, is_demo=False)
-    demo_base_url_public_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.PUBLIC, is_demo=True)
-    demo_base_url_private_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.PRIVATE, is_demo=True)
-    demo_base_url_business_ws: str | None = get_ws_base_url(OKXWsBaseUrlType.BUSINESS, is_demo=True)
+    base_url_http: str | None = None
+    base_url_ws: str | None = None
     margin_mode: OKXMarginMode = OKXMarginMode.CROSS
     is_demo: bool = False
     # use_reduce_only: bool = True  # TODO: check if applicable -> taken from Bybit
-
-    def get_applicable_ws_base_url(self, ws_base_url_type: OKXWsBaseUrlType) -> str | None:
-        if self.is_demo:
-            match ws_base_url_type:
-                case OKXWsBaseUrlType.PUBLIC:
-                    return self.demo_base_url_public_ws
-                case OKXWsBaseUrlType.PRIVATE:
-                    return self.demo_base_url_private_ws
-                case OKXWsBaseUrlType.BUSINESS:
-                    return self.demo_base_url_business_ws
-
-        match ws_base_url_type:
-            case OKXWsBaseUrlType.PUBLIC:
-                return self.base_url_public_ws
-            case OKXWsBaseUrlType.PRIVATE:
-                return self.base_url_private_ws
-            case OKXWsBaseUrlType.BUSINESS:
-                return self.base_url_business_ws
