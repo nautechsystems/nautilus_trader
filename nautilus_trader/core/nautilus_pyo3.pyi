b4aceeb6ffef46031d61930dcebd53f64af3dd2c
Chris Sellers <chris@cjdsellers.io>
2024-10-27 17:41:22 +1100

    Add time constants from Rust

--- nautilus_trader/core/nautilus_pyo3.pyi
+++ nautilus_trader/core/nautilus_pyo3.pyi
@@ -24,6 +24,11 @@ from nautilus_trader.core.data import Data
 NAUTILUS_VERSION: Final[str]
 USER_AGENT: Final[str]
 
+MILLISECONDS_IN_SECOND: Final[int]
+NANOSECONDS_IN_SECOND: Final[int]
+NANOSECONDS_IN_MILLISECOND: Final[int]
+NANOSECONDS_IN_MICROSECOND: Final[int]
+
 class UUID4:
     def __init__(self, value: str) -> None: ...
 
