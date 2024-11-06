da3a27a3412d6e2571250539716879536b6d3e40
Chris Sellers <chris@cjdsellers.io>
2024-11-06 21:15:18 +1100

    Refine Polymarket reconciliation

--- tests/integration_tests/adapters/polymarket/sandbox/sandbox_trades.py
+++ tests/integration_tests/adapters/polymarket/sandbox/sandbox_trades.py
@@ -13,32 +13,22 @@
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
 
-import asyncio
+from pathlib import Path
+
+import msgspec
 
 from nautilus_trader.adapters.polymarket.factories import get_polymarket_http_client
-from nautilus_trader.adapters.polymarket.providers import PolymarketInstrumentProvider
-from nautilus_trader.common.component import LiveClock
 
 
-async def test_polymarket_instrument_provider():
-    clock = LiveClock()
+def save_historical_trades() -> None:
     client = get_polymarket_http_client()
 
-    provider = PolymarketInstrumentProvider(
-        client=client,
-        clock=clock,
-    )
+    response = client.get_trades()
+    print(response)
 
-    filters = {
-        "next_cursor": "MTEyMDA=",
-        "is_active": True,
-    }
-
-    await provider.load_all_async(filters=filters)
-
-    instruments = provider.list_all()
-    await provider.load_async(instruments[0].id)
+    path = Path("trades_history.json")
+    path.write_bytes(msgspec.json.encode(response))
 
 
 if __name__ == "__main__":
-    asyncio.run(test_polymarket_instrument_provider())
+    save_historical_trades()
