09352354d6f94ee06f9cd004b49b720a02268402
Bshara Zahran <bshara23@gmail.com>
2024-11-11 04:27:58 +0800

    Remove Bybit msgspec redundant import alias (#2050)

--- nautilus_trader/adapters/okx/schemas/account/trade_fee.py
+++ nautilus_trader/adapters/okx/schemas/account/trade_fee.py
@@ -13,7 +13,7 @@
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
 
-import msgspec as msgspec
+import msgspec
 
 from nautilus_trader.adapters.okx.common.enums import OKXInstrumentType
 
