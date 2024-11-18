82d139bca47614cb039e8708b095d5411b45ed01
Bshara Zahran <bshara23@gmail.com>
2024-11-18 11:50:53 +1100

    Improve live engines error logging

--- nautilus_trader/live/risk_engine.py
+++ nautilus_trader/live/risk_engine.py
@@ -244,8 +244,8 @@ class LiveRiskEngine(RiskEngine):
                 self._execute_command(command)
         except asyncio.CancelledError:
             self._log.warning("Command message queue canceled")
-        except RuntimeError as e:
-            self._log.error(f"RuntimeError: {e}")
+        except Exception as e:
+            self._log.error(repr(e))
         finally:
             stopped_msg = "Command message queue stopped"
             if not self._cmd_queue.empty():
@@ -265,8 +265,8 @@ class LiveRiskEngine(RiskEngine):
                 self._handle_event(event)
         except asyncio.CancelledError:
             self._log.warning("Event message queue canceled")
-        except RuntimeError as e:
-            self._log.error(f"RuntimeError: {e}")
+        except Exception as e:
+            self._log.error(repr(e))
         finally:
             stopped_msg = "Event message queue stopped"
             if not self._evt_queue.empty():
