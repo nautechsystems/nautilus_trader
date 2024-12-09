257da7158096ecbadcef21a0cee825d4060803b0
David Blom <davidsblom@gmail.com>
2024-12-09 09:11:29 +0100

    Fix order handling for BEST_EFFORT_CANCELED messages of dYdX

--- nautilus_trader/adapters/dydx/common/enums.py
+++ nautilus_trader/adapters/dydx/common/enums.py
@@ -244,7 +244,7 @@ class DYDXEnumParser:
             DYDXOrderStatus.OPEN: OrderStatus.ACCEPTED,
             DYDXOrderStatus.FILLED: OrderStatus.FILLED,
             DYDXOrderStatus.CANCELED: OrderStatus.CANCELED,
-            DYDXOrderStatus.BEST_EFFORT_CANCELED: OrderStatus.CANCELED,
+            DYDXOrderStatus.BEST_EFFORT_CANCELED: OrderStatus.ACCEPTED,
             DYDXOrderStatus.BEST_EFFORT_OPENED: OrderStatus.ACCEPTED,
             DYDXOrderStatus.UNTRIGGERED: OrderStatus.ACCEPTED,
         }
