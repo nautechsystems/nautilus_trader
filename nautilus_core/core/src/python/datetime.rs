f8d68b1109a27cdf29ce334068aa100bde815b29
Chris Sellers <chris@cjdsellers.io>
2024-12-16 07:57:13 +1100

    Add Python docstrings for pyo3 datetime functions

--- nautilus_core/core/src/python/datetime.rs
+++ nautilus_core/core/src/python/datetime.rs
@@ -25,54 +25,162 @@ use crate::{
     nanos::UnixNanos,
 };
 
+/// Return round nanoseconds (ns) converted from the given seconds.
+///
+/// Parameters
+/// ----------
+/// secs : float
+///     The seconds to convert.
+///
+/// Returns
+/// -------
+/// int
 #[must_use]
 #[pyfunction(name = "secs_to_nanos")]
 pub fn py_secs_to_nanos(secs: f64) -> u64 {
     secs_to_nanos(secs)
 }
 
+/// Return round milliseconds (ms) converted from the given seconds.
+///
+/// Parameters
+/// ----------
+/// secs : float
+///     The seconds to convert.
+///
+/// Returns
+/// -------
+/// int
 #[must_use]
 #[pyfunction(name = "secs_to_millis")]
 pub fn py_secs_to_millis(secs: f64) -> u64 {
     secs_to_millis(secs)
 }
 
+/// Return round nanoseconds (ns) converted from the given milliseconds (ms).
+///
+/// Parameters
+/// ----------
+/// millis : float
+///     The milliseconds to convert.
+///
+/// Returns
+/// -------
+/// int
 #[must_use]
 #[pyfunction(name = "millis_to_nanos")]
 pub fn py_millis_to_nanos(millis: f64) -> u64 {
     millis_to_nanos(millis)
 }
 
+/// Return round nanoseconds (ns) converted from the given microseconds (μs).
+///
+/// Parameters
+/// ----------
+/// micros : float
+///     The microseconds to convert.
+///
+/// Returns
+/// -------
+/// int
 #[must_use]
 #[pyfunction(name = "micros_to_nanos")]
 pub fn py_micros_to_nanos(micros: f64) -> u64 {
     micros_to_nanos(micros)
 }
 
+/// Return seconds converted from the given nanoseconds (ns).
+///
+/// Parameters
+/// ----------
+/// nanos : int
+///     The nanoseconds to convert.
+///
+/// Returns
+/// -------
+/// float
 #[must_use]
 #[pyfunction(name = "nanos_to_secs")]
 pub fn py_nanos_to_secs(nanos: u64) -> f64 {
     nanos_to_secs(nanos)
 }
 
+/// Return round milliseconds (ms) converted from the given nanoseconds (ns).
+///
+/// Parameters
+/// ----------
+/// nanos : int
+///     The nanoseconds to convert.
+///
+/// Returns
+/// -------
+/// int
 #[must_use]
 #[pyfunction(name = "nanos_to_millis")]
 pub const fn py_nanos_to_millis(nanos: u64) -> u64 {
     nanos_to_millis(nanos)
 }
 
+/// Return round microseconds (μs) converted from the given nanoseconds (ns).
+///
+/// Parameters
+/// ----------
+/// nanos : int
+///     The nanoseconds to convert.
+///
+/// Returns
+/// -------
+/// int
 #[must_use]
 #[pyfunction(name = "nanos_to_micros")]
 pub const fn py_nanos_to_micros(nanos: u64) -> u64 {
     nanos_to_micros(nanos)
 }
 
+/// Return UNIX nanoseconds at midnight (UTC) of the last weekday (Mon-Fri).
+///
+/// Parameters
+/// ----------
+/// year : int
+///     The year from the datum date.
+/// month : int
+///     The month from the datum date.
+/// day : int
+///     The day from the datum date.
+///
+/// Returns
+/// -------
+/// int
+///
+/// Raises
+/// ------
+/// ValueError
+///     If given an invalid date.
 #[must_use]
 #[pyfunction(name = "unix_nanos_to_iso8601")]
 pub fn py_unix_nanos_to_iso8601(timestamp_ns: u64) -> String {
     unix_nanos_to_iso8601(timestamp_ns.into())
 }
 
+/// Return UNIX nanoseconds at midnight (UTC) of the last weekday (Mon-Fri).
+///
+/// Parameters
+/// ----------
+/// year : int
+///     The year from the datum date.
+/// month : int
+///     The month from the datum date.
+/// day : int
+///     The day from the datum date.
+///
+/// Returns
+/// -------
+/// int
+///
+/// Raises
+/// ------
+/// ValueError
+///     If given an invalid date.
 #[pyfunction(name = "last_weekday_nanos")]
 pub fn py_last_weekday_nanos(year: i32, month: u32, day: u32) -> PyResult<u64> {
     Ok(last_weekday_nanos(year, month, day)
@@ -80,6 +188,21 @@ pub fn py_last_weekday_nanos(year: i32, month: u32, day: u32) -> PyResult<u64> {
         .as_u64())
 }
 
+/// Return whether the given UNIX nanoseconds timestamp is within the last 24 hours.
+///
+/// Parameters
+/// ----------
+/// timestamp_ns : int
+///     The UNIX nanoseconds timestamp datum.
+///
+/// Returns
+/// -------
+/// bool
+///
+/// Raises
+/// ------
+/// ValueError
+///     If `timestamp` is invalid.
 #[pyfunction(name = "is_within_last_24_hours")]
 pub fn py_is_within_last_24_hours(timestamp_ns: u64) -> PyResult<bool> {
     is_within_last_24_hours(UnixNanos::from(timestamp_ns)).map_err(to_pyvalue_err)
