c4873a7c6abe009487acd61b8a2617db062b2501
Chris Sellers <chris@cjdsellers.io>
2024-12-20 17:35:22 +1100

    Fix clippy lints

--- nautilus_core/core/benches/bench_time.rs
+++ nautilus_core/core/benches/bench_time.rs
@@ -19,14 +19,14 @@ use nautilus_core::time::{duration_since_unix_epoch, nanos_since_unix_epoch};
 // Using `SystemTime` under the hood
 fn bench_system_time(c: &mut Criterion) {
     c.bench_function("duration_since_unix_epoch", |b| {
-        b.iter(|| duration_since_unix_epoch());
+        b.iter(duration_since_unix_epoch);
     });
 }
 
 // Using libc `clock_gettime` syscall
 fn bench_rdtscp(c: &mut Criterion) {
     c.bench_function("nanos_since_unix_epoch", |b| {
-        b.iter(|| nanos_since_unix_epoch());
+        b.iter(nanos_since_unix_epoch);
     });
 }
 
