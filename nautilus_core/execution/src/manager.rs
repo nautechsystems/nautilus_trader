80a80e4910e67018239d3507ca3fa0f92d3bea76
Bshara Zahran <bshara23@gmail.com>
2024-12-30 21:25:38 +1100

    Add OrderManager struct in Rust

---
+++ nautilus_core/execution/src/manager.rs
@@ -0,0 +1,34 @@
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2024 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
+
+// Under development
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use std::{cell::RefCell, rc::Rc};
+
+use nautilus_common::{cache::Cache, clock::Clock, msgbus::MessageBus};
+
+use crate::messages::{CancelOrder, ModifyOrder, SubmitOrder};
+
+pub struct OrderManager {
+    clock: Rc<RefCell<dyn Clock>>,
+    cache: Rc<RefCell<Cache>>,
+    msgbus: Rc<RefCell<MessageBus>>,
+    active_local: bool,
+    submit_order_handler: Option<fn(&SubmitOrder)>,
+    cancel_order_handler: Option<fn(&CancelOrder)>,
+    modify_order_handler: Option<fn(&ModifyOrder)>,
+}
