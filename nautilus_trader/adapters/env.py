552517d4bc7c2a3f8d7eeb5d89820166561d99f7
Chris Sellers <chris@cjdsellers.io>
2024-12-30 18:56:14 +1100

    Improve clarity of exception message

--- nautilus_trader/adapters/env.py
+++ nautilus_trader/adapters/env.py
@@ -18,7 +18,7 @@ import os
 
 def get_env_key(key: str) -> str:
     if key not in os.environ:
-        raise RuntimeError(f"Cannot find env var '{key}'")
+        raise RuntimeError(f"Environment variable '{key}' not set")
     else:
         return os.environ[key]
 
