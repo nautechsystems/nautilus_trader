ce63e3b6fc0e9cd8d0e2bb6c7217c3b1ae2c00f6
Bshara Zahran <bshara23@gmail.com>
2025-01-01 11:30:17 +1100

    Update headers

--- nautilus_core/common/src/logging/headers.rs
+++ nautilus_core/common/src/logging/headers.rs
@@ -1,5 +1,5 @@
 // -------------------------------------------------------------------------------------------------
-//  Copyright (C) 2015-2024 Nautech Systems Pty Ltd. All rights reserved.
+//  Copyright (C) 2015-2025 Nautech Systems Pty Ltd. All rights reserved.
 //  https://nautechsystems.io
 //
 //  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
@@ -37,7 +37,7 @@ pub fn log_header(trader_id: TraderId, machine_id: &str, instance_id: UUID4, com
     header_sepr(c, "=================================================================");
     header_sepr(c, " NAUTILUS TRADER - Automated Algorithmic Trading Platform");
     header_sepr(c, " by Nautech Systems Pty Ltd.");
-    header_sepr(c, " Copyright (C) 2015-2024. All rights reserved.");
+    header_sepr(c, " Copyright (C) 2015-2025. All rights reserved.");
     header_sepr(c, "=================================================================");
     header_line(c, "");
     header_line(c, "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀");
