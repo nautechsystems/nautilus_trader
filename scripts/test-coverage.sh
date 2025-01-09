4956e23948ec036419009551d2063305a31df49b
Chris Sellers <chris@cjdsellers.io>
2025-01-10 09:07:52 +1100

    Test CI coverage workflow

--- scripts/test-coverage.sh
+++ scripts/test-coverage.sh
@@ -2,4 +2,9 @@
 
 export PROFILE_MODE=true
 poetry install --with test --all-extras
-poetry run pytest --ignore=tests/performance_tests --cov-report=term --cov-report=xml --cov=nautilus_trader --new-first --failed-first
+poetry run pytest \
+    --ignore=tests/performance_tests \
+    --ignore=tests/acceptance_tests \
+    --cov-report=term \
+    --cov-report=xml \
+    --cov=nautilus_trader
