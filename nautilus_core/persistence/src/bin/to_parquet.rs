1eb63a1782f2e2ae98beccf4953550137d86e4bb
Chris Sellers <chris@cjdsellers.io>
2025-01-15 11:31:26 +1100

    Refine data migration guide

--- nautilus_core/persistence/src/bin/to_parquet.rs
+++ nautilus_core/persistence/src/bin/to_parquet.rs
@@ -1,3 +1,18 @@
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2025 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
+
 use std::{collections::HashMap, path::PathBuf};
 
 use datafusion::parquet::{
@@ -35,7 +50,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Validate file extension
     if !file_path
         .extension()
-        .map_or(false, |ext| ext.eq_ignore_ascii_case("json"))
+        .is_some_and(|ext| ext.eq_ignore_ascii_case("json"))
     {
         return Err("Input file must be a json file".into());
     }
