dd5254c5e1004ea1955fdd1ba3117ab5c1c4cf62
Chris Sellers <chris@cjdsellers.io>
2025-02-07 08:26:31 +1100

    Fix typo in InstrumentProviderConfig docstring

--- nautilus_trader/common/config.py
+++ nautilus_trader/common/config.py
@@ -363,7 +363,7 @@ class InstrumentProviderConfig(NautilusConfig, frozen=True):
     load_all : bool, default False
         If all venue instruments should be loaded on start.
     load_ids : frozenset[InstrumentId], optional
-        The list of instrument IDs to be loaded on start (if `load_all_instruments` is False).
+        The list of instrument IDs to be loaded on start (if `load_all` is False).
     filters : frozendict or dict[str, Any], optional
         The venue specific instrument loading filters to apply.
     filter_callable: str, optional
