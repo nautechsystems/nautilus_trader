1647c4f08a221b56660420284c25409bf253c8af
Filip Macek <filip_macek@hotmail.com>
2025-02-07 08:21:12 +0100

    introduce handlers to order matching core

--- nautilus_core/execution/src/matching_core/handlers.rs
+++ nautilus_core/execution/src/matching_core/handlers.rs
@@ -1,12 +1,28 @@
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2025 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
+
 use std::{cell::RefCell, rc::Rc};
 
 use nautilus_model::orders::OrderAny;
 
-use crate::{matching_engine::OrderMatchingEngine};
-use crate::order_emulator::emulator::OrderEmulator;
+use crate::{
+    matching_engine::engine::OrderMatchingEngine, order_emulator::emulator::OrderEmulator,
+};
 
 pub trait FillMarketOrderHandler {
-    fn fill_market_order(&self, order: &OrderAny);
+    fn fill_market_order(&mut self, order: &OrderAny);
 }
 
 #[derive(Clone)]
@@ -15,11 +31,24 @@ pub enum FillMarketOrderHandlerAny {
     OrderEmulator(Rc<RefCell<OrderEmulator>>),
 }
 
+impl FillMarketOrderHandler for FillMarketOrderHandlerAny {
+    fn fill_market_order(&mut self, order: &OrderAny) {
+        match self {
+            FillMarketOrderHandlerAny::OrderMatchingEngine(engine) => {
+                engine.borrow_mut().fill_market_order(&mut order.clone());
+            }
+            FillMarketOrderHandlerAny::OrderEmulator(emulator) => {
+                emulator.borrow_mut().fill_market_order(&mut order.clone());
+            }
+        }
+    }
+}
+
 #[derive(Clone)]
 pub struct ShareableFillMarketOrderHandler(pub FillMarketOrderHandlerAny);
 
 pub trait FillLimitOrderHandler {
-    fn fill_limit_order(&self, order: &OrderAny);
+    fn fill_limit_order(&mut self, order: &OrderAny);
 }
 
 #[derive(Clone)]
@@ -29,13 +58,13 @@ pub enum FillLimitOrderHandlerAny {
 }
 
 impl FillLimitOrderHandler for FillLimitOrderHandlerAny {
-    fn fill_limit_order(&self, order: &OrderAny) {
+    fn fill_limit_order(&mut self, order: &OrderAny) {
         match self {
             FillLimitOrderHandlerAny::OrderMatchingEngine(engine) => {
-                todo!("implement fill_limit_order for OrderMatchingEngine")
+                engine.borrow_mut().fill_limit_order(order);
             }
             FillLimitOrderHandlerAny::OrderEmulator(emulator) => {
-                todo!("implement fill_limit_order for OrderEmulator")
+                emulator.borrow_mut().fill_limit_order(&mut order.clone());
             }
         }
     }
@@ -45,7 +74,7 @@ impl FillLimitOrderHandler for FillLimitOrderHandlerAny {
 pub struct ShareableFillLimitOrderHandler(pub FillLimitOrderHandlerAny);
 
 pub trait TriggerStopOrderHandler {
-    fn trigger_stop_order(&self, order: &OrderAny);
+    fn trigger_stop_order(&mut self, order: &OrderAny);
 }
 
 #[derive(Clone)]
@@ -55,13 +84,13 @@ pub enum TriggerStopOrderHandlerAny {
 }
 
 impl TriggerStopOrderHandler for TriggerStopOrderHandlerAny {
-    fn trigger_stop_order(&self, order: &OrderAny) {
+    fn trigger_stop_order(&mut self, order: &OrderAny) {
         match self {
             TriggerStopOrderHandlerAny::OrderMatchingEngine(engine) => {
-                todo!("implement trigger_stop_order for OrderMatchingEngine")
+                engine.borrow_mut().trigger_stop_order(&mut order.clone());
             }
             TriggerStopOrderHandlerAny::OrderEmulator(emulator) => {
-                todo!("implement trigger_stop_order for OrderEmulator")
+                emulator.borrow_mut().trigger_stop_order(&mut order.clone());
             }
         }
     }
