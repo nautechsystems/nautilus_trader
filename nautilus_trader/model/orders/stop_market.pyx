b358a3d754a57dba9aac51920ee0bc1aa8826061
Bshara Zahran <bshara23@gmail.com>
2025-02-13 10:40:03 +1100

    Refine emulation_trigger parameter description

--- nautilus_trader/model/orders/stop_market.pyx
+++ nautilus_trader/model/orders/stop_market.pyx
@@ -90,7 +90,10 @@ cdef class StopMarketOrder(Order):
     quote_quantity : bool, default False
         If the order quantity is denominated in the quote currency.
     emulation_trigger : TriggerType, default ``NO_TRIGGER``
-        The order emulation trigger.
+        The type of market price trigger to use for local order emulation.
+        - ``NO_TRIGGER`` (default): Disables local emulation; orders are sent directly to the venue.
+        - ``DEFAULT`` (the same as ``BID_ASK``): Enables local order emulation by triggering orders based on bid/ask prices.
+        Additional trigger types are available. See the "Emulated Orders" section in the documentation for more details.
     trigger_instrument_id : InstrumentId, optional
         The emulation trigger instrument ID for the order (if ``None`` then will be the `instrument_id`).
     contingency_type : ContingencyType, default ``NO_CONTINGENCY``
