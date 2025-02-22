07a91d5ce2d701d707d934dcf5db804b3d4e57e0
Chris Sellers <chris@cjdsellers.io>
2025-02-23 08:49:15 +1100

    Standardize cargo manifest layout

--- crates/common/benches/cache/orders.rs
+++ crates/common/benches/cache/orders.rs
@@ -1,3 +1,18 @@
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2025 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
+
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
 use nautilus_common::cache::Cache;
 use nautilus_model::{
@@ -20,8 +35,8 @@ fn cache_orders_processing(orders: &[OrderAny]) {
     }
 }
 
-fn benchmark_order_indexing(c: &mut Criterion) {
-    // crete 100k orders list and add it to the cache
+fn bench_order_indexing(c: &mut Criterion) {
+    // Create 100k orders list and add it to the cache
     let all_orders = create_order_list_sample(5, 100, 200);
     let mut cache = Cache::default();
     for order in all_orders {
@@ -55,7 +70,7 @@ fn benchmark_order_indexing(c: &mut Criterion) {
     );
 }
 
-fn benchmark_order_processing(c: &mut Criterion) {
+fn bench_order_processing(c: &mut Criterion) {
     // Generate list with 100k orders which we slice per test (5 * 100 * 200 = 100k)
     let all_orders = create_order_list_sample(5, 100, 200);
 
@@ -72,9 +87,5 @@ fn benchmark_order_processing(c: &mut Criterion) {
     });
 }
 
-criterion_group!(
-    benches,
-    benchmark_order_indexing,
-    benchmark_order_processing
-);
+criterion_group!(benches, bench_order_indexing, bench_order_processing);
 criterion_main!(benches);
