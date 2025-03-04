302b3a8cf2716dee5f38916a8b92d327bc9a8836
FGU1 <56175843+FGU1@users.noreply.github.com>
2025-03-04 10:27:28 +0100

    IB adapter update v1030

--- nautilus_trader/adapters/interactive_brokers/client/client.py
+++ nautilus_trader/adapters/interactive_brokers/client/client.py
@@ -24,8 +24,8 @@ from typing import Any
 from ibapi import comm
 from ibapi.client import EClient
 from ibapi.commission_report import CommissionReport
-from ibapi.common import MAX_MSG_LEN
-from ibapi.common import NO_VALID_ID
+from ibapi.const import MAX_MSG_LEN
+from ibapi.const import NO_VALID_ID
 from ibapi.common import BarData
 from ibapi.errors import BAD_LENGTH
 from ibapi.execution import Execution
