03bed0cab2ef4dc2aae3b7154860adad35194f52
Chris Sellers <chris@cjdsellers.io>
2025-03-07 13:46:40 +1100

    Standardize test module organization

--- crates/execution/src/order_emulator/tests.rs
+++ crates/execution/src/order_emulator/tests.rs
@@ -12,10 +12,3 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 // -------------------------------------------------------------------------------------------------
-
-use rstest::rstest;
-
-#[rstest]
-fn test_init() {
-    assert_eq!(1, 1);
-}
