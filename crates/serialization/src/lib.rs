1e87dd677724e6bd697f860f6a79c38551580ac7
Chris Sellers <chris@cjdsellers.io>
2025-05-12 17:21:17 +1000

    Update outer attributes for serialization crate

--- crates/serialization/src/lib.rs
+++ crates/serialization/src/lib.rs
@@ -31,8 +31,9 @@
 #![warn(rustc::all)]
 #![deny(unsafe_code)]
 #![deny(nonstandard_style)]
-#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(missing_debug_implementations)]
 // #![deny(clippy::missing_errors_doc)]
+#![deny(rustdoc::broken_intra_doc_links)]
 
 pub mod arrow;
 pub mod enums;
