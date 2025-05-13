971fc364fc85d1414ae6a6c215f39ded3acb3510
Bshara Zahran <bshara23@gmail.com>
2025-05-13 19:48:16 +1000

    Standardize outer lint attributes

--- crates/adapters/tardis/src/lib.rs
+++ crates/adapters/tardis/src/lib.rs
@@ -34,6 +34,7 @@
 #![deny(nonstandard_style)]
 #![deny(missing_debug_implementations)]
 // #![deny(clippy::missing_errors_doc)]
+// #![deny(clippy::missing_panics_doc)]
 #![deny(rustdoc::broken_intra_doc_links)]
 
 pub mod config;
