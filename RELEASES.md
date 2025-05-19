b9a6385d77e1cd81177c61f3e398fb40ca0b74b8
Bshara Zahran <bshara23@gmail.com>
2025-05-19 17:57:24 +1000

    Add convenient re-exports for Coinbase Intx adapter

--- RELEASES.md
+++ RELEASES.md
@@ -6,6 +6,7 @@ Released on TBD (UTC).
 - Added convenient re-exports for Betfair adapter (constants, configs, factories, types)
 - Added convenient re-exports for Binance adapter (constants, configs, factories, loaders, types)
 - Added convenient re-exports for Bybit adapter (constants, configs, factories, loaders, types)
+- Added convenient re-exports for Coinbase International adapter (constants, configs, factories)
 - Added support for `FillModel`, `LatencyModel` and `FeeModel` in BacktestNode (#2601), thanks @faysou
 - Added `BacktestDataIterator` to backtest engine to provide on-the-fly data loading (#2545), thanks @faysou
 - Added support for `MarkPriceUpdate` streaming from catalog (#2582), thanks @bartolootrit
--- examples/live/coinbase_intx/coinbase_intx_quoter.py
+++ examples/live/coinbase_intx/coinbase_intx_quoter.py
@@ -18,11 +18,11 @@ from decimal import Decimal
 
 import pandas as pd
 
-from nautilus_trader.adapters.coinbase_intx.config import CoinbaseIntxDataClientConfig
-from nautilus_trader.adapters.coinbase_intx.config import CoinbaseIntxExecClientConfig
-from nautilus_trader.adapters.coinbase_intx.constants import COINBASE_INTX
-from nautilus_trader.adapters.coinbase_intx.factories import CoinbaseIntxLiveDataClientFactory
-from nautilus_trader.adapters.coinbase_intx.factories import CoinbaseIntxLiveExecClientFactory
+from nautilus_trader.adapters.coinbase_intx import COINBASE_INTX
+from nautilus_trader.adapters.coinbase_intx import CoinbaseIntxDataClientConfig
+from nautilus_trader.adapters.coinbase_intx import CoinbaseIntxExecClientConfig
+from nautilus_trader.adapters.coinbase_intx import CoinbaseIntxLiveDataClientFactory
+from nautilus_trader.adapters.coinbase_intx import CoinbaseIntxLiveExecClientFactory
 from nautilus_trader.cache.config import CacheConfig
 from nautilus_trader.common.enums import LogColor
 from nautilus_trader.config import InstrumentProviderConfig
