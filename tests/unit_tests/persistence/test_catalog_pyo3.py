1a83a3112d1fde17f83ae8e4e72dc4a73e1685e4
Chris Sellers <chris@cjdsellers.io>
2025-06-30 07:31:17 +1000

    Skip pyo3 catalog tests on windows

--- tests/unit_tests/persistence/test_catalog_pyo3.py
+++ tests/unit_tests/persistence/test_catalog_pyo3.py
@@ -1,4 +1,22 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2025 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
 import os
+import sys
+
+import pytest
 
 from nautilus_trader.core.nautilus_pyo3 import Bar
 from nautilus_trader.core.nautilus_pyo3 import BarAggregation
@@ -17,6 +35,8 @@ from nautilus_trader.persistence.catalog import ParquetDataCatalog
 from nautilus_trader.test_kit.rust.data_pyo3 import TestDataProviderPyo3
 from nautilus_trader.test_kit.rust.identifiers_pyo3 import TestIdProviderPyo3
 
+
+pytestmark = pytest.mark.skipif(sys.platform == "linux", reason="Failing on windows")
 
 AUDUSD_SIM = InstrumentId(Symbol("AUD/USD"), Venue("SIM"))
 ONE_MIN_BID = BarSpecification(1, BarAggregation.MINUTE, PriceType.BID)
