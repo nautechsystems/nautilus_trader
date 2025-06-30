cdd711062bbdb653335bccdbaadf8ed6ed2a52a1
DeevsDeevs <deevsgt15@gmail.com>
2025-06-30 06:16:24 +0200

    fix: format

--- nautilus_trader/adapters/bybit/websocket/client.py
+++ nautilus_trader/adapters/bybit/websocket/client.py
@@ -478,7 +478,9 @@ class BybitWebSocketClient:
                 if order_resp.retCode == 0:
                     future.set_result(order_resp)
                 else:
-                    future.set_exception(BybitError(code=order_resp.retCode, message=order_resp.retMsg))
+                    future.set_exception(
+                        BybitError(code=order_resp.retCode, message=order_resp.retMsg)
+                    )
             except Exception as e:
                 self._log.exception(f"Failed to decode order ack response {raw!r}", e)
         else:
