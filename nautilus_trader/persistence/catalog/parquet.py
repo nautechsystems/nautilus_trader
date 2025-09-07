cde040ddb4fe097f51b55963190e4d223a371fd7
Marcin Szymański <ms32035@gmail.com>
2025-09-07 22:18:13 +0100

    Improve schema copying

--- nautilus_trader/persistence/catalog/parquet.py
+++ nautilus_trader/persistence/catalog/parquet.py
@@ -687,9 +687,11 @@ class ParquetDataCatalog(BaseDataCatalog):
 
     @staticmethod
     def _deduplicate_table(table: pa.Table) -> pa.Table:
-        res = table.group_by(table.column_names).aggregate([])
-        res = res.replace_schema_metadata(table.schema.metadata)
-        return res
+        deduped_data_table = table.group_by(table.column_names).aggregate([])
+        return pa.Table.from_arrays(
+            deduped_data_table.columns,
+            schema=table.schema,
+        )
 
     def consolidate_catalog_by_period(
         self,
