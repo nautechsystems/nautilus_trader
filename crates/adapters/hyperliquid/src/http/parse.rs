8f0175283d9e742949be6c4640e5183684162fdf
Vadim Nicolai <nicolai.vadim@gmail.com>
2025-09-25 06:57:48 +0300

    Header fix

--- crates/adapters/hyperliquid/src/http/parse.rs
+++ crates/adapters/hyperliquid/src/http/parse.rs
@@ -1,14 +1,17 @@
-//! Response parsing functions for Hyperliquid HTTP models.
-//!
-//! This module converts upstream Hyperliquid "info" responses into normalized
-//! instrument definitions suitable for later construction of Nautilus
-//! `Instrument` instances inside an InstrumentProvider.
-//!
-//! Conventions:
-//! - Mirror upstream field names via casing only (camelCase -> snake_case).
-//! - Keep this module free of transport concerns (pure parsing).
-//! - Return simple, adapter-local definition structs; mapping into
-//!   Nautilus model types can be done by the InstrumentProvider.
+// -------------------------------------------------------------------------------------------------
+//  Copyright (C) 2015-2025 Nautech Systems Pty Ltd. All rights reserved.
+//  https://nautechsystems.io
+//
+//  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+//  You may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// -------------------------------------------------------------------------------------------------
 
 use rust_decimal::Decimal;
 use serde::{Deserialize, Serialize};
