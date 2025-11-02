19d8eda39fe98b0af57af2abd89278666692b150
Deirh <deirh.mana@gmail.com>
2025-11-02 20:11:29 +0100

    Formatting fix

--- nautilus_trader/adapters/polymarket/common/gamma_markets.py
+++ nautilus_trader/adapters/polymarket/common/gamma_markets.py
@@ -1,3 +1,18 @@
+# -------------------------------------------------------------------------------------------------
+#  Copyright (C) 2015-2025 Nautech Systems Pty Ltd. All rights reserved.
+#  https://nautechsystems.io
+#
+#  Licensed under the GNU Lesser General Public License Version 3.0 (the "License");
+#  You may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at https://www.gnu.org/licenses/lgpl-3.0.en.html
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------------------------
+
 """
 Thin Gamma Markets API client utilities for Polymarket.
 
@@ -20,12 +35,11 @@ from typing import List
 
 import requests
 
-
-DEFAULT_GAMMA_BASE_URL = os.getenv("GAMMA_API_URL", "https://gamma-api.polymarket.com")
 from trader.common.logging import get_logger
 
-
 _logger = get_logger(__name__)
+DEFAULT_GAMMA_BASE_URL = os.getenv("GAMMA_API_URL", "https://gamma-api.polymarket.com")
+
 
 def _normalize_base_url(base_url: str | None) -> str:
     url = base_url or DEFAULT_GAMMA_BASE_URL
