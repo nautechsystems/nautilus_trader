88e78ec4d37282d1d8eb74225c4ce43d6e25d65c
Deirh <deirh.mana@gmail.com>
2025-11-09 18:54:46 +0100

    Style fix

--- nautilus_trader/adapters/polymarket/data.py
+++ nautilus_trader/adapters/polymarket/data.py
@@ -12,9 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # -------------------------------------------------------------------------------------------------
-
 import asyncio
-from collections.abc import Coroutine
 from typing import Any
 
 import msgspec
