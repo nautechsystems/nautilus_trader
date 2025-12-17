8126e6e69c8124bc5286be8f86b0be0606e849f8
Filip Macek <filip_macek@hotmail.com>
2025-12-17 19:06:07 +0100

    Improve `send_response` message handling by adding explicit matching for `DataResponse` variants

--- crates/common/src/msgbus/mod.rs
+++ crates/common/src/msgbus/mod.rs
@@ -110,7 +110,15 @@ pub fn send_response(correlation_id: &UUID4, message: &DataResponse) {
         .cloned();
 
     if let Some(handler) = handler {
-        handler.0.handle(message);
+        match message {
+            DataResponse::Data(resp) => handler.0.handle(resp),
+            DataResponse::Instrument(resp) => handler.0.handle(resp.as_ref()),
+            DataResponse::Instruments(resp) => handler.0.handle(resp),
+            DataResponse::Book(resp) => handler.0.handle(resp),
+            DataResponse::Quotes(resp) => handler.0.handle(resp),
+            DataResponse::Trades(resp) => handler.0.handle(resp),
+            DataResponse::Bars(resp) => handler.0.handle(resp),
+        }
     } else {
         log::error!("send_response: handler not found for correlation_id '{correlation_id}'");
     }
@@ -124,19 +132,6 @@ pub fn publish_data(topic: &Ustr, message: Data) {
 
     for sub in matching_subs {
         sub.handler.0.handle(&message);
-    }
-}
-
-/// Sends the response to the handler registered for the `correlation_id` (if found).
-pub fn response(correlation_id: &UUID4, message: &dyn Any) {
-    let handler = get_message_bus()
-        .borrow()
-        .get_response_handler(correlation_id)
-        .cloned();
-    if let Some(handler) = handler {
-        handler.0.handle(message);
-    } else {
-        log::error!("response: handler not found for correlation_id '{correlation_id}'");
     }
 }
 
