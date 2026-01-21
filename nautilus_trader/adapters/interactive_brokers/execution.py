6d6f4e553be5f87526f605c72be97fc2e492cb98
Shuo Zheng <github@shuo.dev>
2026-01-21 10:57:30 -0500

    Fixed IB reconciliation error when account has no positions

--- nautilus_trader/adapters/interactive_brokers/execution.py
+++ nautilus_trader/adapters/interactive_brokers/execution.py
@@ -439,9 +439,12 @@ class InteractiveBrokersExecutionClient(LiveExecutionClient):
         # periodic checks causes filled_qty mismatches because the position may have
         # changed due to partial fills on exit orders.
         if not command.open_only:
-            positions: list[IBPosition] = await self._client.get_positions(
+            positions: list[IBPosition] | None = await self._client.get_positions(
                 self.account_id.get_id(),
             )
+
+            if not positions:
+                positions = []
 
             ts_init = self._clock.timestamp_ns()
 
